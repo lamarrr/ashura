9d050a90c5d0d658eaea79a0d0eab28035867079
Basit Ayantunde <rlamarrr@gmail.com>
2020-11-23 02:02:02 +0100

    enabled optional args for VLK_ENSURE

--- include/vlk/utils.h
+++ include/vlk/utils.h
@@ -4,13 +4,14 @@
 
 #include "stx/panic.h"
 
-#define VLK_ENSURE(expr, message)     \
-  do {                                \
-    if (!(expr)) stx::panic(message); \
+#define IMPL_VLK_ENSURE(expr, ...)        \
+  do {                                    \
+    if (!(expr)) stx::panic(__VA_ARGS__); \
   } while (false);
+
+#define VLK_ENSURE(expr, ...) IMPL_VLK_ENSURE(expr, __VA_ARGS__)
 
 #define VLK_LOG(expr)               \
   do {                              \
     std::cout << expr << std::endl; \
   } while (false);
-
