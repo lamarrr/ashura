884136f44d332d8ae4e42c257d2abc947604151e
Basit Ayantunde <rlamarrr@gmail.com>
2020-12-08 00:46:38 +0100

    added make_pipeline_color_blend_attachment_state

--- include/vlk/gl.h
+++ include/vlk/gl.h
@@ -786,4 +786,21 @@ make_pipeline_depth_stencil_state_create_info() {
   return create_info;
 }
 
+// per framebuffer
+[[nodiscard]] VkPipelineColorBlendAttachmentState
+make_pipeline_color_blend_attachment_state() {
+  // simply overwrites the pixels in the destination
+  VkPipelineColorBlendAttachmentState state{};
+  state.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT |
+                         VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+  state.blendEnable = VK_TRUE;
+  state.srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA;
+  state.dstColorBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;
+  state.colorBlendOp = VK_BLEND_OP_ADD;
+  state.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;
+  state.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO;
+  state.alphaBlendOp = VK_BLEND_OP_ADD;
+  return state;
+}
+
 }  // namespace vlk
