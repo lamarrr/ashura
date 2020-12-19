28319e9b4edaf0b10a4c632141c5fa21c21497c5
Basit Ayantunde <rlamarrr@gmail.com>
2020-12-19 21:23:40 +0100

    changed spirv binaries path

--- include/vlk/config.h
+++ include/vlk/config.h
@@ -6,8 +6,7 @@ namespace vlk {
 
 namespace config {
 
-const std::filesystem::path kSpirvBinariesPath = std::filesystem::path(
-    "/home/lamar/Documents/workspace/oss/valkyrie/build/spirv-binaries");
+const std::filesystem::path kSpirvBinariesPath = std::filesystem::path(".");
 
 }
 
