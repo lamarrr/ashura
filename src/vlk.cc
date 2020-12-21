b52e1688f440af7ac37f9503fc15c234b74110e7
Basit Ayantunde <rlamarrr@gmail.com>
2020-12-21 02:41:50 +0100

    added vertex data loading

--- src/vlk.cc
+++ src/vlk.cc
@@ -441,18 +441,208 @@ struct [[nodiscard]] Application {
     }
   }
 
-  void create_buffers_() {
-    std::vector<Vertex> vertices = {{{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
-                                    {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
-                                    {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}};
-    device_vertex_buffer_ =
-        Buffer<VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_SHARING_MODE_EXCLUSIVE,
-               static_cast<VkMemoryPropertyFlagBits>(
-                   VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
-                   VK_MEMORY_PROPERTY_HOST_COHERENT_BIT)>::
-            create_with_allocator(logical_device_, physical_device_,
-                                  vertices.size() * sizeof(Vertex), 50'000'000);
-    // TODO(lamarrr): move semantics, resource types, allocator context, etc
+  // loads vertex and index data to Graphics device
+  void load_vertex_index_data_() {
+    Vertex const vertices[] = {{
+                                   {-0.5f, -0.5f, -0.5f},
+                                   {0.583f, 0.771f, 0.014f},
+                               },
+                               {
+                                   {-0.5f, -0.5f, 0.5f},
+                                   {0.609f, 0.115f, 0.436f},
+                               },
+                               {
+                                   {-0.5f, 0.5f, 0.5f},
+                                   {0.327f, 0.483f, 0.844f},
+                               },
+                               {
+                                   {0.5f, 0.5f, -0.5f},
+                                   {0.822f, 0.569f, 0.201f},
+                               },
+                               {
+                                   {-0.5f, -0.5f, -0.5f},
+                                   {0.435f, 0.602f, 0.223f},
+                               },
+                               {
+                                   {-0.5f, 0.5f, -0.5f},
+                                   {0.31f, 0.747f, 0.185f},
+                               },
+                               {
+                                   {0.5f, -0.5f, 0.5f},
+                                   {0.597f, 0.77f, 0.761f},
+                               },
+                               {
+                                   {-0.5f, -0.5f, -0.5f},
+                                   {0.559f, 0.436f, 0.73f},
+                               },
+                               {
+                                   {0.5f, -0.5f, -0.5f},
+                                   {0.359f, 0.583f, 0.152f},
+                               },
+                               {
+                                   {0.5f, 0.5f, -0.5f},
+                                   {0.483f, 0.596f, 0.789f},
+                               },
+                               {
+                                   {0.5f, -0.5f, -0.5f},
+                                   {0.559f, 0.861f, 0.639f},
+                               },
+                               {
+                                   {-0.5f, -0.5f, -0.5f},
+                                   {0.195f, 0.548f, 0.859f},
+                               },
+                               {
+                                   {-0.5f, -0.5f, -0.5f},
+                                   {0.014f, 0.184f, 0.576f},
+                               },
+                               {
+                                   {-0.5f, 0.5f, 0.5f},
+                                   {0.771f, 0.328f, 0.97f},
+                               },
+                               {
+                                   {-0.5f, 0.5f, -0.5f},
+                                   {0.406f, 0.615f, 0.116f},
+                               },
+                               {
+                                   {0.5f, -0.5f, 0.5f},
+                                   {0.676f, 0.977f, 0.133f},
+                               },
+                               {
+                                   {-0.5f, -0.5f, 0.5f},
+                                   {0.971f, 0.572f, 0.833f},
+                               },
+                               {
+                                   {-0.5f, -0.5f, -0.5f},
+                                   {0.14f, 0.616f, 0.489f},
+                               },
+                               {
+                                   {-0.5f, 0.5f, 0.5f},
+                                   {0.997f, 0.513f, 0.064f},
+                               },
+                               {
+                                   {-0.5f, -0.5f, 0.5f},
+                                   {0.945f, 0.719f, 0.592f},
+                               },
+                               {
+                                   {0.5f, -0.5f, 0.5f},
+                                   {0.543f, 0.021f, 0.978f},
+                               },
+                               {
+                                   {0.5f, 0.5f, 0.5f},
+                                   {0.279f, 0.317f, 0.505f},
+                               },
+                               {
+                                   {0.5f, -0.5f, -0.5f},
+                                   {0.167f, 0.62f, 0.077f},
+                               },
+                               {
+                                   {0.5f, 0.5f, -0.5f},
+                                   {0.347f, 0.857f, 0.137f},
+                               },
+                               {
+                                   {0.5f, -0.5f, -0.5f},
+                                   {0.055f, 0.953f, 0.042f},
+                               },
+                               {
+                                   {0.5f, 0.5f, 0.5f},
+                                   {0.714f, 0.505f, 0.345f},
+                               },
+                               {
+                                   {0.5f, -0.5f, 0.5f},
+                                   {0.783f, 0.29f, 0.734f},
+                               },
+                               {
+                                   {0.5f, 0.5f, 0.5f},
+                                   {0.722f, 0.645f, 0.174f},
+                               },
+                               {
+                                   {0.5f, 0.5f, -0.5f},
+                                   {0.302f, 0.455f, 0.848f},
+                               },
+                               {
+                                   {-0.5f, 0.5f, -0.5f},
+                                   {0.225f, 0.587f, 0.04f},
+                               },
+                               {
+                                   {0.5f, 0.5f, 0.5f},
+                                   {0.517f, 0.713f, 0.338f},
+                               },
+                               {
+                                   {-0.5f, 0.5f, -0.5f},
+                                   {0.053f, 0.959f, 0.12f},
+                               },
+                               {
+                                   {-0.5f, 0.5f, 0.5f},
+                                   {0.393f, 0.621f, 0.362f},
+                               },
+                               {
+                                   {0.5f, 0.5f, 0.5f},
+                                   {0.673f, 0.211f, 0.457f},
+                               },
+                               {
+                                   {-0.5f, 0.5f, 0.5f},
+                                   {0.82f, 0.883f, 0.371f},
+                               },
+                               {
+                                   {0.5f, -0.5f, 0.5f},
+                                   {0.982f, 0.099f, 0.879f},
+                               }};
+
+    std::vector<uint32_t> indices;
+    indices.resize(std::size(vertices));
+    std::iota(indices.begin(), indices.end(), 0);
+
+    HostStagingBuffer host_staging_buffer =
+        HostStagingBuffer::create(logical_device_, physical_device_,
+                                  std::max(std::size(vertices) * sizeof(Vertex),
+                                           indices.size() * sizeof(uint32_t)),
+                                  50'000'000);
+
+    device_vertex_buffer_ = DeviceVertexBuffer::create(
+        logical_device_, physical_device_, std::size(vertices) * sizeof(Vertex),
+        50'000'000);
+    device_index_buffer_ = DeviceIndexBuffer::create(
+        logical_device_, physical_device_, indices.size() * sizeof(uint32_t),
+        50'000'000);
+
+    host_staging_buffer.write(logical_device_, 0, stx::Span(vertices).as_u8());
+
+    cmd::Recorder{transfer_command_buffer_}
+        .begin_recording(VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT)
+        .copy(host_staging_buffer.buffer_, 0,
+              std::size(vertices) * sizeof(Vertex),
+              device_vertex_buffer_.buffer_, 0)
+        .end_recording();
+
+    {
+      auto fence = create_fence(logical_device_, false);
+
+      submit_commands(transfer_command_queue_, transfer_command_buffer_, {}, {},
+                      {}, fence);
+
+      await_fence(logical_device_, fence);
+
+      reset_fence(logical_device_, fence);
+
+      reset_command_buffer(transfer_command_buffer_);
+
+      host_staging_buffer.write(logical_device_, 0, stx::Span(indices).as_u8());
+      cmd::Recorder{transfer_command_buffer_}
+          .begin_recording(VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT)
+          .copy(host_staging_buffer.buffer_, 0,
+                indices.size() * sizeof(uint32_t), device_index_buffer_.buffer_,
+                0)
+          .end_recording();
+
+      submit_commands(transfer_command_queue_, transfer_command_buffer_, {}, {},
+                      {}, fence);
+
+      await_fence(logical_device_, fence);
+
+      vkDestroyFence(logical_device_, fence, nullptr);
+    }
+
+    host_staging_buffer.destroy(logical_device_);
   }
 
   VkViewport get_viewport_() {
