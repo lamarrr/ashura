9f31e07391cab34f300391e7666119cf39656e48
Basit Ayantunde <rlamarrr@gmail.com>
2020-12-24 09:36:31 +0100

    added timer

---
+++ include/vlk/timer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <chrono>
+
+template <typename ClockType = std::chrono::steady_clock>
+struct TickTimer {
+  using time_point = typename ClockType::time_point;
+  using clock_type = ClockType;
+
+  TickTimer() noexcept {}
+
+  auto start() noexcept { last_time_point_ = clock_type::now(); }
+
+  std::chrono::nanoseconds tick() noexcept {
+    auto current_time_point = clock_type::now();
+    auto tick_duration = current_time_point - last_time_point_;
+    last_time_point_ = current_time_point;
+    return tick_duration;
+  }
+
+ private:
+  time_point last_time_point_;
+};
