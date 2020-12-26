8cee19f6e1842b52106b6566f13dac31ee95aeb5
Basit Ayantunde <rlamarrr@gmail.com>
2020-12-26 10:53:36 +0100

    Updated widget graph implementation

--- include/vlk/widget.h
+++ include/vlk/widget.h
@@ -1,237 +1,289 @@
+#include <chrono>
+#include <cinttypes>
+#include <functional>
+#include <map>
+#include <variant>
+#include <vector>
 
+#include "stx/option.h"
+#include "vlk/image.h"
 
+namespace vlk {
 
+// Used for PIMPL
+struct CanvasBackend2D {};
 
-struct Canvas;
-struct Context{};
+// might need caching for resources?
+struct CanvasRecorder2D {};
 
-struct StatefulWidget{
+struct ScreenState {
+  uint32_t width;
+  uint32_t height;
 
-
-virtual bool is_dirty() { return dirty_; };
-
-
-virtual void draw(Canvas& canvas, Context const& context){
-	mark_clean_();
-}
-
-protected:
-
-void mark_dirty_() {
-	dirty_ = true;
-}
-
-void mark_clean_(){
-	dirty_ = false;
-}
-
-private:
-	bool dirty_;
-
+  float aspect_ratio() const;
 };
 
-namespace desc{
-struct Image{
-enum Format{
-rgba,
-argb,
-rgb
+struct Context2D {
+  ScreenState screen_state;
 };
 
-uint64_t width;
-uint64_t height;
+struct Coordinates2D {
+  float data[2];
 
-// depends on how skia receives it
-stx::Span<u32 const> data;
+  float x() const;
+  float y() const;
 
-};
-};
-
-
-struct Coordinates{
-f32 x, y;
+  void set_x();
+  void set_y();
 };
 
-struct BackgroundImage{
-Image image;
-Coordinates coordinates;
+struct Coordinates3D {
+  float data[3];
+
+  float x() const;
+  float y() const;
+  float z() const;
+
+  void set_x();
+  void set_y();
+  void set_z();
 };
 
-namespace box{
-using px = u32;
-
-struct Dimensions{
-stx::Option<px> width = 0; // auto-fit if None
-stx::Option<px> height = 0; // auto-fit if None
+struct AutoExtent2D {
+  stx::Option<uint32_t> width;   // auto-fit if None
+  stx::Option<uint32_t> height;  // auto-fit if None
 };
 
-struct Color{
-u32 rgba;
-static rgba(u8, u8, u8, u8);
-static rgb(u8, u8, u8);
+struct Color {
+  uint32_t rgba;
+  static Color rgba(uint8_t, uint8_t, uint8_t, uint8_t);
+  static Color rgb(uint8_t, uint8_t, uint8_t);
 
-constexpr auto Color with_opacity(u8) const noexcept;
-constexpr auto Color with_red(u8) const noexcept;
-constexpr auto Color with_green(u8) const noexcept;
-constexpr auto Color with_blue(u8) const noexcept;
+  constexpr Color with_opacity(uint8_t) const;
+  constexpr Color with_red(uint8_t) const;
+  constexpr Color with_green(uint8_t) const;
+  constexpr Color with_blue(uint8_t) const;
 
-static constexpr auto Red = Color::rgba(0xFF, 0x00, 0x00, 0xFF);
-static constexpr auto White = Color::rgba(0xFF, 0xFF, 0xFF, 0xFF);
-static constexpr auto Black = Color::rgba(0x00, 0x00, 0x00, 0xFF);
-static constexpr auto Blue = Color::rgba(0x00, 0x00, 0xFF, 0xFF);
-static constexpr auto Green = Color::rgba(0x00, 0xFF, 0x00, 0xFF);
+  static constexpr auto Red = Color::rgb(0xFF, 0x00, 0x00);
+  static constexpr auto White = Color::rgb(0xFF, 0xFF, 0xFF);
+  static constexpr auto Black = Color::rgb(0x00, 0x00, 0x00);
+  static constexpr auto Blue = Color::rgb(0x00, 0x00, 0xFF);
+  static constexpr auto Green = Color::rgb(0x00, 0xFF, 0x00);
 };
 
-struct Tbrl{
-px top = 0, bottom = 0, right = 0, left = 0;
-static uniform(px);
-static xy(px, px);
-static tbrl(px, px, px, px);
+// to be inherited from
+struct TopBottomLeftRight {
+  uint32_t top = 0, bottom = 0, right = 0, left = 0;
+  static TopBottomLeftRight uniform(uint32_t);
+  static TopBottomLeftRight xy(uint32_t, uint32_t);
+  static TopBottomLeftRight tblr(uint32_t, uint32_t, uint32_t, uint32_t);
 };
 
-struct Border: public Tbrl{
-	using Tbrl::Tbrl;
+struct Border : public TopBottomLeftRight {
+  using TopBottomLeftRight::TopBottomLeftRight;
 };
 
-struct Padding: public Tbrl{
-	using Tbrl::Tbrl;
+struct Padding : public TopBottomLeftRight {
+  using TopBottomLeftRight::TopBottomLeftRight;
 };
 
-struct Properties{
-std::variant<Color, BackgroundImage> background;
-Border border;
-Dimensions dimensions;
-px padding [4];
-px margin [4];
+struct Margin : public TopBottomLeftRight {
+  using TopBottomLeftRight::TopBottomLeftRight;
 };
 
+struct BackgroundImage {
+  desc::Image2D image;
+  Coordinates2D coordinates;
+  uint8_t opacity;  // uint8?
 };
 
-struct DynamicButton: StatefulWidget{
-
-bool is_dirty() override {
-	return content_changed_;
+struct BoxModel {
+  std::variant<Color, BackgroundImage> background;
+  Border border;
+  AutoExtent2D extent;
+  Padding padding;
+  Margin margin;
 };
 
-void draw(Canvas& canvas, Context const& context){
-	auto subcanvas = canvas.subcanvas();
-	subcanvas.draw_text("", ...,...,...);
-	subcanvas.draw_rect(...,...,...,...);
-	StatefulWidget::draw(canvas, context);
-	// use recorder actually
-}
+// namespace ui
 
+namespace impl {
+// how to iterate over array of widgets?
+// we are hereby avoiding unneeded hierachies via this proxy
+struct WidgetEventProxy {
+  template <typename... ParamTypes>
+  using EventHandler = void(void *, ParamTypes...);
 
-Properties properties_;
+  using TickHandler = EventHandler<std::chrono::nanoseconds const &>;
+  using DrawHandler = EventHandler<Context2D const &, CanvasRecorder2D &>;
+  using Handler = EventHandler<void>;
+  using QueryHandler = bool(void *);
 
+ public:
+  WidgetEventProxy(void *widget, bool is_stateful)
+      : widget_{widget}, is_stateful_{is_stateful_}, is_dirty_{true} {}
+
+  TickHandler on_tick;
+  DrawHandler on_draw;
+
+  Handler on_click;
+  Handler on_hover;
+  Handler on_enter_view;
+  Handler on_leave_view;
+
+  QueryHandler is_dirty;
+
+  void *widget() const noexcept { return widget_; }
+
+  bool is_stateful() const noexcept { return is_stateful_; };
+
+ private:
+  void *widget_;
+  bool is_stateful_;
 };
 
+/************ These widget graphs will always be held in memory because
+ * WidgetProxy will be referencing them *********/
 
-struct StatelessWidget{
-
-virtual void draw(Canvas& canvas, Context const& context) = 0;
+// The widgets will be stored as a pointer to this type, this will enable the
+// widget's appropriate destructor to be called
+virtual struct WidgetDestructorProxy {
+  virtual ~WidgetDestructor() noexcept = 0;
 };
 
-
-
-template<typename ClockType = std::chrono::steady_clock>
-struct Ticker{
-
-void start(){
-last_tick_time_point_ = ClockType::now();
-}
-
-std::chrono::nanoseconds tick(){
-auto previous_tick_time_point = last_tick_time_point_;
-last_tick_timepoint_ = ClockType::now();
-return last_tick_time_point_ - previous_tick_time_point;
+// For feeding the widgets events
+struct WidgetEventBase {
+  void on_click() {}       // may or may not be defined
+  void on_hover() {}       // may or may not be defined
+  void on_enter_view() {}  // may or may not be defined
+  void on_leave_view() {}  // may or may not be defined
 };
 
-typename ClockType::timepoint last_tick_time_point_;
+struct WidgetDrawBase {
+  void draw(Context2D const &context, CanvasRecorder2D &canvas) {
+    // no-op
+  }
 };
 
-
-
-struct Body{
-Color color;
+struct WidgetTickBase {
+  // for UI related processing, all other-processing should be done
+  // asynchronously
+  void tick(std::chrono::nanoseconds const &interval) {
+    // no-op
+  }
 };
 
+struct WidgetStatefulnessBase {
+  bool is_dirty() { return true; }
+};
 
+};  // namespace impl
 
-// dynamic library to get ui description and doesn't contain the actual contents of the engine itself.
-// The engine is always loaded but gets ui description from DLL.
-// It's okay to use virtual inheritance here since it is not in a real-time loop and it only gets the UI desription
+// virtual function calls here are not actually used, but helps to guide the
+// user, the only virtual function call used here is the destructor
+struct StatelessWidget : public impl::WidgetDestructorProxy,
+                         public impl::WidgetDrawBase,
+                         public impl::WidgetEventBase,
+                         public impl::WidgetTickBase {
+  virtual ~StatelessWidget()  noexcept override = 0;
+};
+
+struct StatefulWidget : public impl::WidgetDestructorProxy,
+                        public impl::WidgetDrawBase,
+                        public impl::WidgetEventBase,
+                        public impl::WidgetTickBase,
+                        public impl::WidgetStatefulnessBase {
+  virtual ~StatelessWidget()  noexcept override = 0;
+};
+
+struct WidgetGraph {
+  std::vector<impl::WidgetDestructorProxy *> children_;
+  std::vector<impl::WidgetEventProxy> event_proxies_;
+
+  // The WidgetType determines how the function methods will be used, since we
+  // are constructing it ourself and not using an external reference, we can
+  // take shortcuts on the inheritance hierarchy, this will help prevent
+  // excessive vtable look-up in the rendering code. we know the exact type of
+  // the widget hence we only pay at compile time with many functions.
+  template <typename WidgetType, typename... ConstructorArguments>
+  WidgetGraph &add_stateful_child(ConstructorArguments &&... arguments) & {
+    static_assert(!std::is_reference_v<WidgetType>);
+    static_assert(!std::is_const_v<WidgetType>);
+    static_assert(std::is_base_of_v<StatefulWidget, WidgetType>);
+
+    // std::unique_ptr because std::vector::push_back is not noexcept and could
+    // possibly leak otherwise
+    std::unique_ptr<WidgetType> widget{
+        new WidgetType{std::forward<ConstructorArguments &&>(arguments)...}};
+
+    impl::WidgetEventProxy proxy{widget.get(), true};
+
+    proxy.on_tick =  [](void *widget_ptr,  std::chrono::nanoseconds const& interval) {
+      auto widget = reinterpret_cast<WidgetType *>(widget_ptr);
+      widget->tick(interval);
+    });
+
+    proxy.on_draw =  [](void *widget_ptr,  Context2D const & context, CanvasRecorder2D & canvas) {
+      auto widget = reinterpret_cast<WidgetType *>(widget_ptr);
+      widget->draw(context, canvas);
+    });
+
+    proxy.on_click = [](void *widget_ptr) {
+      auto widget = reinterpret_cast<WidgetType *>(widget_ptr);
+      widget->on_click();
+    };
+
+    proxy.on_hover = [](void *widget_ptr) {
+      auto widget = reinterpret_cast<WidgetType *>(widget_ptr);
+      widget->on_hover();
+    };
+
+    proxy.on_enter_view = [](void *widget_ptr) {
+      auto widget = reinterpret_cast<WidgetType *>(widget_ptr);
+      widget->on_enter_view();
+    };
+
+    proxy.on_leave_view = [](void *widget_ptr) {
+      auto widget = reinterpret_cast<WidgetType *>(widget_ptr);
+      widget->on_leave_view();
+    };
+
+    proxy.is_dirty = [](void *widget_ptr) {
+      auto widget = reinterpret_cast<WidgetType *>(widget_ptr);
+      return widget->is_dirty();
+    };
+
+    children_.push_back(widget.get());
+
+    widget.release();
+
+    return *this;
+  }
+
+  WidgetGraph &add_stateful_child() && = delete;
+
+  ~WidgetGraph() {
+    for (auto *child : children_) {
+      child->~WidgetDestructorProxy();
+    }
+  }
+};
+
+struct Button : StatefulWidget {};
+
+// dynamic library to get ui description and doesn't contain the actual contents
+// of the engine itself. The engine is always loaded but gets ui description
+// from DLL. It's okay to use virtual inheritance here since it is not in a
+// real-time loop and it only gets the UI desription
 
 // the whole 2d ui performs only one flushandsubmit call to skia
 
-namespace ui{
-inline namespace desc{
+};  // namespace vlk
 
-struct Screen{
-	size_t width;
-	size_t height;
-	bool portrait_mode;
+// Same goes for 3D?
 
-	auto aspect_ratio();
+extern "C" void *vlk_get_func(char const *name) {
+  // get widgets description
+  // get whole 2d context information required for the engine
+  return {};
 };
-
-struct Context{
-	Screen screen;
-};
-
-
-struct InputsHandler{
-enum class Keys{
-Up,
-Down,
-Left,
-Right,
-...
-} keys;
-struct Cursor {};
-
-};
-
-struct Widget{
-
-virtual void draw(Context const & context) {
-struct CanvasBackend; // in exported header file
-// in internal impl => reinterpret_cast to SkCanvas. with this the user won't have to interact with the canvas backend,
-// only we get to do that and we don't have to duplicate the canvas implementation
-	
-using CanvasBackend =  CanvasBackendT*;
-	
-	VLK_ENSURE(vlk::canvas_backend() == CanvasBackendType::Skia);
-
-	CanvasBackend canvas_backend = context.canvas_backend();
-	SkCanvas& canvas = *reinterpret_cast<SkiaCanvas*>(canvas_backend);
-	auto& rect = canvas.rect(...);
-	rect.text("Empty Widget", 0, 0);
-};
-
-private:
-	Position position_;
-};
-
-}; // namespace desc
-}; // namespace ui
-
-
-Same goes for 3D?
-
-extern "C" void* vlk_get_func(char const* name){
-// get widgets description
-// get whole 2d context information required for the engine
-};
-
-
-virtual struct StatefulWidget{
-
-bool is_dirty();
-
-};
-
-virtual struct StatelessWidget{
-};
-
-
