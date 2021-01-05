e1610bd9cfb222c35eed04f7479bee9c515524a5
Basit Ayantunde <rlamarrr@gmail.com>
2021-01-05 22:55:23 +0100

    added ui2d stubs

---
+++ ui2d/include/vlk/ui2d/theme.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace vlk {
+namespace ui2d {
+
+struct Theme {
+  virtual void text();
+  virtual void h1();
+  virtual void primary_color();
+};
+
+}  // namespace ui2d
+}  // namespace vlk
