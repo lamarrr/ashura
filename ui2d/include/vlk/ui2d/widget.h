b5f23eaa37f58225fce7d7794dec6c42ba022ad1
Basit Ayantunde <rlamarrr@gmail.com>
2021-01-23 00:48:42 +0100

    code refactoring

--- ui2d/include/vlk/ui2d/widget.h
+++ ui2d/include/vlk/ui2d/widget.h
@@ -31,7 +31,7 @@ namespace ui2d {
 // they should be children???? what about stack? we might need R-tree after all
 // events should be processed in the R-tree/map not by the renderer
 // tap_event(Offset position)
-struct Widget {
+struct [[nodiscard]] Widget {
   // used by the widget render compositor to determine if a widget needs drawing
   // or not.
   //
@@ -50,7 +50,7 @@ struct Widget {
   //
   //
   // TODO(lamarrr): what can we do to prevent rebuilding the whole tree?
-  virtual bool is_layout_type() const noexcept = 0;
+  [[nodiscard]] virtual bool is_layout_type() const noexcept = 0;
 
   // will any property of this widget that can affect its or its children
   // rendering change?
@@ -69,19 +69,34 @@ struct Widget {
   // their rendering data has changed.
   //
   //
-  virtual bool is_stateful() const noexcept = 0;
+  [[nodiscard]] virtual bool is_stateful() const noexcept = 0;
 
   // called to check if the rendering data of the widget has changed.
   // called on every frame if and only if the widget is a stateful type.
-  virtual bool is_dirty() const noexcept = 0;
+  [[nodiscard]] virtual bool is_dirty() const noexcept = 0;
 
   // marks that the renderer has responded and rebuilt this widget.
   virtual void mark_clean() noexcept = 0;
 
   // gets the list of children for this widget
-  virtual stx::Span<Widget *const> get_children() const noexcept = 0;
+  [[nodiscard]] virtual stx::Span<Widget *const> get_children()
+      const noexcept = 0;
 
-  bool has_children() const noexcept { return !get_children().empty(); }
+  [[nodiscard]] bool has_children() const noexcept {
+    return !get_children().empty();
+  }
+
+  // static or part of widget state?
+  // if this changes from the one in our cache entry then we need to update and
+  // put it in the appropriate position by default, z_index increases by
+  // increasing depth
+  [[nodiscard]] virtual stx::Option<uint32_t> z_index() const noexcept {
+    return stx::None;
+  }
+
+  // used for optimizing the case where the widget is a simple one and don't
+  // take much of render time. i.e. a simple color fill block.
+  // TODO(lamarrr) virtual bool is_simple() const noexcept { return false; }
 
   // used for positioning this widget within it's allotted area by its parent.
   // it should return an area it wants to occupy out of the allotted_extent, and
@@ -104,22 +119,22 @@ struct Widget {
   //    -   the extent returned will be used for allocating a canvas for
   //    drawing.
   //
-  virtual Rect compute_area(Extent const &allotted_extent,
-                            stx::Span<Rect> const &children_area) const
-      noexcept = 0;
+  [[nodiscard]] virtual Rect compute_area(
+      Extent const &allotted_extent, stx::Span<Rect> const &children_area) = 0;
 
   // draw itself. if it has children, the children will be drawn separately.
   // for overlaying, use a `Stack` widget instead.
-  virtual void draw(Canvas &canvas, Extent const &requested_extent) const = 0;
+  virtual void draw(Canvas & canvas, Extent const &requested_extent) = 0;
 
   // useful for debugging widgets
-  virtual std::string_view get_name() const noexcept { return "<unnamed>"; }
-
-  // get a type identifier for this widget type. uses RTTI.
-  virtual std::string_view get_type_name() const noexcept final {
-    auto &info = typeid(*this);
-    return info.name();
+  [[nodiscard]] virtual std::string_view get_name() const noexcept {
+    return "<unnamed>";
   }
+
+  // get a type identifier for this widget type.
+  [[nodiscard]] virtual std::string_view get_type_hint() const noexcept = 0;
+
+  [[nodiscard]] virtual bool should_cache() const noexcept { return true; }
 
   // enables the widget's appropriate destructor to be called.
   // widget graphs will be held in memory till rendering is finished.
