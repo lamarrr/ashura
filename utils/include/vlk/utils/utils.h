90d114bca069130a05a0a7b9139f56fa1419d5e7
Basit Ayantunde <rlamarrr@gmail.com>
2021-01-23 00:36:36 +0100

    corrected macros

--- utils/include/vlk/utils/utils.h
+++ utils/include/vlk/utils/utils.h
@@ -1,12 +1,21 @@
 #pragma once
 
+#include <type_traits>
+
+#include "vlk/utils/limits.h"
+
 #include "spdlog/spdlog.h"
 #include "stx/panic.h"
+
+#define VLK_PANIC(...)         \
+  do {                         \
+    ::stx::panic(__VA_ARGS__); \
+  } while (false)
 
 #define VLK_ENSURE(expr, ...)               \
   do {                                      \
     if (!(expr)) ::stx::panic(__VA_ARGS__); \
-  } while (false);
+  } while (false)
 
 #define VLK_ENABLE_DEBUG_CHECKS 1
 
@@ -19,7 +28,7 @@
 #if VLK_ENABLE_DEBUG_CHECKS
 #define VLK_DEBUG_ENSURE(expr, ...) \
   do {                              \
-    VLK_ENSURE(expr, __VA_ARGS__)   \
+    VLK_ENSURE(expr, __VA_ARGS__);  \
   } while (false)
 #else
 #define VLK_DEBUG_ENSURE(expr, ...) \
@@ -32,27 +41,27 @@
     auto VLK_VK_GL_Result = (expr);            \
     if (VLK_VK_GL_Result != VK_SUCCESS)        \
       ::stx::panic(message, VLK_VK_GL_Result); \
-  } while (false);
+  } while (false)
 
 #define VLK_LOG(fmt, ...)               \
   do {                                  \
     ::spdlog::info(fmt, ##__VA_ARGS__); \
-  } while (false);
+  } while (false)
 
 #define VLK_LOG_IF(expr, fmt, ...)                  \
   do {                                              \
     if ((expr)) ::spdlog::info(fmt, ##__VA_ARGS__); \
-  } while (false);
+  } while (false)
 
 #define VLK_WARN(fmt, ...)              \
   do {                                  \
     ::spdlog::warn(fmt, ##__VA_ARGS__); \
-  } while (false);
+  } while (false)
 
 #define VLK_WARN_IF(expr, fmt, ...)                 \
   do {                                              \
     if ((expr)) ::spdlog::warn(fmt, ##__VA_ARGS__); \
-  } while (false);
+  } while (false)
 
 namespace vlk {
 
