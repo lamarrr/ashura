a6d2f47ce09c42c552c800ebab31133df4cbed87
Basit Ayantunde <rlamarrr@gmail.com>
2021-03-22 10:14:55 +0100

    moved widget builder

--- ui/include/vlk/ui/widget_builder.h
+++ ui/include/vlk/ui/widget_builder.h
@@ -1,23 +1,27 @@
+#pragma once
 
+#include <algorithm>
+#include <cinttypes>
+#include <cstddef>
 #include <initializer_list>
 #include <vector>
 
 #include "stx/option.h"
 #include "stx/span.h"
 
-#include <cstdint>
-
 namespace vlk {
 namespace ui {
 
 struct Widget;
 
-/// builds a list of widgets, the function is called with the index, until it
+/// each widget accepting children could use these helper functions:
+/// they should have function overloads with: `WidgetBuilder`, `stx::Span<Widget
+/// *>`, `std::initializer_list<Widget*>`, and `std::vector<Widget*>`
+
+/// builds a list of widgets. the function is called with the index, until it
 /// returns `stx::None`
 using WidgetBuilder = std::function<stx::Option<Widget *>(size_t)>;
 
-// each widget accepting children args must have each of these.
-// Widget::Builder, stx::Span<Widget *>, std::initializer_list<Widget*>
 inline std::vector<Widget *> build_children(
     stx::Span<Widget *const> const &src_children) {
   std::vector<Widget *> children;
@@ -28,15 +32,15 @@ inline std::vector<Widget *> build_children(
 
 inline std::vector<Widget *> build_children(
     std::initializer_list<Widget *const> const &src_children) {
-  return build_children(
-      stx::Span<Widget *const>(src_children.begin(), src_children.end()));
+  return build_children(stx::Span<Widget *const>(src_children));
 }
 
 inline std::vector<Widget *> build_children(WidgetBuilder &builder) {
+  // l-value ref argument cos the builder could possibly have internal state
   std::vector<Widget *> children;
   size_t i = 0;
   while (auto child = builder(i)) {
-    children.push_back(child.value());
+    children.push_back(child.clone().unwrap());
     i++;
   }
   return children;
