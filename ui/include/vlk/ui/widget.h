64a702a3eaeb810be063f82fdbce06c58c1fbaee
Basit Ayantunde <rlamarrr@gmail.com>
2021-03-22 10:15:14 +0100

    updated widget struct

--- ui/include/vlk/ui/widget.h
+++ ui/include/vlk/ui/widget.h
@@ -2,6 +2,7 @@
 #pragma once
 
 #include <chrono>
+#include <cinttypes>
 #include <functional>
 #include <string_view>
 
@@ -14,29 +15,26 @@
 #include "vlk/ui/layout.h"
 
 // TODO(lamarrr): should we pass a rasterization constext to the draw function?
+// (do draw commands keep references to image data)?
+// can we abstract the datasource provider to destroy returned memeory
+// immediately after use so we can copy elsewhere? also, we need to have async
+// first asset loading as loading synchronously will be slow
 
 namespace vlk {
 namespace ui {
 
-// Effects effects effects, i don't think effect insertion is right
-
-/// TODO(lamarrr): we should ensure user data is always accessed on the main
-/// thread (do draw commands keep references to image data)?
-/// can we abstract the datasource provider to destroy returned memeory
-/// immediately after use so we can copy elsewhere? also, we need to have async
-/// first asset loading as loading synchronously will be slow
-
-/// NOTE: Widget is a very large struct (about 360-bytes). avoid
+/// NOTE: Widget is a very large struct (about 420-bytes). avoid
 /// touching the struct in hot code paths as it could disrupt cache
 /// if you're touching a large number of them, especially whilst not all fields
 /// of it are touched
+/// NOTE: this struct's data is always accessed from the main thread.
 struct Widget {
   friend struct WidgetStateProxyAccessor;
 
   struct DebugInfo {
     DebugInfo(std::string_view const &name = "<unnamed widget>",
-              std::string_view const &type_hint = "Generic Widget",
-              std::string_view const &info = "")
+              std::string_view const &type_hint = "Unhinted Widget Type",
+              std::string_view const &info = "(no info)")
         : name{name}, type_hint{type_hint}, info{info} {}
 
     std::string_view name;
@@ -88,23 +86,23 @@ struct Widget {
         view_offset_{view_offset},
         state_proxy_{} {}
 
-  Type get_type() const noexcept { return type_; }
+  Type get_type() const { return type_; }
 
-  SelfExtent get_self_extent() const noexcept { return self_extent_; }
+  SelfExtent get_self_extent() const { return self_extent_; }
 
-  Flex get_flex() const noexcept { return flex_; }
+  Flex get_flex() const { return flex_; }
 
-  stx::Span<Widget *const> get_children() const noexcept { return children_; }
+  stx::Span<Widget *const> get_children() const { return children_; }
 
-  bool has_children() const noexcept { return get_children().size() != 0; }
+  bool has_children() const { return !get_children().empty(); }
 
   DebugInfo get_debug_info() const { return debug_info_; }
 
-  stx::Option<ZIndex> get_z_index() const noexcept { return z_index_.clone(); }
+  stx::Option<ZIndex> get_z_index() const { return z_index_.clone(); }
 
-  SelfExtent get_view_extent() const noexcept { return view_extent_; }
+  SelfExtent get_view_extent() const { return view_extent_; }
 
-  ViewOffset get_view_offset() const noexcept { return view_offset_; }
+  ViewOffset get_view_offset() const { return view_offset_; }
 
   /// create draw commands
   virtual void draw([[maybe_unused]] Canvas &canvas) {
@@ -120,15 +118,15 @@ struct Widget {
 
   void init_type(Type type) { type_ = type; }
 
-  void update_self_extent(SelfExtent const &extent = {}) {
-    self_extent_ = extent;
+  void update_self_extent(SelfExtent const &self_extent) {
+    self_extent_ = self_extent;
     mark_layout_dirty();
   }
 
   // NOTE: this widget will now bind itself to this string
   void set_debug_info(DebugInfo const &info) { debug_info_ = info; }
 
-  void init_z_index(stx::Option<ZIndex> const &z_index = stx::None) {
+  void init_z_index(stx::Option<ZIndex> const &z_index) {
     z_index_ = z_index.clone();
   }
 
@@ -150,8 +148,8 @@ struct Widget {
     mark_render_dirty();
   }
 
-  /// this does not free the children, the derived widget is in charge of
-  /// freeing memory
+  /// MOTE: this does not free the memory associated with the referenced
+  /// container. the derived widget is in charge of freeing memory as necessary
   void update_children(stx::Span<Widget *const> children) {
     children_ = children;
     mark_children_dirty();
@@ -203,7 +201,8 @@ struct Widget {
   /// resolved using the view extent.
   ViewOffset view_offset_;
 
-  // constant throughout lifetime, but modified by the system
+  /// constant throughout lifetime, but modified and used for communication and
+  /// managing updates in the system
   StateProxy state_proxy_;
 };
 
