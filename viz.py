6da5984ef5fd21d457e60bee303098bb0239aba0
Basit Ayantunde <rlamarrr@gmail.com>
2021-04-13 14:32:44 +0100

    updated viz script

--- viz.py
+++ viz.py
@@ -1,8 +1,10 @@
 from matplotlib import pyplot as pyp
 import numpy as np
+import sys
 
-with open("/home/lamar/Documents/workspace/oss/valkyrie/build/compositor.dump") as file:
-	pdata = np.reshape(np.array([int(c) for c in file.read().split(",")[:-1]]), (1080, 1920, 4))
+
+with open(sys.argv[1]) as file:
+	pdata = np.reshape(np.array([int(c) for c in file.read().split(",")[:-1]]), (1080, 1080, 4))
 
 pyp.imshow(pdata)
 pyp.show()
