886c521795ea01491e8e37ba4103355601e31d85
Basit Ayantunde <rlamarrr@gmail.com>
2021-04-13 14:33:21 +0100

    refactored canvas API

--- ui/include/vlk/ui/canvas.h
+++ ui/include/vlk/ui/canvas.h
@@ -14,21 +14,21 @@ namespace ui {
 /// a reference to a specific canvas implementation. Only supports Skia (with no
 /// implementation abstraction) at the moment.
 struct Canvas {
-  explicit constexpr Canvas(SkCanvas& pimpl, Extent const& extent)
-      : pimpl_{&pimpl}, extent_{extent} {}
+  explicit constexpr Canvas(SkCanvas& canvas, Extent const& extent)
+      : canvas_addr_{&canvas}, extent_{extent} {}
 
   static constexpr Canvas from_skia(SkCanvas& pimpl, Extent const& extent) {
     return Canvas{pimpl, extent};
   }
 
   stx::Option<SkCanvas*> as_skia() const {
-    return stx::Some(static_cast<SkCanvas*>(pimpl_));
+    return stx::Some(static_cast<SkCanvas*>(canvas_addr_));
   }
 
   constexpr Extent extent() const { return extent_; }
 
  private:
-  void* pimpl_;
+  void* canvas_addr_;
   Extent extent_;
 };
 
