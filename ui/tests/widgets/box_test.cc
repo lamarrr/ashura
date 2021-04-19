7df52045749b4cbe2d024bc8bd9dd031b19e3535
Basit Ayantunde <rlamarrr@gmail.com>
2021-04-19 01:24:09 +0100

    updated box test

--- ui/tests/widgets/box_test.cc
+++ ui/tests/widgets/box_test.cc
@@ -4,15 +4,19 @@
 #include <fstream>
 #include <vector>
 #include "gtest/gtest.h"
+#include "vlk/ui/pipeline.h"
 #include "vlk/ui/tile_cache.h"
+#include "vlk/ui/widgets/text.h"
+
+#include "stx/panic.h"
 
 #include "mock_widgets.h"
 
 using namespace vlk::ui;
 using namespace vlk;
 
-
-// TODO(lamarrr): if image is not opaque and its size is preserved we can perform a direct write to the surface.
+// TODO(lamarrr): if image is not opaque and its size is preserved we can
+// perform a direct write to the surface.
 //
 // writing rgb images will be faster as it doesn't need blending
 //
@@ -34,23 +38,48 @@ struct FileImageSource : public ImageSource {
 };
 
 TEST(BoxTest, BasicTest) {
+  RasterContext context;
+
+  // TODO(lamarrr): we need to provide a children updating abstraction that
+  // would cleanup children as necessary
   //
   // TODO(lamarrr): image source instead
-  RasterContext context;
-  Box box{
-      new MockSized{Extent{1000, 1000}},
-      BoxProps()
-          .padding(Padding::all(80))
-          .border_radius(BorderRadius::all(100))
-          .border(Border::all(colors::Black, 5)),
-      BoxDecoration()
-          .image(BoxDecoration::Image::load("/home/lamar/Pictures/loba.jpg",
-                                            vlk::desc::Image2D::Format::RGBA)
-                     .unwrap())
-          .color(colors::Black.with_alpha(62))};
-  MockView v1{{&box}};
+  // we need text layout in the system
 
-  Extent screen_extent{1080, 1080};
+  MockView v1{{new Box(
+      new Text("gfx::RenderText is a stateful API - an instance of a "
+               "gfx::RenderText subclass will cache its layout information "
+               "between "
+               "draw calls. Because of this, it is often more efficient to "
+               "use the "
+               "gfx::RenderText API directly instead of using a state-less "
+               "abstraction such as the gfx::Canvas drawing calls. In "
+               "particular, "
+               "for text that changes rarely but that may be drawn "
+               "multiple times, "
+               "it is more efficient to keep an instance of "
+               "gfx::RenderText around, "
+               "so that the text layout would be performed only when the "
+               "text is "
+               "updated and not on every draw operation. Prior to the "
+               "introduction "
+               "of gfx::RenderText, this pattern was not possible, so you "
+               "may see "
+               "existing code still doing its text drawing through "
+               "gfx::Canvas text "
+               "drawing APIs (which in the past were not based on "
+               "gfx::RenderText).",
+               TextProps()
+                   // .underlined()
+                   // .italic()
+                   .align(TextAlign::Center)
+                   .color(colors::White)
+                   .font_size(16.0f)
+                   .font_family("Arial")),
+      BoxProps().padding(Padding::all(10)).border_radius(BorderRadius::all(0)),
+      BoxDecoration().color(colors::Black))}};
+
+  Extent screen_extent{3840, 2160};
 
   LayoutTree layout_tree;
   layout_tree.allot_extent(screen_extent);
@@ -67,11 +96,10 @@ TEST(BoxTest, BasicTest) {
 
   cache.resize_viewport(screen_extent);
 
-  for (size_t i = 0; i < 2000; i++) {
-    constexpr float mul = 1 / 50.0f;
+  for (size_t i = 0; i < 5; i++) {
+    constexpr float mul = 1 / 5.0f;
     cache.scroll_viewport(ViewOffset{{mul * i}, {mul * i}});
     cache.tick(std::chrono::nanoseconds(0));
-    //cache.backing_store.save_pixels_to_file("./ui_output_" +
-    // std::to_string(i));
+  //  cache.backing_store.save_pixels_to_file("./ui_output_" + std::to_string(i));
   }
 }
