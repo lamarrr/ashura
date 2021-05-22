f4f2e207ec308c310ed769cd667a3269e9c143fc
Basit Ayantunde <rlamarrr@gmail.com>
2021-05-22 01:41:43 +0100

    cleaned up raster context and added method to create textures

--- ui/include/vlk/ui/raster_context.h
+++ ui/include/vlk/ui/raster_context.h
@@ -2,46 +2,73 @@
 #pragma once
 
 #include "include/core/SkSurface.h"
+#include "include/gpu/GrDirectContext.h"
 #include "stx/option.h"
-
 #include "vlk/ui/primitives.h"
 
 namespace vlk {
 namespace ui {
 
-struct RasterContext {
-  SkBudgeted budgeted = SkBudgeted::kYes;
-  SkAlphaType alpha_type = SkAlphaType::kPremul_SkAlphaType;
-  SkColorType color_type = SkColorType::kRGBA_8888_SkColorType;
-  sk_sp<SkColorSpace> color_space = nullptr;
-  GrSurfaceOrigin const surface_origin =
-      GrSurfaceOrigin::kTopLeft_GrSurfaceOrigin;
-  enum class Target { Cpu, Gpu } target = Target::Cpu;
-  stx::Option<GrRecordingContext*> recording_context = stx::None;
+enum class RasterTarget : uint8_t { Cpu, Gpu };
 
-  sk_sp<SkSurface> create_cpu_surface(Extent const& extent) {
+struct RasterContext {
+  RasterContext(RasterTarget target = RasterTarget::Cpu,
+                stx::Option<std::shared_ptr<GrRecordingContext>>&&
+                    recording_context = stx::None,
+                SkColorType color_type = SkColorType::kRGBA_8888_SkColorType,
+                SkAlphaType alpha_type = SkAlphaType::kPremul_SkAlphaType,
+                stx::Option<sk_sp<SkColorSpace>>&& color_space = stx::None,
+                GrSurfaceOrigin surface_origin = kTopLeft_GrSurfaceOrigin,
+                SkBudgeted budgeted = SkBudgeted::kYes)
+      : target_{target},
+        recording_context_{std::move(recording_context)},
+        color_type_{color_type},
+        alpha_type_{alpha_type},
+        color_space_{std::move(color_space)},
+        surface_origin_{surface_origin},
+        budgeted_{budgeted} {
+    VLK_ENSURE(!(target_ != RasterTarget::Cpu && recording_context_.is_none()),
+               "Using GPU Target but no recording context is set");
+  }
+
+  sk_sp<SkSurface> create_cpu_surface(Extent const& extent) const {
     VLK_ENSURE(extent.visible());
-    sk_sp surface = SkSurface::MakeRaster(SkImageInfo::Make(
-        extent.width, extent.height, color_type, alpha_type, color_space));
+    sk_sp surface = SkSurface::MakeRaster(
+        SkImageInfo::Make(extent.width, extent.height, color_type_, alpha_type_,
+                          color_space_.clone().unwrap_or(nullptr)));
     VLK_ENSURE(surface != nullptr);
     return surface;
   }
 
-  sk_sp<SkSurface> create_target_surface(Extent const& extent) {
-    if (target == Target::Gpu) {
+  sk_sp<SkSurface> create_target_surface(Extent const& extent) const {
+    return create_target_texture(extent, color_type_, alpha_type_);
+  }
+
+  sk_sp<SkSurface> create_target_texture(
+      Extent const& extent, SkColorType required_color_type,
+      SkAlphaType required_alpha_type) const {
+    if (target_ == RasterTarget::Gpu) {
       VLK_ENSURE(extent.visible());
       sk_sp surface = SkSurface::MakeRenderTarget(
-          recording_context.clone().expect(
-              "Using GPU Target but no recording context is set"),
-          budgeted,
-          SkImageInfo::Make(extent.width, extent.height, color_type, alpha_type,
-                            color_space));
+          recording_context_.as_cref().unwrap().get().get(), budgeted_,
+          SkImageInfo::Make(extent.width, extent.height, required_color_type,
+                            required_alpha_type,
+                            color_space_.clone().unwrap_or(nullptr)));
       VLK_ENSURE(surface != nullptr);
       return surface;
     } else {
       return create_cpu_surface(extent);
     }
   }
+
+ private:
+  RasterTarget target_;
+  stx::Option<std::shared_ptr<GrRecordingContext>> recording_context_;
+  SkColorType color_type_;
+  SkAlphaType alpha_type_;
+  stx::Option<sk_sp<SkColorSpace>> color_space_;
+  GrSurfaceOrigin const surface_origin_;
+  SkBudgeted budgeted_ = SkBudgeted::kYes;
 };
 
 }  // namespace ui
