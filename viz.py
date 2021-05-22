2c7cb2d0031acf5454ca11eb4f01ada6db64d2c8
Basit Ayantunde <rlamarrr@gmail.com>
2021-05-22 02:25:10 +0100

    updated viz file

--- viz.py
+++ viz.py
@@ -1,11 +1,15 @@
 from matplotlib import pyplot as pyp
 import numpy as np
 import sys
-
+from PIL import Image
 
 with open(sys.argv[1]) as file:
-	pdata = np.reshape(np.array([int(c) for c in file.read().split(",")[:-1]]), (1080, 1080, 4))
+    pdata = np.reshape(
+        np.array([int(c) for c in file.read().split(",")[:-1]]), (600, 1600, 4)).astype(np.uint8)
 
 pyp.imshow(pdata)
 pyp.show()
 
+
+im = Image.fromarray(pdata)
+im.save(sys.argv[1] + '_image.png')
