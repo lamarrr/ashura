50c432e0a15bd3cb6b73a074f24bc04dc59464ea
Basit Ayantunde <rlamarrr@gmail.com>
2021-06-15 19:48:21 +0100

    made viewport immediately resolve extents and use the deferred system tick approach

--- ui/include/vlk/ui/viewport.h
+++ ui/include/vlk/ui/viewport.h
@@ -7,60 +7,85 @@
 namespace vlk {
 namespace ui {
 
-// TODO(lamarrr): scroll to widget?
-// we might need a pointer binding
-// TODO(lamarrr): friend struct tilecache or whatever?
-namespace impl {
+struct ViewportStateProxy {
+  std::function<void()> on_resize = [] {};
+  std::function<void()> on_scroll = [] {};
+};
+
 struct Viewport {
-  Viewport(Extent const& extent = Extent{1920, 1080},
-           ViewOffset const& offset = ViewOffset{Constrain{.0f},
-                                                 Constrain{.0f}},
-           ViewExtent const& widgets_allocation =
-               ViewExtent{Constrain{1.0f}, Constrain{.0f, u32_max}})
-      : extent_{extent},
-        offset_{offset},
-        widgets_allocation_{widgets_allocation},
-        on_resize_{[] {}},
-        on_scroll_{[] {}} {}
+  friend struct ViewportSystemProxy;
+
+  Viewport(Extent const extent, ViewOffset const offset,
+           ViewExtent const widgets_allocation = ViewExtent{
+               Constrain::relative(1.0f), Constrain::absolute(u32_max)}) {
+    resize(extent, widgets_allocation);
+    scroll_to(offset);
+  }
 
   Extent get_extent() const { return extent_; }
 
-  ViewOffset get_offset() const { return offset_; }
+  IOffset get_offset() const { return offset_; }
 
-  ViewExtent get_widgets_allocation() const { return widgets_allocation_; }
+  Extent get_widgets_allocation() const { return widgets_allocation_; }
 
-  void resize(Extent const& extent = Extent{1920, 1080},
-              ViewExtent const& widgets_allocation = ViewExtent{
-                  Constrain{1.0f}, Constrain{.0f, u32_max}}) {
-    extent_ = extent;
-    widgets_allocation_ = widgets_allocation;
-    on_resize_();
+  void resize(Extent const extent,
+              // give the widgets Extent{width of viewport, infinite height}
+              ViewExtent const widgets_allocation = ViewExtent{
+                  Constrain::relative(1.0f), Constrain::absolute(u32_max)}) {
+    if (extent_ != extent) {
+      extent_ = extent;
+      is_resized_ = true;
+    }
+
+    Extent const new_widgets_allocation = widgets_allocation.resolve(extent);
+    if (widgets_allocation_ != new_widgets_allocation) {
+      widgets_allocation_ = new_widgets_allocation;
+      is_resized_ = true;
+    }
   }
 
-  void scroll(ViewOffset const& offset = ViewOffset{Constrain{0.0f},
-                                                    Constrain{0.0f}}) {
-    offset_ = offset;
-    on_scroll_();
+  void scroll_to(ViewOffset const unresolved_offset) {
+    // we need to resolve immediately as scroll offset depends on the extent
+    // at that point in time
+    IOffset const new_resolved_offset = unresolved_offset.resolve(extent_);
+    if (offset_ != new_resolved_offset) {
+      offset_ = new_resolved_offset;
+      is_scrolled_ = true;
+    }
   }
-
-  std::function<void()>& get_on_resize() { return on_resize_; }
-
-  std::function<void()>& get_on_scroll() { return on_scroll_; }
 
  private:
-  // usually updated due to a resize event
+  void system_tick() {
+    if (is_resized_) {
+      state_proxy_.on_resize();
+      is_resized_ = false;
+    }
+
+    if (is_scrolled_) {
+      state_proxy_.on_scroll();
+      is_scrolled_ = false;
+    }
+  }
+
+  // updated due to a resize event
   Extent extent_;
+  Extent widgets_allocation_;
+  bool is_resized_ = true;
 
-  // usually updated due to a scrolling event
-  ViewOffset offset_;
+  // updated due to a scrolling event
+  IOffset offset_;
+  bool is_scrolled_ = true;
 
-  ViewExtent widgets_allocation_;
-
-  std::function<void()> on_resize_;
-  std::function<void()> on_scroll_;
+  ViewportStateProxy state_proxy_{};
 };
 
-}  // namespace impl
+struct ViewportSystemProxy {
+  static void tick(Viewport& viewport) { viewport.system_tick(); }
+
+  static ViewportStateProxy& get_state_proxy(Viewport& viewport) {
+    return viewport.state_proxy_;
+  }
+};
 
 }  // namespace ui
 }  // namespace vlk
