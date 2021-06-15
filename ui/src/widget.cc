f68e312c9b32e1e8b596b0351d9ee983c041d7f1
Basit Ayantunde <rlamarrr@gmail.com>
2021-06-15 19:08:20 +0100

    added widget source impl

---
+++ ui/src/widget.cc
@@ -0,0 +1,14 @@
+#include "vlk/ui/widget.h"
+
+#include "fmt/format.h"
+
+std::string vlk::ui::format(Widget const &widget) {
+  vlk::ui::WidgetDebugInfo const debug_info = widget.get_debug_info();
+  return fmt::format("Widget: {} (type hint: {}, address: {})", debug_info.name,
+                     debug_info.type_hint, (void *)&widget);
+}
+
+stx::FixedReport vlk::ui::operator>>(stx::ReportQuery,
+                                     vlk::ui::Widget const &widget) {
+  return stx::FixedReport(vlk::ui::format(widget));
+}
