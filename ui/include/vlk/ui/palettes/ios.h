d2c10d1c7bfafa7d22fddc96037d7978aff4be8b
Basit Ayantunde <rlamarrr@gmail.com>
2021-06-15 19:16:27 +0100

    added ios system colors

---
+++ ui/include/vlk/ui/palettes/ios.h
@@ -0,0 +1,92 @@
+
+#pragma once
+
+#include "vlk/ui/primitives.h"
+
+namespace vlk {
+namespace ui {
+
+// ios default system colors
+namespace ios {
+constexpr Color LightBlue = Color::from_rgb(0, 122, 255);
+constexpr Color DarkBlue = Color::from_rgb(10, 132, 255);
+constexpr Color LightBrown = Color::from_rgb(162, 132, 94);
+constexpr Color DarkBrown = Color::from_rgb(172, 142, 104);
+constexpr Color LightCyan = Color::from_rgb(50, 173, 230);
+constexpr Color DarkCyan = Color::from_rgb(100, 210, 255);
+constexpr Color LightGreen = Color::from_rgb(52, 199, 89);
+constexpr Color DarkGreen = Color::from_rgb(48, 209, 88);
+constexpr Color LightIndigo = Color::from_rgb(88, 86, 214);
+constexpr Color DarkIndigo = Color::from_rgb(94, 92, 230);
+constexpr Color LightMint = Color::from_rgb(0, 199, 190);
+constexpr Color DarkMint = Color::from_rgb(102, 212, 207);
+constexpr Color LightOrange = Color::from_rgb(255, 149, 0);
+constexpr Color DarkOrange = Color::from_rgb(255, 159, 10);
+constexpr Color LightPink = Color::from_rgb(255, 45, 85);
+constexpr Color DarkPink = Color::from_rgb(255, 55, 95);
+constexpr Color LightPurple = Color::from_rgb(175, 82, 222);
+constexpr Color DarkPurple = Color::from_rgb(191, 90, 242);
+constexpr Color LightRed = Color::from_rgb(255, 59, 48);
+constexpr Color DarkRed = Color::from_rgb(255, 69, 58);
+constexpr Color LightTeal = Color::from_rgb(48, 176, 199);
+constexpr Color DarkTeal = Color::from_rgb(64, 200, 224);
+constexpr Color LightYellow = Color::from_rgb(255, 204, 0);
+constexpr Color DarkYellow = Color::from_rgb(255, 214, 10);
+
+constexpr Color LightGray = Color::from_rgb(142, 142, 147);
+constexpr Color DarkGray = Color::from_rgb(142, 142, 147);
+constexpr Color LightGray2 = Color::from_rgb(174, 174, 178);
+constexpr Color DarkGray2 = Color::from_rgb(99, 99, 102);
+constexpr Color LightGray3 = Color::from_rgb(199, 199, 204);
+constexpr Color DarkGray3 = Color::from_rgb(72, 72, 74);
+constexpr Color LightGray4 = Color::from_rgb(209, 209, 214);
+constexpr Color DarkGray4 = Color::from_rgb(58, 58, 60);
+constexpr Color LightGray5 = Color::from_rgb(229, 229, 234);
+constexpr Color DarkGray5 = Color::from_rgb(44, 44, 46);
+constexpr Color LightGray6 = Color::from_rgb(242, 242, 247);
+constexpr Color DarkGray6 = Color::from_rgb(28, 28, 30);
+}  // namespace ios
+
+// ios accessible colors
+namespace ios_acc {
+constexpr Color LightBlue = Color::from_rgb(0, 64, 221);
+constexpr Color DarkBlue = Color::from_rgb(64, 156, 255);
+constexpr Color LightBrown = Color::from_rgb(127, 101, 69);
+constexpr Color DarkBrown = Color::from_rgb(181, 148, 105);
+constexpr Color LightCyan = Color::from_rgb(0, 113, 164);
+constexpr Color DarkCyan = Color::from_rgb(112, 215, 255);
+constexpr Color LightGreen = Color::from_rgb(36, 138, 61);
+constexpr Color DarkGreen = Color::from_rgb(48, 219, 91);
+constexpr Color LightIndigo = Color::from_rgb(54, 52, 163);
+constexpr Color DarkIndigo = Color::from_rgb(125, 122, 255);
+constexpr Color LightMint = Color::from_rgb(12, 129, 123);
+constexpr Color DarkMint = Color::from_rgb(102, 212, 207);
+constexpr Color LightOrange = Color::from_rgb(201, 52, 0);
+constexpr Color DarkOrange = Color::from_rgb(255, 179, 64);
+constexpr Color LightPink = Color::from_rgb(211, 15, 69);
+constexpr Color DarkPink = Color::from_rgb(255, 100, 130);
+constexpr Color LightPurple = Color::from_rgb(137, 68, 171);
+constexpr Color DarkPurple = Color::from_rgb(218, 143, 255);
+constexpr Color LightRed = Color::from_rgb(215, 0, 21);
+constexpr Color DarkRed = Color::from_rgb(255, 105, 97);
+constexpr Color LightTeal = Color::from_rgb(0, 130, 153);
+constexpr Color DarkTeal = Color::from_rgb(93, 230, 255);
+constexpr Color LightYellow = Color::from_rgb(178, 80, 0);
+constexpr Color DarkYellow = Color::from_rgb(255, 212, 38);
+
+constexpr Color LightGray = Color::from_rgb(108, 108, 112);
+constexpr Color DarkGray = Color::from_rgb(174, 174, 178);
+constexpr Color LightGray2 = Color::from_rgb(142, 142, 147);
+constexpr Color DarkGray2 = Color::from_rgb(124, 124, 128);
+constexpr Color LightGray3 = Color::from_rgb(174, 174, 178);
+constexpr Color DarkGray3 = Color::from_rgb(84, 84, 86);
+constexpr Color LightGray4 = Color::from_rgb(188, 188, 192);
+constexpr Color DarkGray4 = Color::from_rgb(68, 68, 70);
+constexpr Color LightGray5 = Color::from_rgb(216, 216, 220);
+constexpr Color DarkGray5 = Color::from_rgb(54, 54, 56);
+constexpr Color LightGray6 = Color::from_rgb(235, 235, 240);
+constexpr Color DarkGray6 = Color::from_rgb(36, 36, 36);
+}  // namespace ios_acc
+
+}  // namespace ui
+}  // namespace vlk
