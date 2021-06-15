2fde3843ff3a131f68e0bb5848e27ff148b48a84
Basit Ayantunde <rlamarrr@gmail.com>
2021-06-15 19:55:57 +0100

    updated tile cache for rebuilding and renamed functions and members to state their true purposes

--- ui/include/vlk/ui/tile_cache.h
+++ ui/include/vlk/ui/tile_cache.h
@@ -4,11 +4,10 @@
 #include <queue>
 #include <vector>
 
-#include "vlk/ui/impl/widget_state_proxy_accessor.h"
 #include "vlk/ui/primitives.h"
 #include "vlk/ui/raster_cache.h"
-#include "vlk/ui/raster_context.h"
 #include "vlk/ui/raster_tiles.h"
+#include "vlk/ui/render_context.h"
 #include "vlk/ui/view_tree.h"
 #include "vlk/ui/widget.h"
 
@@ -39,7 +38,6 @@ constexpr auto get_tile_region(Extent const &tile_extent, int64_t const nrows,
   // also note that the `end`s can be negative
   //
   //
-  // auto -> int64_t
   auto const [x_min, x_max, y_min, y_max] = region.bounds();
 
   int64_t const i_begin = x_min / tile_extent.width;
@@ -61,6 +59,14 @@ constexpr auto get_tile_region(Extent const &tile_extent, int64_t const nrows,
 // tree, with all of the widgets having individual effects as a result we
 // need to be able to render the effects independent of the widget, we'll
 // thus need bindings for them
+
+//! Invalidation sources:
+//!   - Viewport scroll
+//!   - Viewport resize (TODO(lamarrr): shouldn't extent only change when layout
+//!   tree becomes dirty? since tile cache depends on the extent of the layout
+//!   tree and not viewport)
+//!   - Layout tree invalidation
+//!   - View tree view_offset and screen_offset invalidation
 struct TileCache {
   // both raster and view widgets are added here. when a view widget's offset
   // are dirty, it marks its spanning raster tiles as dirty
@@ -93,8 +99,7 @@ struct TileCache {
       clip_rect = &entry.clip_rect;
     }
 
-    void draw(RasterCache &cache, IRect const &tile_screen_area,
-              AssetManager &asset_manager) const {
+    void draw(RasterCache &cache, IRect const &tile_screen_area) const {
       // use tile index and size to determine tile position on the screen and
       // use that as a translation matrix relative to the objects own position
       // on the screen
@@ -109,26 +114,25 @@ struct TileCache {
 
       IRect const widget_screen_area = IRect{*screen_offset, *extent};
 
-      VLK_DEBUG_ENSURE(IRect(tile_screen_area).overlaps(widget_screen_area));
+      VLK_ENSURE(IRect(tile_screen_area).overlaps(widget_screen_area));
 
       Canvas canvas = cache.get_recording_canvas();
-      SkCanvas *sk_canvas =
-          canvas.as_skia().expect("canvas backend is not Skia");
+      SkCanvas &sk_canvas = canvas.to_skia();
 
-      Canvas widget_canvas{*sk_canvas, widget_screen_area.extent};
+      Canvas widget_canvas{sk_canvas, widget_screen_area.extent};
 
       // backup matrix and clip state
-      sk_canvas->save();
+      sk_canvas.save();
 
       IOffset const translation =
           widget_screen_area.offset - tile_screen_area.offset;
 
-      sk_canvas->translate(translation.x, translation.y);
+      sk_canvas.translate(translation.x, translation.y);
 
       if (widget_clip_rect.visible()) {
         if (widget_clip_rect == widget_screen_area) {
           // draw without clip
-          widget->draw(widget_canvas, asset_manager);
+          widget->draw(widget_canvas);
         } else {
           // draw with clip
 
@@ -137,17 +141,17 @@ struct TileCache {
           IOffset const clip_start =
               widget_clip_rect.offset - widget_screen_area.offset;
 
-          sk_canvas->clipRect(SkRect::MakeXYWH(clip_start.x, clip_start.y,
-                                               widget_clip_rect.width(),
-                                               widget_clip_rect.height()));
-          widget->draw(widget_canvas, asset_manager);
+          sk_canvas.clipRect(SkRect::MakeXYWH(clip_start.x, clip_start.y,
+                                              widget_clip_rect.width(),
+                                              widget_clip_rect.height()));
+          widget->draw(widget_canvas);
         }
       } else {
         // draw nothing
       }
 
       // restore matrix and clip state
-      sk_canvas->restore();
+      sk_canvas.restore();
     }
   };
 
@@ -161,72 +165,76 @@ struct TileCache {
 
   ~TileCache() = default;
 
-  RasterContext const *context = nullptr;
+  // must be very small to reserve space, make 2 and increase as necessary
+  // oov means out-of-view or out-of-focus
+  // constant througout lifetime.
+  static constexpr Ticks max_oof_ticks = Ticks{1};
+  static constexpr Extent tile_extent = Extent{256, 256};
 
-  AssetManager *asset_manager = nullptr;
+  RenderContext const *context = nullptr;
 
   // entries are sorted in descending z-index order
   std::vector<Entry> entries{};
 
-  IOffset viewport_scroll_offset = IOffset{0, 0};
-  bool viewport_scrolled = true;
+  IOffset backing_store_offset = IOffset{0, 0};
+  bool backing_store_offset_changed = true;
 
-  Extent viewport_extent = Extent{1920, 1080};
-  bool viewport_resized = true;
-
-  // this is used for preloading some of the tiles
-  // constant throughout lifetime.
-  Extent focus_extension = Extent{0, 0};
-
-  // must be very small to reserve space, make 2 and increase as necessary
-  // oov means out-of-view or out-of-focus
-  // constant througout lifetime.
-  Ticks max_oof_ticks = Ticks{1};
+  Extent backing_store_extent = Extent{1920, 1080};  // non-zero
+  bool backing_store_resized = true;
 
   // accumulates the cache result of all the tiles
   // resized on viewport resize.
-  RasterCache backing_store{viewport_extent};
+  RasterCache backing_store{backing_store_extent};
 
-  RasterTiles tiles{Extent{0, 0}, Extent{256, 256}};
+  RasterTiles tiles{Extent{0, 0}, tile_extent};
+  bool tiles_extent_dirty = true;
 
   bool any_tile_dirty = true;
+
   std::vector<bool> tile_is_dirty{};
 
   std::vector<bool> tile_is_in_focus{};
 
   std::vector<Ticks> tile_oof_ticks{};
 
-  ViewTree::View const *root_view = nullptr;
+  ViewTree::View *root_view = nullptr;
 
-  // focusing helps us preload a part of the screen into the tiles
-  IRect get_focus_rect() const {
-    uint32_t const focus_x = focus_extension.width / 2;
-    uint32_t const focus_y = focus_extension.height / 2;
+  // checkerboard or clear color?
 
-    int64_t const x_min = viewport_scroll_offset.x - focus_x;
-    int64_t const y_min = viewport_scroll_offset.y - focus_y;
-
-    return IRect{IOffset{x_min, y_min},
-                 Extent{viewport_extent.width + focus_extension.width,
-                        viewport_extent.height + focus_extension.height}};
+  IRect get_backing_store_rect() const {
+    return IRect{backing_store_offset, backing_store_extent};
   }
 
-  IRect get_viewport_rect() const {
-    return IRect{viewport_scroll_offset, viewport_extent};
+  void scroll_backing_store(IOffset new_offset) {
+    if (backing_store_offset != new_offset) {
+      backing_store_offset = new_offset;
+      backing_store_offset_changed = true;
+    }
   }
 
-  void scroll_viewport(ViewOffset const &new_viewport_offset) {
-    viewport_scroll_offset = new_viewport_offset.resolve(viewport_extent);
-    viewport_scrolled = true;
+  void resize_backing_store(Extent new_extent) {
+    if (backing_store_extent != new_extent) {
+      backing_store_extent = new_extent;
+      backing_store_resized = true;
+    }
   }
 
-  void resize_viewport(Extent const &new_viewport_extent) {
-    viewport_extent = new_viewport_extent;
-    viewport_resized = true;
+  void mark_tiles_extent_dirty() { tiles_extent_dirty = true; }
+
+  void mark_all_tiles_dirty() {
+    any_tile_dirty = true;
+    for (size_t i = 0; i < tile_is_dirty.size(); i++) {
+      tile_is_dirty[i] = true;
+    }
   }
 
- public:
-  void build_from(ViewTree::View &view) {
+  void mark_backing_store_offset_dirty() {
+    backing_store_offset_changed = true;
+  }
+
+  void mark_backing_store_render_dirty() { mark_backing_store_offset_dirty(); }
+
+  void build_entries(ViewTree::View &view) {
     // insert by z-index order
     for (ViewTree::View::Entry &view_entry : view.entries) {
       auto const insert_pos =
@@ -238,21 +246,14 @@ struct TileCache {
     }
 
     for (ViewTree::View &subview : view.subviews) {
-      build_from(subview);
+      build_entries(subview);
     }
   }
 
-  void build(ViewTree &view_tree, RasterContext const &raster_context,
-             AssetManager &iasset_manager) {
-    context = &raster_context;
-
-    asset_manager = &iasset_manager;
-    root_view = &view_tree.root_view;
-
-    build_from(view_tree.root_view);
-
+  void attach_state_proxies() {
     for (Entry &entry : entries) {
-      WidgetStateProxyAccessor::access(*entry.widget).on_render_dirty =
+      // attach proxy
+      WidgetSystemProxy::get_state_proxy(*entry.widget).on_render_dirty =
           [this, &entry] {
             // note that tile binding is semi-automatic and determined by the
             // screen offset
@@ -271,30 +272,70 @@ struct TileCache {
             }
           };
     }
-
-    tick(std::chrono::nanoseconds(0));
   }
 
-  // TODO(lamarrr): implement
-  void recycle(ViewTree &view_tree) {}
+  // TODO(lamarrr): raster_context shared_ptr?, even internally? ....
+  //
+  void build(ViewTree::View &view_tree_root,
+             RenderContext const &raster_context) {
+    context = &raster_context;
 
-  // consider scroll swapping instead of allocating and de-allocating
+    entries.clear();
+
+    // scroll offset maintained, unless explicitly scrolled
+
+    backing_store_offset_changed = true;
+
+    // viewport extent maintained, unless explicitly resized
+
+    backing_store_resized =
+        true;  // forces the tiles to be resized and marked as dirty
+
+    // backing_store maintained, will be invalidated and marked as dirty in
+    // tick() due to viewport_resized and viewport_scrolled
+
+    // tiles maintained, will be resized in tick as appropriate and if necessary
+    // they will all be forcibly marked dirty due to resize and scroll of
+    // viewport
+
+    tiles_extent_dirty = true;
+
+    // any_tile_dirty maintained, will be updated in tick() to true
+    // [tiles_extent_dirty]
+
+    // tile_is_dirty maintained, will be resized and updated in tick() to all
+    // true [tiles_extent_dirty]
+
+    // tile_is_in_focus maintained, will be resized and updated in tick()
+    // [tiles_extent_dirty] tile_oof_ticks maintained, will be updated and
+    // resized in tick() [tiles_extent_dirty]
+
+    root_view = &view_tree_root;
+
+    build_entries(view_tree_root);
+    attach_state_proxies();
+  }
 
   void tick(std::chrono::nanoseconds) {
-    // TODO(lamarrr): fix and make all correct
-    // TODO(lamarrr): consider removing the focus/extent or finishing its
-    // implementation and making it correct
+    // marks that the backing store needs repainting
+    bool backing_store_dirty = false;
 
-    // for now, we'll check if any in focus tile is dirty to make the backing
-    // store dirty instead of any in viewport tile dirty
-    bool backing_store_dirty =
-        viewport_resized ||
-        viewport_scrolled;  // || any_ in viewport tile dirty;
-
-    if (viewport_resized) {
-      backing_store = RasterCache{viewport_extent};
+    if (backing_store_resized) {
+      backing_store = RasterCache{backing_store_extent};
       backing_store.init_surface(*context);
 
+      backing_store_dirty = true;
+
+      backing_store_resized = false;
+    }
+
+    if (backing_store_offset_changed) {
+      backing_store_dirty = true;
+
+      backing_store_offset_changed = false;
+    }
+
+    if (tiles_extent_dirty) {
       // layout changes, screen offsets are automatically updated as long as
       // view_tree is cleaned we therefore need to assume that all the tiles are
       // now dirty. and resize? them
@@ -309,26 +350,23 @@ struct TileCache {
 
       for (size_t i = 0; i < num_tiles; i++) {
         tile_is_dirty[i] = true;
-        tile_is_in_focus[i] = true;
-        tile_oof_ticks[i] = Ticks{max_oof_ticks};
+        tile_is_in_focus[i] = false;
+        tile_oof_ticks[i] = Ticks{0};
       }
 
-      viewport_resized = false;
+      backing_store_dirty = true;
+
+      tiles_extent_dirty = false;
     }
 
-    if (viewport_scrolled) {
-      // just marks the backing store as dirty
-      viewport_scrolled = false;
-    }
-
-    IRect const focus_rect = get_focus_rect();
+    IRect const backing_store_rect = get_backing_store_rect();
 
     for (uint32_t j = 0; j < tiles.columns(); j++) {
       for (uint32_t i = 0; i < tiles.rows(); i++) {
         IOffset const offset{i * tiles.tile_extent().width,
                              j * tiles.tile_extent().height};
         tile_is_in_focus[j * tiles.rows() + i] =
-            IRect{offset, tiles.tile_extent()}.overlaps(focus_rect);
+            IRect{offset, tiles.tile_extent()}.overlaps(backing_store_rect);
       }
     }
 
@@ -342,20 +380,38 @@ struct TileCache {
         subtile.init_surface(*context);
       }
 
-      if (tile_is_in_focus[i]) {
-        tile_oof_ticks[i].reset();
+      if (tile_is_in_focus[i] && tile_is_dirty[i]) {
         // mark the backing store as dirty
         backing_store_dirty = true;
+      }
+
+      if (tile_is_in_focus[i]) {
+        tile_oof_ticks[i].reset();
       } else {
         tile_oof_ticks[i]++;
-        if (tile_oof_ticks[i] > max_oof_ticks) {
-          // recording is always kept and not discarded
-          subtile.deinit_surface();
-        }
+      }
+
+      if (!tile_is_in_focus[i] && tile_oof_ticks[i] > max_oof_ticks) {
+        // recording is always kept and not discarded
+        // but we need to detach the surface it owns to save memory as we'll
+        // have multiple of these tiles in memory and their total number would
+        // be proportional to the total extent of the root widget
+        // TODO(lamarrr): can we have a stack of surfaces we can pop and attach
+        // to? allocating these tiles can be costly, BENCHMARK.
+        // consider scroll swapping instead of allocating and de-allocating.
+        // i.e. tile surface stack. we might need to re-allocate when the
+        // viewport zoom occurs?
+        // the stack will always have enough tile surfaces for the tiles covered
+        // by the viewport
+        subtile.deinit_surface();
+        // discard recording
       }
 
       if (any_tile_dirty) {
-        if (tile_is_dirty[i]) {
+        if (tile_is_dirty[i]) {  // && tile_is_in_focus[i], we'll still need to
+                                 // take care of any_tile_dirty and our
+                                 // dirtiness handling criteria since it is now
+                                 // not attended to immediately
           subtile.discard_recording();
           subtile.begin_recording();
         }
@@ -365,14 +421,6 @@ struct TileCache {
     // recordings are updated even when not in view
     if (any_tile_dirty) {
       for (Entry &entry : entries) {
-        // viewport affecting it?
-        // raster tile dirtiness is also affected by movement of the widgets
-        // (typically by viewport scrolling), but the viewport itself
-        // invalidates the whole area so this is solved? won't the viewport also
-        // be doing too much work? no since all its widget will be moved
-
-        // TODO(lamarrr): fix
-
         int64_t const nrows = this->tiles.rows();
         auto const [i_begin, i_end, j_begin, j_end] = get_tile_region(
             this->tiles.tile_extent(), nrows, this->tiles.columns(),
@@ -382,6 +430,9 @@ struct TileCache {
           for (int64_t i = i_begin; i < i_end; i++) {
             RasterCache &subtile = tiles.tile_at_index(i, j);
 
+            // TODO(lamarrr): notify of entering or leaving view irregardless of
+            // whether tile is dirty, we'd thus need to not first check
+            // `any_tile_dirty`
             if (tile_is_dirty[j * tiles.rows() + i]) {
               // draw to appropriate position relative to the tile size. and
               // also respect the view clipping
@@ -389,8 +440,7 @@ struct TileCache {
               IOffset tile_screen_offset =
                   IOffset{i * tile_extent.width, j * tile_extent.height};
 
-              entry.draw(subtile, IRect{tile_screen_offset, tile_extent},
-                         *asset_manager);
+              entry.draw(subtile, IRect{tile_screen_offset, tile_extent});
             }
           }
         }
@@ -401,14 +451,13 @@ struct TileCache {
       for (size_t i = 0; i < tiles.get_tiles().size(); i++) {
         RasterCache &subtile = tiles.get_tiles()[i];
 
-        if (tile_is_dirty[i]) {
+        if (tile_is_dirty[i]) {  // && tile_is_in_focus[i]
           subtile.finish_recording();
 
           // tile caches are only updated if the tile is in focus
           // we need to submit
           if (tile_oof_ticks[i] <= max_oof_ticks) {
             subtile.rasterize();
-            backing_store_dirty = true;
           }
 
           tile_is_dirty[i] = false;
@@ -419,13 +468,12 @@ struct TileCache {
     }
 
     if (backing_store_dirty) {
-      // accumulate raster cache
+      // accumulate raster cache into backing store
 
       backing_store.begin_recording();
 
       Canvas canvas = backing_store.get_recording_canvas();
-      SkCanvas *const sk_canvas =
-          canvas.as_skia().expect("canvas is not using a skia backend");
+      SkCanvas &sk_canvas = canvas.to_skia();
 
       Extent tile_extent = tiles.tile_extent();
 
@@ -436,9 +484,9 @@ struct TileCache {
               IOffset{i * tile_extent.width, j * tile_extent.height};
 
           if (IRect{tile_screen_offset, tile_extent}.overlaps(
-                  IRect{viewport_scroll_offset, viewport_extent})) {
-            subtile.write_to(*sk_canvas,
-                             tile_screen_offset - viewport_scroll_offset);
+                  backing_store_rect)) {
+            subtile.write_to(sk_canvas,
+                             tile_screen_offset - backing_store_offset);
           }
         }
       }
