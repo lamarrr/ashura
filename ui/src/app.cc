467e488d0984ed4ae68bd70ded70471a1f8c86d0
Basit Ayantunde <rlamarrr@gmail.com>
2021-07-04 00:30:04 +0100

    good cpu usage checkpoint

--- ui/src/app.cc
+++ ui/src/app.cc
@@ -1,6 +1,9 @@
 #include "vlk/ui/app.h"
 
+#include "include/gpu/vk/GrVkBackendContext.h"
+#include "include/gpu/vk/GrVkExtensions.h"
 #include "vlk/ui/pipeline.h"
+#include "vlk/ui/vk_render_context.h"
 #include "vlk/ui/vulkan.h"
 #include "vlk/ui/window.h"
 #include "vlk/ui/window_handle.h"
@@ -46,8 +49,7 @@ static constexpr char const* required_validation_layers[] = {
 App::~App() = default;
 
 void App::init() {
-  window_api = std::unique_ptr<WindowApi>{new WindowApi{}};
-  window_api->init();
+  window_api = WindowApi::init();
 
   WindowCfg win_cfg;
   win_cfg.extent = cfg.extent;
@@ -55,22 +57,20 @@ void App::init() {
   win_cfg.resizable = cfg.resizable;
   win_cfg.title = cfg.resizable;
 
-  window =
-      std::unique_ptr<Window>{new Window{Window::create(*window_api, win_cfg)}};
+  window = Window::create(window_api, win_cfg);
   std::vector window_required_instance_extensions =
-      window->handle->get_required_instance_extensions();
+      window.handle->get_required_instance_extensions();
 
   // TODO(lamarrr): check for validation layers requirement
-  vk_instance =
-      std::unique_ptr<vk::Instance>{new vk::Instance{vk::Instance::create(
-          cfg.name, VK_MAKE_VERSION(0, 0, 1), engine_cfg.name,
-          VK_MAKE_VERSION(engine_cfg.version.major, engine_cfg.version.minor,
-                          engine_cfg.version.patch),
-          window_required_instance_extensions, required_validation_layers)}};
+  vk::Instance vk_instance = vk::Instance::create(
+      cfg.name, VK_MAKE_VERSION(0, 0, 1), engine_cfg.name,
+      VK_MAKE_VERSION(engine_cfg.version.major, engine_cfg.version.minor,
+                      engine_cfg.version.patch),
+      window_required_instance_extensions, required_validation_layers);
 
-  window->attach_surface(*vk_instance);
+  window.attach_surface(vk_instance);
 
-  std::vector phys_devices = vk::PhysDevice::get_all(*vk_instance);
+  std::vector phys_devices = vk::PhysDevice::get_all(vk_instance);
 
   VkPhysicalDeviceType const device_preference[] = {
       VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU,
@@ -85,7 +85,7 @@ void App::init() {
   }
 
   vk::PhysDevice phys_device =
-      select_device(phys_devices, device_preference, window->handle->surface)
+      select_device(phys_devices, device_preference, window.handle->surface)
           .expect("Unable to find any suitable rendering device");
 
   VLK_LOG("Selected Physical Device: {}", phys_device.format());
@@ -113,20 +113,19 @@ void App::init() {
       phys_device, command_queue_create_infos, required_device_extensions,
       required_validation_layers, required_features);
 
-  vk_graphics_command_queue =
-      std::unique_ptr<vk::CommandQueue>{new vk::CommandQueue{
-          vk::CommandQueue::get(device, graphic_command_queue_family, 0)
-              .expect("Failed to create graphics command queue")}};
+  vk::CommandQueue vk_graphics_command_queue = vk::CommandQueue{
+      vk::CommandQueue::get(device, graphic_command_queue_family, 0)
+          .expect("Failed to create graphics command queue")};
 
   GrVkBackendContext sk_vk_context{};
 
   GrVkExtensions sk_extensions_cache{};
   sk_vk_context.fVkExtensions = &sk_extensions_cache;
-  sk_vk_context.fInstance = vk_instance->handle->instance;
+  sk_vk_context.fInstance = vk_instance.handle->instance;
   sk_vk_context.fPhysicalDevice = phys_device.info.phys_device;
   sk_vk_context.fDevice = device.handle->device;
-  sk_vk_context.fQueue = vk_graphics_command_queue->info.queue;
-  sk_vk_context.fGraphicsQueueIndex = vk_graphics_command_queue->info.index;
+  sk_vk_context.fQueue = vk_graphics_command_queue.info.queue;
+  sk_vk_context.fGraphicsQueueIndex = vk_graphics_command_queue.info.index;
   sk_vk_context.fMaxAPIVersion = VK_API_VERSION_1_1;
   sk_vk_context.fDeviceFeatures = &phys_device.info.features;
   // TODO(lamarrr): vk_context.fMemoryAllocator, see: GrVkAMDMemoryAllocator
@@ -151,15 +150,114 @@ void App::init() {
   sk_sp<GrDirectContext> sk_direct_context =
       GrDirectContext::MakeVulkan(sk_vk_context);
 
-  RenderContext{
-      RasterTarget::Gpu,
-      // sk_direct_context->asDirectContext()
+  VLK_ENSURE(sk_direct_context != nullptr,
+             "Unable to create Skia GPU Vulkan Context");
+  // NOTE: we use a swapchain-independent color space, color type and color
+  // info. we don't want to have to reconstruct the pipeline if for instance the
+  // monitor is changed
+  RenderContext render_context{
+      stx::Some(std::move(sk_direct_context)),
   };
 
-  // TODO(lamarrr): initial extent?, RenderContext
-  pipeline = std::unique_ptr<Pipeline>{new Pipeline{*root_widget}};
+  vk_render_context = std::shared_ptr<VkRenderContext>{new VkRenderContext{
+      std::move(vk_graphics_command_queue), std::move(render_context)}};
 
-  // resize viewport
+  window.handle->recreate_swapchain(vk_render_context);
+
+  // TODO(lamarrr): initial extent?, RenderContext
+  pipeline = std::unique_ptr<Pipeline>{
+      new Pipeline{*root_widget, vk_render_context->render_context}};
+}
+
+// TODO(lamarrr): handle should_quit
+void App::tick() {
+  auto frame_budget = frequency_to_period(present_refresh_rate_hz);
+  auto begin = std::chrono::steady_clock::now();
+  auto total_used = std::chrono::nanoseconds(0);
+
+  // TODO(lamarrr): add actual tick
+  BackingStoreDiff backing_store_diff = pipeline->tick({});
+
+  // only try to present if the pipeline has new changes or window was
+  // resized
+  if (backing_store_diff != BackingStoreDiff::None || window_extent_changed) {
+    auto present_begin = std::chrono::steady_clock::now();
+
+    // TODO(lamarrr): make presentation happen after recreation, for the first
+    // iteration. and remove the created swapchain in the init method
+
+    WindowSwapchainDiff swapchain_diff = window.handle->present_backing_store(
+        pipeline->tile_cache.backing_store_cache.get_surface_ref());
+
+    auto present_end = std::chrono::steady_clock::now();
+
+    VLK_LOG("Presented new frame to swapchain in {}ms",
+            std::chrono::duration_cast<std::chrono::milliseconds>(present_end -
+                                                                  present_begin)
+                .count());
+
+    while (swapchain_diff != WindowSwapchainDiff::None) {
+      window.handle->recreate_swapchain(vk_render_context);
+      swapchain_diff = window.handle->present_backing_store(
+          pipeline->tile_cache.backing_store_cache.get_surface_ref());
+      auto recreate_end = std::chrono::steady_clock::now();
+      VLK_LOG("Recreated swapchain in {}ms",
+              std::chrono::duration_cast<std::chrono::milliseconds>(
+                  recreate_end - present_end)
+                  .count());
+    }
+
+    // we need to update viewport in case it changed
+    pipeline->viewport.resize(
+        window.handle->extent,
+        pipeline->viewport.get_unresolved_widgets_allocation());
+
+    // resize event could have been caused by a display or DPR-setting change.
+    // we thus need to update the DPR accordingly
+    pipeline->tile_cache.update_dpr(
+        dpr_from_extents(window.handle->extent, window.handle->surface_extent));
+
+    SDL_DisplayMode display_mode{};
+    VLK_SDL_ENSURE(
+        SDL_GetWindowDisplayMode(window.handle->window, &display_mode) == 0,
+        "Unable to get window display mode");
+
+    // TODO(lamarrr): log refresh rate
+    present_refresh_rate_hz = static_cast<uint32_t>(display_mode.refresh_rate);
+
+    window_extent_changed = false;
+
+    auto render_end = std::chrono::steady_clock::now();
+
+    total_used = render_end - begin;
+  }
+
+  // poll events irregardless to make the window not be marked as unresponsive.
+  //
+  // any missed event should be rolled over to the next tick()
+  do {
+  } while (window_api.poll_events());
+  // TODO(lamarrr): we are still missing events
+
+  // this would mean the pipeline needs to store the states and process and
+  // dispatch them on the next tick()
+  pipeline->dispatch_events(window.handle->event_queue.mouse_button_events,
+                            window.handle->event_queue.window_events);
+
+  window_extent_changed =
+      any_eq(window.handle->event_queue.window_events, WindowEvent::Resized);
+
+  if (any_eq(window.handle->event_queue.window_events, WindowEvent::Close)) {
+    std::exit(0);
+  }
+
+  window.handle->event_queue.clear();
+
+  total_used = std::chrono::steady_clock::now() - begin;
+
+  if (total_used < frame_budget) {
+    std::this_thread::sleep_for(frame_budget - total_used);
+  }
 }
 
 }  // namespace ui
