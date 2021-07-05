930466c2a4835ed2be04257bfb654e4e2a92cfd6
Basit Ayantunde <rlamarrr@gmail.com>
2021-07-05 07:13:23 +0100

    added any_eq helper

--- utils/include/vlk/utils/utils.h
+++ utils/include/vlk/utils/utils.h
@@ -118,6 +118,12 @@ constexpr bool any_true(Container const &cont) {
                        [](auto const &value) -> bool { return value; });
 }
 
+template <typename Container, typename Value>
+constexpr bool any_eq(Container const &cont, Value &&value) {
+  return ::std::find(cont.begin(), cont.end(), static_cast<Value &&>(value)) !=
+         cont.end();
+}
+
 template <typename Container, typename UnaryPredicate>
 constexpr bool any(Container const &cont, UnaryPredicate &&predicate) {
   return ::std::any_of(cont.begin(), cont.end(),
