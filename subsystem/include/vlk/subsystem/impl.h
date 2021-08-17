70bcfc15996a10bf55251d24af3fec8eb448b436
Basit Ayantunde <rlamarrr@gmail.com>
2021-08-17 20:38:42 +0100

    updated susbystem implementation interface

--- subsystem/include/vlk/subsystem/impl.h
+++ subsystem/include/vlk/subsystem/impl.h
@@ -3,8 +3,6 @@
 #include <chrono>
 
 #include "stx/async.h"
-#include "stx/mem.h"
-#include "stx/span.h"
 #include "vlk/subsystem.h"
 
 namespace vlk {
@@ -13,9 +11,10 @@ struct SubsystemsContext;
 
 struct SubsystemImpl : public Subsystem {
   virtual stx::FutureAny get_future() = 0;
-  virtual void link(SubsystemsContext const&);
+  // called after all futures have been added
+  virtual void link(SubsystemsContext const&) = 0;
+  // called on every frame
   virtual void tick(std::chrono::nanoseconds) = 0;
-  virtual ~SubsystemImpl() = 0;
 };
 
 };  // namespace vlk
