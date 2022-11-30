a5f15545bc328fe718c5a94432ffefbe9c1cac1f
lamarrr <rlamarrr@gmail.com>
2022-11-30 01:08:38 +0000

    added sdl_main for windows

--- ashura/src/ashura.cc
+++ ashura/src/ashura.cc
@@ -1,7 +1,13 @@
 #include "ashura/app.h"
 
+#if STX_CFG(OS, WINDOWS)
+#define main SDL_main
+#endif
+
 int main() {
   asr::App app{asr::AppConfig{.enable_validation_layers = true}};
 
   app.tick({});
+
+  return 0;
 }
