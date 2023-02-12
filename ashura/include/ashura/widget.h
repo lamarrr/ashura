2493c2066e4414a9e4456f8b632940d24ff4053c
lamarrr <rlamarrr@gmail.com>
2023-02-12 07:12:30 +0000

    updated widget struct

--- ashura/include/ashura/widget.h
+++ ashura/include/ashura/widget.h
@@ -41,6 +41,10 @@ enum class CrossAlign : u8 { Start, End, Center, Stretch };
 
 enum class Fit : u8 { Shrink, Expand };
 
+struct layout {
+  constraint x, y, width, height;
+};
+
 struct WidgetInfo {
   std::string_view id;
   std::string_view type;
@@ -54,170 +58,179 @@ struct WidgetContext {};
 struct WidgetImpl;
 
 /// SEE: (https://www.w3.org/TR/uievents)
+//
+// TODO(lamarrr): should we go recursive?
+//
 struct Widget {
-  virtual ~Widget() {}
-  //
-  virtual stx::Span<WidgetImpl const> get_children() { return {}; }
+  constexpr Widget() {}
+
+  STX_MAKE_PINNED(Widget)
+
+  constexpr virtual ~Widget() {}
 
   //
-  virtual WidgetInfo get_debug_info() { return {}; }
+  constexpr virtual stx::Span<WidgetImpl const> get_children() { return {}; }
 
   //
-  virtual Visibility get_visibility() { return Visibility::Shown; }
+  constexpr virtual WidgetInfo get_debug_info() { return {}; }
 
   //
-  virtual stx::Option<i64> get_z_index() { return stx::None; }
+  constexpr virtual Visibility get_visibility() { return Visibility::Shown; }
 
-  // + handling floating, relative, sticky and fixed positioned elements
-  // min, max, available
-  virtual rect layout(rect target, stx::Span<rect> children_allocation) {
-    (void)target;
-    (void)children_allocation;
-    return {};
-  }
+  //
+  constexpr virtual stx::Option<i64> get_z_index() { return stx::None; }
+
+  //
+  constexpr virtual ash::layout layout(rect area) { return {}; }
 
   // children layout??? or do we entirely allow self layout
   // + other properties that will make its rendering work well with other
   // widgets
-  virtual void draw(gfx::Canvas &canvas, rect area) {
+  constexpr virtual void draw(gfx::Canvas &canvas, rect area) {
     (void)canvas;
     (void)area;
   }
 
   // called before children are drawn
-  virtual void pre_effect(gfx::Canvas &canvas, rect area, usize child) {
+  constexpr virtual void pre_draw(gfx::Canvas &canvas, rect area, usize child) {
     (void)canvas;
     (void)area;
     (void)child;
   }
 
   // called once children are drawn
-  virtual void post_effect(gfx::Canvas &canvas, rect area, usize child) {
+  constexpr virtual void post_draw(gfx::Canvas &canvas, rect area,
+                                   usize child) {
     (void)canvas;
     (void)area;
     (void)child;
   }
 
   //
-  virtual void tick(WidgetContext &context, std::chrono::nanoseconds interval) {
+  constexpr virtual void tick(WidgetContext &context,
+                              std::chrono::nanoseconds interval) {
     (void)context;
     (void)interval;
   }
 
   //
-  virtual void on_launch() {}
+  constexpr virtual void on_launch() {}
 
   //
-  virtual void on_enter_viewport() {}
+  constexpr virtual void on_enter_viewport() {}
 
   //
-  virtual void on_leave_viewport() {}
+  constexpr virtual void on_leave_viewport() {}
 
   //
-  virtual void on_click(MouseButton button, KeyModifiers modifiers,
-                        vec2 position) {}
+  constexpr virtual void on_click(MouseButton button, KeyModifiers modifiers,
+                                  vec2 position) {}
 
   //
-  virtual void on_double_click(MouseButton button, KeyModifiers modifiers,
-                               vec2 position) {}
+  constexpr virtual void on_double_click(MouseButton button,
+                                         KeyModifiers modifiers,
+                                         vec2 position) {}
 
   //
-  virtual void on_mouse_scroll(KeyModifiers modifiers, vec2 translation,
-                               vec2 previous_position, vec2 current_position) {}
+  constexpr virtual void on_mouse_scroll(KeyModifiers modifiers,
+                                         vec2 translation,
+                                         vec2 previous_position,
+                                         vec2 current_position) {}
 
   //
-  virtual void on_mouse_move(KeyModifiers modifiers, vec2 previous_position,
-                             vec2 current_position) {}
+  constexpr virtual void on_mouse_move(KeyModifiers modifiers,
+                                       vec2 previous_position,
+                                       vec2 current_position) {}
 
   //
-  virtual void on_hover(KeyModifiers modifiers, vec2 position) {}
+  constexpr virtual void on_hover(KeyModifiers modifiers, vec2 position) {}
 
   //
-  virtual void on_mouse_down(KeyModifiers modifiers, vec2 position) {}
+ constexpr virtual void on_mouse_down(KeyModifiers modifiers, vec2 position) {}
 
   //
-  virtual void on_mouse_up(KeyModifiers modifiers) {}
+  constexpr virtual void on_mouse_up(KeyModifiers modifiers) {}
 
   //
-  virtual void on_mouse_enter(KeyModifiers modifiers) {}
+  constexpr virtual void on_mouse_enter(KeyModifiers modifiers) {}
 
   //
-  virtual void on_mouse_leave(KeyModifiers modifiers) {}
+  constexpr virtual void on_mouse_leave(KeyModifiers modifiers) {}
 
   //
-  virtual void on_mouse_out(KeyModifiers modifiers) {}
+  constexpr virtual void on_mouse_out(KeyModifiers modifiers) {}
 
   //
-  virtual void on_mouse_over(KeyModifiers modifiers) {}
+  constexpr virtual void on_mouse_over(KeyModifiers modifiers) {}
 
   //
-  virtual void on_enter(KeyModifiers modifiers) {}
+  constexpr virtual void on_enter(KeyModifiers modifiers) {}
 
   //
-  virtual void on_tap() {}
+  constexpr virtual void on_tap() {}
 
   //
-  virtual void on_drag() {}
+  constexpr virtual void on_drag() {}
 
   //
-  virtual void on_drag_start() {}
+  constexpr virtual void on_drag_start() {}
 
   //
-  virtual void on_drag_end() {}
+  constexpr virtual void on_drag_end() {}
 
   //
-  virtual void on_drag_enter() {}
+  constexpr virtual void on_drag_enter() {}
 
   //
-  virtual void on_drag_leave() {}
+  constexpr virtual void on_drag_leave() {}
 
   //
-  virtual void on_drag_over() {}
+  constexpr virtual void on_drag_over() {}
 
   //
-  virtual void on_drop() {}
+  constexpr virtual void on_drop() {}
 
   //
-  virtual void on_touch_cancel() {}
+  constexpr virtual void on_touch_cancel() {}
 
   //
-  virtual void on_touch_end() {}
+  constexpr virtual void on_touch_end() {}
 
   //
-  virtual void on_touch_move() {}
+  constexpr virtual void on_touch_move() {}
 
   //
-  virtual void on_touch_start() {}
+  constexpr virtual void on_touch_start() {}
 
   //
-  virtual void on_touch_enter() {}
+  constexpr virtual void on_touch_enter() {}
 
   //
-  virtual void on_touch_leave() {}
+  constexpr virtual void on_touch_leave() {}
 
   //
-  virtual void on_focus() {}
+  constexpr virtual void on_focus() {}
 
   //
-  virtual void on_focus_in() {}
+  constexpr virtual void on_focus_in() {}
 
   //
-  virtual void on_focus_out() {}
+  constexpr virtual void on_focus_out() {}
 
   //
-  virtual void on_select() {}
+  constexpr virtual void on_select() {}
 
   //
-  virtual void on_key_down(KeyModifiers modifiers) {}
+  constexpr virtual void on_key_down(KeyModifiers modifiers) {}
 
   //
-  virtual void on_key_up(KeyModifiers modifiers) {}
+  constexpr virtual void on_key_up(KeyModifiers modifiers) {}
 
   //
-  virtual void on_keypressed(KeyModifiers modifiers) {}
+  constexpr virtual void on_keypressed(KeyModifiers modifiers) {}
 
   //
-  virtual void on_input(/*data*/) {}  // <- input widget
+  constexpr virtual void on_input(/*data*/) {}  // <- input widget
 
   // TODO(lamarrr): can this be simpler?
   // virtual void raise_tooltip(){}
