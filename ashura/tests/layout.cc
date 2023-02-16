a5d923b4a7aed4877e13a0b8b26e59e1fbe491c2
lamarrr <rlamarrr@gmail.com>
2023-02-16 23:56:43 +0000

    added spacearound and spaceevenly tests

--- ashura/tests/layout.cc
+++ ashura/tests/layout.cc
@@ -55,10 +55,49 @@ TEST(ImageTest, SerializationDeserialization) {
   EXPECT_EQ(im.props.border_radius, (vec4{1, 2, 3, 4}));
 }
 
-  std::variant<int, float> x;
+TEST(LayoutTest, SpaceAround) {
+  Flex flex{FlexProps{.direction = Direction::Row,
+                      .main_align = MainAlign::SpaceAround,
+                      .cross_align = CrossAlign::Center,
+                      .width = constraint{.scale = 1},
+                      .height = constraint{.scale = 1}},
+            Widget{},
+            Image{ImageProps{.width = constraint{.bias = 100},
+                             .height = constraint{.bias = 100}}},
+            Image{ImageProps{.width = constraint{.bias = 200},
+                             .height = constraint{.bias = 200}}}};
+
+  perform_layout(&flex,
+                 rect{.offset = vec2{50, 50}, .extent = vec2{1920, 1080}});
+
+  f32 space = (1920 - 300) / 6;
+  f32 x = 50;
+
+  EXPECT_EQ(flex.area.offset.x, x);
+  EXPECT_EQ(flex.area.offset.y, 50);
+  EXPECT_EQ(flex.area.extent.x, 1920);
+  EXPECT_EQ(flex.area.extent.y, 1080);
+
+  x = x + space;
+  EXPECT_EQ(flex.get_children()[0]->area.offset.x, x);
+  EXPECT_EQ(flex.get_children()[0]->area.offset.y, 150);
+  EXPECT_EQ(flex.get_children()[0]->area.extent.x, 0);
+  EXPECT_EQ(flex.get_children()[0]->area.extent.y, 0);
+
+  x = x + 0 + space + space;
+  EXPECT_EQ(flex.get_children()[1]->area.offset.x, x);
+  EXPECT_EQ(flex.get_children()[1]->area.offset.y, 100);
+  EXPECT_EQ(flex.get_children()[1]->area.extent.x, 100);
+  EXPECT_EQ(flex.get_children()[1]->area.extent.y, 100);
+
+  x = x + 100 + space + space;
+  EXPECT_EQ(flex.get_children()[2]->area.offset.x, x);
+  EXPECT_EQ(flex.get_children()[2]->area.offset.y, 50);
+  EXPECT_EQ(flex.get_children()[2]->area.extent.x, 200);
+  EXPECT_EQ(flex.get_children()[2]->area.extent.y, 200);
 }
 
-TEST(LayoutTest, Space) {
+TEST(LayoutTest, SpaceEvenly) {
   Flex flex{FlexProps{.direction = Direction::Row,
                       .main_align = MainAlign::SpaceEvenly,
                       .cross_align = CrossAlign::Center,
@@ -70,26 +109,72 @@ TEST(LayoutTest, Space) {
             Image{ImageProps{.width = constraint{.bias = 200},
                              .height = constraint{.bias = 200}}}};
 
-  perform_layout(WidgetImpl::make(&flex),
+  perform_layout(&flex,
                  rect{.offset = vec2{50, 50}, .extent = vec2{1920, 1080}});
 
-  EXPECT_EQ(flex.area.offset.x, 50);
+  f32 x = 50;
+  f32 space = (1920 - 300) / 4.0f;
+
+  EXPECT_EQ(flex.area.offset.x, x);
   EXPECT_EQ(flex.area.offset.y, 50);
   EXPECT_EQ(flex.area.extent.x, 1920);
   EXPECT_EQ(flex.area.extent.y, 1080);
 
-  EXPECT_EQ(flex.get_children()[0]->area.offset.x, (1920 - 300) / 4.0f + 50);
+  x = x + space;
+  EXPECT_EQ(flex.get_children()[0]->area.offset.x, x);
   EXPECT_EQ(flex.get_children()[0]->area.offset.y, 150);
   EXPECT_EQ(flex.get_children()[0]->area.extent.x, 0);
   EXPECT_EQ(flex.get_children()[0]->area.extent.y, 0);
 
-  EXPECT_EQ(flex.get_children()[1]->area.offset.x, (1920 - 300) / 2.0f + 50);
+  x = x + 0 + space;
+  EXPECT_EQ(flex.get_children()[1]->area.offset.x, x);
   EXPECT_EQ(flex.get_children()[1]->area.offset.y, 100);
   EXPECT_EQ(flex.get_children()[1]->area.extent.x, 100);
   EXPECT_EQ(flex.get_children()[1]->area.extent.y, 100);
 
-  EXPECT_EQ(flex.get_children()[2]->area.offset.x,
-            ((1920 - 300) / 4.0f) * 3 + 150);
+  x = x + 100 + space;
+  EXPECT_EQ(flex.get_children()[2]->area.offset.x, x);
+  EXPECT_EQ(flex.get_children()[2]->area.offset.y, 50);
+  EXPECT_EQ(flex.get_children()[2]->area.extent.x, 200);
+  EXPECT_EQ(flex.get_children()[2]->area.extent.y, 200);
+}
+
+TEST(LayoutTest, SpaceBetween) {
+  Flex flex{FlexProps{.direction = Direction::Row,
+                      .main_align = MainAlign::SpaceBetween,
+                      .cross_align = CrossAlign::Center,
+                      .width = constraint{.scale = 1},
+                      .height = constraint{.scale = 1}},
+            Widget{},
+            Image{ImageProps{.width = constraint{.bias = 100},
+                             .height = constraint{.bias = 100}}},
+            Image{ImageProps{.width = constraint{.bias = 200},
+                             .height = constraint{.bias = 200}}}};
+
+  perform_layout(&flex,
+                 rect{.offset = vec2{50, 50}, .extent = vec2{1920, 1080}});
+
+  f32 x = 50;
+  f32 space = (1920 - 300) / 2;
+
+  EXPECT_EQ(flex.area.offset.x, x);
+  EXPECT_EQ(flex.area.offset.y, 50);
+  EXPECT_EQ(flex.area.extent.x, 1920);
+  EXPECT_EQ(flex.area.extent.y, 1080);
+
+  EXPECT_EQ(flex.get_children()[0]->area.offset.x, x);
+  EXPECT_EQ(flex.get_children()[0]->area.offset.y, 150);
+  EXPECT_EQ(flex.get_children()[0]->area.extent.x, 0);
+  EXPECT_EQ(flex.get_children()[0]->area.extent.y, 0);
+
+  x = x + space;
+  EXPECT_EQ(flex.get_children()[1]->area.offset.x, x);
+  EXPECT_EQ(flex.get_children()[1]->area.offset.y, 100);
+  EXPECT_EQ(flex.get_children()[1]->area.extent.x, 100);
+  EXPECT_EQ(flex.get_children()[1]->area.extent.y, 100);
+
+  x = x + 100 + space;
+  EXPECT_EQ(flex.get_children()[2]->area.offset.x, x);
   EXPECT_EQ(flex.get_children()[2]->area.offset.y, 50);
   EXPECT_EQ(flex.get_children()[2]->area.extent.x, 200);
   EXPECT_EQ(flex.get_children()[2]->area.extent.y, 200);
