447652f9358ffeea86cb708f42b3e9cb6ed13744
lamarrr <rlamarrr@gmail.com>
2023-02-18 06:10:19 +0000

    minor refactoring

--- ashura/include/ashura/asset_bundle.h
+++ ashura/include/ashura/asset_bundle.h
@@ -15,29 +15,29 @@ enum class AssetBundleError : u8 { InvalidId };
 template <typename T>
 struct AssetBundle {
   u64 add(T&& asset) {
-    u64 id = next_;
-    next_++;
-    data_.emplace(id, std::move(asset));
+    u64 id = next_id;
+    next_id++;
+    data.emplace(id, std::move(asset));
     return id;
   }
 
   stx::Result<stx::Void, AssetBundleError> remove(u64 asset) {
-    auto it = data_.find(asset);
-    if (it == data_.end()) return stx::Err(AssetBundleError::InvalidId);
-    data_.erase(it);
+    auto it = data.find(asset);
+    if (it == data.end()) return stx::Err(AssetBundleError::InvalidId);
+    data.erase(it);
     return stx::Ok(stx::Void{});
   }
 
   stx::Result<T const*, AssetBundleError> get(u64 asset) const {
-    auto it = data_.find(asset);
-    if (it == data_.end()) return stx::Err(AssetBundleError::InvalidId);
+    auto it = data.find(asset);
+    if (it == data.end()) return stx::Err(AssetBundleError::InvalidId);
     return stx::Ok(&it->second);
   }
 
-  bool has(u64 asset) const { return data_.find(asset) != data_.end(); }
+  bool has(u64 asset) const { return data.find(asset) != data.end(); }
 
-  std::map<u64, T> data_;
-  u64 next_ = 0;
+  std::map<u64, T> data;
+  u64 next_id = 0;
 };
 
 }  // namespace ash
