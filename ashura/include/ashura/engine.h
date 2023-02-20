67b393b74c32cdfb201925ebd599a68ca20c1c26
lamarrr <rlamarrr@gmail.com>
2023-02-20 03:50:48 +0000

    minor code refactoring

--- ashura/include/ashura/engine.h
+++ ashura/include/ashura/engine.h
@@ -5,26 +5,34 @@
 #include "ashura/app_config.h"
 #include "ashura/asset_bundle.h"
 #include "ashura/canvas.h"
+#include "ashura/plugins/vulkan_image_bundle.h"
 #include "ashura/version.h"
 #include "ashura/vulkan.h"
+#include "ashura/vulkan_canvas_renderer.h"
 #include "ashura/window.h"
 #include "ashura/window_api.h"
 #include "spdlog/logger.h"
 #include "stx/rc.h"
 #include "stx/string.h"
-#include "ashura/vulkan_canvas_renderer.h"
+
 
 namespace ash {
 
 struct Engine {
   Engine(AppConfig const& cfg);
 
+  ~Engine(){
+    renderer.destroy();
+    upload_context.destroy();
+  }
+
   stx::Option<stx::Rc<spdlog::logger*>> logger;
   stx::Option<stx::Rc<WindowApi*>> window_api;
   stx::Option<stx::Rc<Window*>> window;
   stx::Option<stx::Rc<vk::CommandQueue*>> queue;
-  stx::Option<gfx::Canvas> canvas;
-  stx::Option<stx::Rc<vk::CanvasRenderer*>> renderer;
+  gfx::Canvas canvas;
+  vk::CanvasRenderer renderer;
+  vk::UploadContext upload_context;
   AssetBundle<stx::Rc<vk::ImageResource*>> image_bundle;
 
   // asset manager
