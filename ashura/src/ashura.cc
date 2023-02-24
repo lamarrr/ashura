81a667df690f73c488d60e7e8eea1cace856a46b
lamarrr <rlamarrr@gmail.com>
2023-02-24 05:40:34 +0000

    updated app

--- ashura/src/ashura.cc
+++ ashura/src/ashura.cc
@@ -1,14 +1,28 @@
 #define SDL_MAIN_HANDLED
 #include "SDL2/SDL.h"
 #include "ashura/app.h"
+#include "ashura/widget.h"
+#include "ashura/widgets/image.h"
 
 int main(int, char*[]) {
   SDL_SetMainReady();
   ash::AppConfig cfg{.enable_validation_layers = true};
   cfg.window_config.borderless = false;
-  ash::App app{std::move(cfg)};
-
-  while (true) app.tick({});
+  ash::App app{
+      std::move(cfg),
+      new ash::Image{ash::ImageProps{
+          .source =
+              ash::FileImageSource{
+                  .path = stx::string::make_static(
+                      R"(C:\Users\Basit\OneDrive\Desktop\xxval.jpg)")},
+          .resize_on_load = true}}};
+  std::chrono::steady_clock::time_point last_tick =
+      std::chrono::steady_clock::now();
+  while (true) {
+    auto present = std::chrono::steady_clock::now();
+    app.tick(present - last_tick);
+    last_tick = present;
+  }
 
   return 0;
 }
