22174aa9096fc189260c3e491291fc0d1f19b606
Basit Ayantunde <rlamarrr@gmail.com>
2023-02-26 07:51:41 +0000

    removed file path hardcoding

--- ashura/src/ashura.cc
+++ ashura/src/ashura.cc
@@ -3,19 +3,19 @@
 #include "ashura/widget.h"
 #include "ashura/widgets/image.h"
 
-int main(int, char*[]) {
+int main(int argc, char* argv[]) {
+  ASH_CHECK(argc == 2);
   // SDL_SetMainReady();
   ash::AppConfig cfg{.enable_validation_layers = false};
   cfg.window_config.borderless = false;
-  ash::App app{std::move(cfg),
-               new ash::Image{ash::ImageProps{
-                   .source =
-                       ash::FileImageSource{
-                           .path = stx::string::make_static(
-                               R"(/home/lamar/Desktop/eren.jpg)")},
-                   .border_radius = ash::vec4{200, 200, 200, 200},
-                   .aspect_ratio = stx::Some(1.0f),
-                   .resize_on_load = true}}};
+  ash::App app{
+      std::move(cfg),
+      new ash::Image{ash::ImageProps{
+          .source =
+              ash::FileImageSource{.path = stx::string::make_static(argv[1])},
+          .border_radius = ash::vec4{200, 200, 200, 200},
+          .aspect_ratio = stx::Some(1.0f),
+          .resize_on_load = true}}};
   std::chrono::steady_clock::time_point last_tick =
       std::chrono::steady_clock::now();
   while (true) {
