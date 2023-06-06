ac7d6dc1dc1e478b70bf8409a3b9bf13ebc76914
Basit Ayantunde <rlamarrr@gmail.com>
2023-06-07 00:39:34 +0100

    update

--- ashura/src/ashura.cc
+++ ashura/src/ashura.cc
@@ -1573,8 +1573,8 @@ int main(int argc, char **argv)
   FontSpec fonts[] = {
       {.name = "Roboto", .path = R"(C:\Users\Basit\Documents\workspace\oss\ashura\assets\fonts\Roboto\Roboto-Regular.ttf)", .stroke_thickness = 2.5},
       {.name = "Arabic", .path = R"(C:\Users\Basit\Desktop\IBM_Plex_Sans_Arabic\IBMPlexSansArabic-Regular.ttf)", .stroke_thickness = 2.5},
-      {.name = "JP", .path = R"(C:\Users\Basit\Desktop\Noto_Sans_HK\NotoSansHK-Regular.otf)", .stroke_thickness = 2.5,.max_atlas_extent = {8000,8000}},
-      };
+      {.name = "JP", .path = R"(C:\Users\Basit\Desktop\Noto_Sans_HK\NotoSansHK-Regular.otf)", .stroke_thickness = 2.5, .max_atlas_extent = {8000, 8000}},
+      {.name = "MaterialIcons", .path = R"(C:\Users\Basit\Documents\workspace\oss\ashura\assets\fonts\MaterialIcons\MaterialIcons-Regular.ttf)", .stroke_thickness = 0}};
 
   AppConfig cfg{.enable_validation_layers = false, .fonts = fonts};
 
