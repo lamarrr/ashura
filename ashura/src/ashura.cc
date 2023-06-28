efd45645180c044f7ab46e68610bc46fc9d25928
Basit Ayantunde <rlamarrr@gmail.com>
2023-06-29 00:34:05 +0100

    update

--- ashura/src/ashura.cc
+++ ashura/src/ashura.cc
@@ -3,6 +3,7 @@
 #include "ashura/text.h"
 #include "ashura/uuid.h"
 #include "ashura/widget.h"
+#include "ashura/widget_tree.h"
 #include "ashura/widgets/box.h"
 #include "ashura/widgets/checkbox.h"
 #include "ashura/widgets/flex.h"
@@ -26,30 +27,26 @@ int main(int argc, char **argv)
   AppConfig cfg{.enable_validation_layers = false, .fonts = fonts};
 
   RadioState state{8};
-  App          app{std::move(cfg),
-          FlexBox{FlexBoxProps{},
-                  CheckBox{},
-                  Slider{},
-                   Box{BoxProps{.width            = constraint::absolute(200),
-                                         .height           = constraint::absolute(200),
-                                         .background_color = colors::WHITE,
-                                         .border_thickness = 2,
-                                         .border_color     = colors::GREEN,
-                                         .border_radius    = {5, 5, 5, 5}},
-                       Text{"Click Me!", TextProps{.foreground_color = colors::RED}}},
-                  Radio(5, state),
-                  Radio(6, state),
-                  Radio(8, state),/*
-                  Text{"verified", TextProps{.font = "MaterialIcons", .foreground_color = colors::YELLOW}},
-                  Text{R"(Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum)", TextProps{.font = "Roboto", .foreground_color = material::BLUE_500, .background_color = material::GRAY_100}},
-                  Text{R"(I didn't wanna say anything, but this game seems lame)", TextProps{.font = "Roboto", .font_height = 30, .foreground_color = material::BLUE_500, .background_color = material::GRAY_100}},
-                  Text{"explicit", TextProps{.font = "MaterialIcons", .foreground_color = colors::GREEN}},
-              */    Image{ImageProps{
-                               .source         = FileImageSource{.path = R"(C:\Users\Basit\Desktop\wallpaperflare.com_wallpaper.jpg)"},
-                               .border_radius  = vec4{20, 20, 20, 20},
-                               .aspect_ratio   = stx::Some(2.0f),
-                               .resize_on_load = true}}}};
-  timepoint    last_tick = Clock::now();
+  App        app{std::move(cfg),
+          Flex{FlexProps{},
+               CheckBox{},
+               Slider{},
+               Box{BoxProps{.width            = constraint::absolute(200),
+                                   .height           = constraint::absolute(200),
+                                   .background_color = colors::WHITE,
+                                   .border_thickness = 2,
+                                   .border_color     = colors::GREEN,
+                                   .border_radius    = {5, 5, 5, 5}},
+                   Text{"Click Me!", TextProps{.foreground_color = colors::RED}}},
+               Radio(5, state),
+               Radio(6, state),
+               Radio(8, state), /*
+                Text{"verified", TextProps{.font = "MaterialIcons", .foreground_color = colors::YELLOW}},
+                Text{R"(Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum)", TextProps{.font = "Roboto", .foreground_color = material::BLUE_500, .background_color = material::GRAY_100}},
+                Text{R"(I didn't wanna say anything, but this game seems lame)", TextProps{.font = "Roboto", .font_height = 30, .foreground_color = material::BLUE_500, .background_color = material::GRAY_100}},
+                Text{"explicit", TextProps{.font = "MaterialIcons", .foreground_color = colors::GREEN}},
+            */ Image{ImageProps{.source = FileImageSource{.path = R"(C:\Users\Basit\Desktop\wallpaperflare.com_wallpaper.jpg)"}, .border_radius = vec4{20, 20, 20, 20}, .aspect_ratio = stx::Some(2.0f), .resize_on_load = true}}}};
+  timepoint  last_tick = Clock::now();
   while (true)
   {
     timepoint present = Clock::now();
