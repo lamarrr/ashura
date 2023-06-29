88a29ece607adfad64188eaedf81cf21a4a2bb12
Basit Ayantunde <rlamarrr@gmail.com>
2023-06-29 14:59:20 +0100

    update

--- ashura/src/ashura.cc
+++ ashura/src/ashura.cc
@@ -20,9 +20,9 @@ int main(int argc, char **argv)
   ASH_CHECK(SDL_Init(SDL_INIT_EVERYTHING) == 0);
 
   FontSpec fonts[] = {
-      {.name = "Roboto", .path = R"(C:\Users\Basit\Documents\workspace\oss\ashura\assets\fonts\Roboto\Roboto-Regular.ttf)", .stroke_thickness = 2.5},
-      {.name = "RobotoMono", .path = R"(C:\Users\Basit\Documents\workspace\oss\ashura\assets\fonts\RobotoMono\RobotoMono-Regular.ttf)", .stroke_thickness = 2.5},
-      {.name = "MaterialIcons", .path = R"(C:\Users\Basit\Documents\workspace\oss\ashura\assets\fonts\MaterialIcons\MaterialIcons-Regular.ttf)", .stroke_thickness = 0}};
+      {.name = "Roboto", .path = R"(C:\Users\Basit\Documents\workspace\oss\ashura\ashura\assets\fonts\Roboto\Roboto-Regular.ttf)", .stroke_thickness = 2.5},
+      {.name = "RobotoMono", .path = R"(C:\Users\Basit\Documents\workspace\oss\ashura\ashura\assets\fonts\RobotoMono\RobotoMono-Regular.ttf)", .stroke_thickness = 2.5},
+      {.name = "MaterialIcons", .path = R"(C:\Users\Basit\Documents\workspace\oss\ashura\ashura\assets\fonts\MaterialIcons\MaterialIcons-Regular.ttf)", .stroke_thickness = 0}};
 
   AppConfig cfg{.enable_validation_layers = false, .fonts = fonts};
 
@@ -40,12 +40,12 @@ int main(int argc, char **argv)
                    Text{"Click Me!", TextProps{.foreground_color = colors::RED}}},
                Radio(5, state),
                Radio(6, state),
-               Radio(8, state), /*
-                Text{"verified", TextProps{.font = "MaterialIcons", .foreground_color = colors::YELLOW}},
-                Text{R"(Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum)", TextProps{.font = "Roboto", .foreground_color = material::BLUE_500, .background_color = material::GRAY_100}},
-                Text{R"(I didn't wanna say anything, but this game seems lame)", TextProps{.font = "Roboto", .font_height = 30, .foreground_color = material::BLUE_500, .background_color = material::GRAY_100}},
-                Text{"explicit", TextProps{.font = "MaterialIcons", .foreground_color = colors::GREEN}},
-            */ Image{ImageProps{.source = FileImageSource{.path = R"(C:\Users\Basit\Desktop\wallpaperflare.com_wallpaper.jpg)"}, .border_radius = vec4{20, 20, 20, 20}, .aspect_ratio = stx::Some(2.0f), .resize_on_load = true}}}};
+               Radio(8, state),
+               Text{"verified", TextProps{.font = "MaterialIcons", .foreground_color = colors::YELLOW}},
+               Text{R"(Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum)", TextProps{.font = "Roboto", .foreground_color = material::BLUE_500, .background_color = material::GRAY_100}},
+               Text{R"(I didn't wanna say anything, but this game seems lame)", TextProps{.font = "Roboto", .font_height = 30, .foreground_color = material::BLUE_500, .background_color = material::GRAY_100}},
+               Text{"explicit", TextProps{.font = "MaterialIcons", .foreground_color = colors::GREEN}},
+               Image{ImageProps{.source = FileImageSource{.path = R"(C:\Users\Basit\Desktop\wallpaperflare.com_wallpaper.jpg)"}, .border_radius = vec4{20, 20, 20, 20}, .aspect_ratio = stx::Some(2.0f), .resize_on_load = true}}}};
   timepoint  last_tick = Clock::now();
   while (true)
   {
