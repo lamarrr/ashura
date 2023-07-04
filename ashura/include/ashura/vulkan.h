5c617075320cf7843b314bffce0e0083904e3dba
Basit Ayantunde <rlamarrr@gmail.com>
2023-07-04 12:43:30 +0100

    refactoring

--- ashura/include/ashura/vulkan.h
+++ ashura/include/ashura/vulkan.h
@@ -1034,12 +1034,8 @@ inline Image create_msaa_color_resource(VkDevice dev, VkPhysicalDeviceMemoryProp
       .image            = image,
       .viewType         = VK_IMAGE_VIEW_TYPE_2D,
       .format           = swapchain_format,
-      .components       = VkComponentMapping{.r = VK_COMPONENT_SWIZZLE_IDENTITY,
-                                             .g = VK_COMPONENT_SWIZZLE_IDENTITY,
-                                             .b = VK_COMPONENT_SWIZZLE_IDENTITY,
-                                             .a = VK_COMPONENT_SWIZZLE_IDENTITY},
-      .subresourceRange = VkImageSubresourceRange{
-          .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT, .baseMipLevel = 0, .levelCount = 1, .baseArrayLayer = 0, .layerCount = 1}};
+      .components       = VkComponentMapping{.r = VK_COMPONENT_SWIZZLE_IDENTITY, .g = VK_COMPONENT_SWIZZLE_IDENTITY, .b = VK_COMPONENT_SWIZZLE_IDENTITY, .a = VK_COMPONENT_SWIZZLE_IDENTITY},
+      .subresourceRange = VkImageSubresourceRange{.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT, .baseMipLevel = 0, .levelCount = 1, .baseArrayLayer = 0, .layerCount = 1}};
 
   VkImageView view;
 
