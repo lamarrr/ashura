3eaf87724751a755d85a095b2f4cc7a2855967ba
Basit Ayantunde <rlamarrr@gmail.com>
2023-07-13 23:30:04 +0100

    added tracing template

---
+++ ashura/include/ashura/trace.h
@@ -0,0 +1,9 @@
+
+#pragma once
+
+#define ASH_DECLARE_TRACE_CATEGORY()
+#define ASH_DEFINE_TRACE_CATEGORY()
+#define ASH_TRACE()
+#define ASH_TRACE_FUNC()
+#define ASH_TRACE_BEGIN()
+#define ASH_TRACE_END()
