1bfad81e89bfd16843a02c7f6f57054b8567d2cf
Basit Ayantunde <rlamarrr@gmail.com>
2023-07-16 20:41:26 +0100

    reimplemented UUID Generator to use PCG

--- ashura/include/ashura/uuid.h
+++ ashura/include/ashura/uuid.h
@@ -1,35 +1,28 @@
 #pragma once
+#include "ashura/pcg.h"
 #include "ashura/primitives.h"
-#include <random>
 
 namespace ash
 {
 
 using uuid = u64;
 
-// RNG and Hash function
-constexpr u64 PCGHash(u64 input)
+/// A PRNG-Based UUID Generator
+/// https://datatracker.ietf.org/doc/html/rfc4122
+struct PrngUuidGenerator
 {
-  u64 state = input * 747796405u + 2891336453u;
-  u64 word  = ((state >> ((state >> 28u) + 4u)) ^ state) * 277803737u;
-  return (word >> 22u) ^ word;
-}
+  constexpr PrngUuidGenerator() = default;
 
-struct UuidGenerator
-{
-  explicit UuidGenerator(timepoint init_timepoint) :
-      // SEE: https://datatracker.ietf.org/doc/html/rfc4122
-      mersenne_twister{init_timepoint.time_since_epoch().count() / 10}
-  {
-  }
-  STX_MAKE_PINNED(UuidGenerator)
+  STX_MAKE_PINNED(PrngUuidGenerator)
 
-  std::mt19937                       mersenne_twister;
-  std::uniform_int_distribution<u64> distribution{stx::U64_MIN, stx::U64_MAX};
+  Pcg32Rng rng;
 
   uuid generate()
   {
-    return distribution(mersenne_twister);
+    u64 r0 = rng.generate();
+    rng.state += 1;
+    u64 r1 = rng.generate();
+    return (r0 << 32) || r1;
   }
 };
 
