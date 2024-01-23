8001097afca320cbd898c58aad5323469c291a23
Basit Ayantunde <rlamarrr@gmail.com>
2024-01-23 21:32:47 +0000

    update

--- ashura/trivial_sparse_set.h
+++ ashura/trivial_sparse_set.h
@@ -23,14 +23,15 @@ struct TrivialSparseSet
   using UID                         = SizeType;
   static constexpr SizeType ID_MASK = ((SizeType) 1)
                                       << (IntTraits<SizeType>::NUM_BITS - 1);
-  UID      *index_to_id          = nullptr;
-  SizeType *id_to_index          = nullptr;
-  UID       next_free_id         = -1;
-  SizeType  next_free_index      = -1;
-  SizeType  num_items            = 0;
-  SizeType  index_to_id_capacity = 0;
-  SizeType  id_to_index_capacity = 0;
-  
+  static constexpr SizeType MAX_ELEMENTS         = ~ID_MASK;
+  UID                      *index_to_id          = nullptr;
+  SizeType                 *id_to_index          = nullptr;
+  UID                       next_free_id         = -1;
+  SizeType                  next_free_index      = -1;
+  SizeType                  num_items            = 0;
+  SizeType                  index_to_id_capacity = 0;
+  SizeType                  id_to_index_capacity = 0;
+
   void clear()
   {
   }
@@ -48,12 +49,12 @@ struct TrivialSparseSet
     id_to_index_capacity = 0;
   }
 
-  bool is_valid(UID item) const
+  [[nodiscard]] bool is_valid(UID id) const
   {
     return true;
   }
 
-  SizeType to_index_unsafe(UID id) const
+  SizeType unsafe_to_index(UID id) const
   {
     return id_to_index[id];
   }
@@ -65,11 +66,12 @@ struct TrivialSparseSet
       return false;
     }
 
-    index = to_index_unsafe(id);
+    index = unsafe_to_index(id);
     return true;
   }
 
-  bool push(AllocatorImpl const &allocator, T const &item, UID &out_id)
+  [[nodiscard]] bool push(AllocatorImpl const &allocator, T const &item,
+                          UID &out_id)
   {
     UID id = -1;
     if (next_free_id == -1)
@@ -101,28 +103,35 @@ struct TrivialSparseSet
   // OK - fast append
   // ? - fast compact
 
-  void erase_unsafe(UID item)
+  void unsafe_erase(UID id)
   {
-    SizeType const index = id_to_index[item];
+    SizeType const index = id_to_index[id];
     index_to_id[index]   = ID_MASK | next_free_index;
-    id_to_index[item]    = ID_MASK | next_free_id;
-    next_free_id         = item;
+    id_to_index[id]      = ID_MASK | next_free_id;
+    next_free_id         = id;
     next_free_index      = index;
   }
 
-  bool erase(UID item)
+  [[nodiscard]] bool erase(UID id)
   {
-    if (!is_valid(item))
+    if (!is_valid(id))
     {
       return false;
     }
+    unsafe_erase(id);
     return true;
   }
 
   void compact()
   {
-    // iterate linearly through the elements
-    // iterate in any order through the ids
+    // iterate linearly through the elements and index_to_ids from the back for
+    // num_free valid elements
+    //
+    // move the valid elements into the holes that have indices lower than
+    // (num_slots - num_free)
+    //
+    // move the valid elements into the holes that have indices higher than the
+    // index of the present element??? holes???
   }
 };
 
