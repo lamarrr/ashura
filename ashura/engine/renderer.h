46f172ed76d2452f30a71f1976ba6489168269b5
Basit Ayantunde <basit.ayantunde@codethink.co.uk>
2024-06-06 11:24:29 +0100

    more re-work + refactoring necessary for widgets

--- ashura/engine/renderer.h
+++ ashura/engine/renderer.h
@@ -32,23 +32,23 @@ struct Renderer
   StrHashMap<RenderPassImpl> custom = {};
 
   // ********* TEST ******** //
-  gfx::Buffer        params_buffer = nullptr;
-  gfx::Image         image         = nullptr;
-  gfx::ImageView     texture       = nullptr;
-  gfx::Sampler       sampler       = nullptr;
-  gfx::DescriptorSet params_ssbo   = nullptr;
-  /// TODO(lamarrr): attachment textures group, updated on every frame
-  /// recreation. update descriptor sets when either attachments are recreated/
+  /* gfx::Buffer        params_buffer = nullptr;
+   gfx::Image         image         = nullptr;
+   gfx::ImageView     texture       = nullptr;
+   gfx::Sampler       sampler       = nullptr;
+   gfx::DescriptorSet params_ssbo   = nullptr;
+   /// TODO(lamarrr): attachment textures group, updated on every frame
+   /// recreation. update descriptor sets when either attachments are recreated/
 
-  gfx::Buffer        pbr_vtx_buff      = nullptr;
-  gfx::Buffer        pbr_idx_buff      = nullptr;
-  gfx::Buffer        pbr_prm_buff      = nullptr;
-  gfx::Buffer        pbr_indirect_buff = nullptr;
-  gfx::Buffer        pbr_lights_buff   = nullptr;
-  gfx::DescriptorSet pbr_vtx_ssbo      = nullptr;
-  gfx::DescriptorSet pbr_idx_ssbo      = nullptr;
-  gfx::DescriptorSet pbr_prm_ssbo      = nullptr;
-  gfx::DescriptorSet pbr_lights_ssbo   = nullptr;
+   gfx::Buffer        pbr_vtx_buff      = nullptr;
+   gfx::Buffer        pbr_idx_buff      = nullptr;
+   gfx::Buffer        pbr_prm_buff      = nullptr;
+   gfx::Buffer        pbr_indirect_buff = nullptr;
+   gfx::Buffer        pbr_lights_buff   = nullptr;
+   gfx::DescriptorSet pbr_vtx_ssbo      = nullptr;
+   gfx::DescriptorSet pbr_idx_ssbo      = nullptr;
+   gfx::DescriptorSet pbr_prm_ssbo      = nullptr;
+   gfx::DescriptorSet pbr_lights_ssbo   = nullptr;*/
 
   void init(gfx::DeviceImpl p_device, bool p_use_hdr,
             u32 p_max_frames_in_flight, gfx::Extent p_initial_extent,
@@ -63,6 +63,7 @@ struct Renderer
     rrect.init(ctx);
 
     /******REMOVE******/
+    /*
     params_buffer =
         ctx.device
             ->create_buffer(
@@ -217,7 +218,8 @@ struct Renderer
             .element = 0,
             .buffers = to_span({gfx::BufferBinding{.buffer = pbr_lights_buff,
                                                    .offset = 0,
-                                                   .size = gfx::WHOLE_SIZE}})});
+                                                   .size =
+    gfx::WHOLE_SIZE}})});*/
   }
 
   void uninit()
@@ -293,175 +295,175 @@ struct Renderer
                                             .first_array_layer = 0,
                                             .num_array_layers  = 1}}));
 
-    enc->clear_color_image(
-        enc.self, image, gfx::Color{.float32 = {1, 1, 1, 1}},
-        to_span({gfx::ImageSubresourceRange{.aspects = gfx::ImageAspects::Color,
-                                            .first_mip_level   = 0,
-                                            .num_mip_levels    = 1,
-                                            .first_array_layer = 0,
-                                            .num_array_layers  = 1}}));
+    /*
+        enc->clear_color_image(
+            enc.self, image, gfx::Color{.float32 = {1, 1, 1, 1}},
+            to_span({gfx::ImageSubresourceRange{.aspects =
+       gfx::ImageAspects::Color, .first_mip_level   = 0, .num_mip_levels    = 1,
+                                                .first_array_layer = 0,
+                                                .num_array_layers  = 1}}));
 
-    timespec ts;
-    timespec_get(&ts, TIME_UTC);
-    enc->update_buffer(
-        enc.self,
-        to_span<RRectParam>(
-            {RRectParam{.transform = affine_scale3d({1080.0 / 1920, 1, 1}) *
-                                     affine_scale3d({.8F, .75F, 1}) *
-                                     affine_rotate3d_z(0.5F),
-                        .radii        = {.25F, .2F, .1F, .4F},
-                        .uv           = {{0, 0}, {1, 1}},
-                        .tint         = {{1, 0, 1, 1},
-                                         {1, 0, 0, 1},
-                                         {0, 0, 1, 1},
-                                         {1, 1, 1, 1}},
-                        .aspect_ratio = .75 / .8,
-                        .albedo       = 0},
-             RRectParam{
-                 .transform =
-                     affine_scale3d({1080.0 / 1920, 1, 1}) *
-                     affine_scale3d({.8F, .75F, 1}) *
-                     affine_rotate3d_z((ts.tv_nsec / 5'000'000'000.0f) * 1),
-                 .radii = {.25F, .2F, .1F, .4F},
-                 .uv    = {{0, 0}, {1, 1}},
-                 .tint =
-                     {{1, 0, 1, 1}, {1, 1, 0, 1}, {0, 0, 1, 1}, {1, 1, 1, 1}},
-                 .aspect_ratio = .75 / .8,
-                 .albedo       = 0}})
-            .as_u8(),
-        0, params_buffer);
+        timespec ts;
+        timespec_get(&ts, TIME_UTC);
+        enc->update_buffer(
+            enc.self,
+            to_span<RRectParam>(
+                {RRectParam{.transform = affine_scale3d({1080.0 / 1920, 1, 1}) *
+                                         affine_scale3d({.8F, .75F, 1}) *
+                                         affine_rotate3d_z(0.5F),
+                            .radii        = {.25F, .2F, .1F, .4F},
+                            .uv           = {{0, 0}, {1, 1}},
+                            .tint         = {{1, 0, 1, 1},
+                                             {1, 0, 0, 1},
+                                             {0, 0, 1, 1},
+                                             {1, 1, 1, 1}},
+                            .aspect_ratio = .75 / .8,
+                            .albedo       = 0},
+                 RRectParam{
+                     .transform =
+                         affine_scale3d({1080.0 / 1920, 1, 1}) *
+                         affine_scale3d({.8F, .75F, 1}) *
+                         affine_rotate3d_z((ts.tv_nsec / 5'000'000'000.0f) * 1),
+                     .radii = {.25F, .2F, .1F, .4F},
+                     .uv    = {{0, 0}, {1, 1}},
+                     .tint =
+                         {{1, 0, 1, 1}, {1, 1, 0, 1}, {0, 0, 1, 1}, {1, 1, 1,
+       1}}, .aspect_ratio = .75 / .8, .albedo       = 0}}) .as_u8(), 0,
+       params_buffer);
 
-    rrect.add_pass(
-        ctx, RRectPassParams{
-                 .rendering_info =
-                     gfx::RenderingInfo{
-                         .extent            = ctx.framebuffer.extent,
-                         .num_layers        = 1,
-                         .color_attachments = to_span({gfx::RenderingAttachment{
-                             .view = ctx.framebuffer.color_image_view,
-                         }})},
-                 .params_ssbo        = params_ssbo,
-                 .params_ssbo_offset = 0,
-                 .textures           = textures,
-                 .first_instance     = 0,
-                 .num_instances      = 2});
+        rrect.add_pass(
+            ctx, RRectPassParams{
+                     .rendering_info =
+                         gfx::RenderingInfo{
+                             .extent            = ctx.framebuffer.extent,
+                             .num_layers        = 1,
+                             .color_attachments =
+       to_span({gfx::RenderingAttachment{ .view =
+       ctx.framebuffer.color_image_view,
+                             }})},
+                     .params_ssbo        = params_ssbo,
+                     .params_ssbo_offset = 0,
+                     .textures           = textures,
+                     .first_instance     = 0,
+                     .num_instances      = 2});
 
-    enc->update_buffer(enc.self,
-                       to_span<PBRVertex>({{-1, -1, 0.5F, 0, 0},
-                                           {1, -1, 0.5F, 0, 1},
-                                           {-1, 1, 0.5F, 0, 1},
-                                           {1, 1, 0.5F, 1, 1},
-                                           {-1, -1, -0.5F, 0, 0},
-                                           {1, -1, -0.5F, 0, 1},
-                                           {-1, 1, -0.5F, 0, 1},
-                                           {1, 1, -0.5F, 1, 1}})
-                           .as_u8(),
-                       0, pbr_vtx_buff);
+        enc->update_buffer(enc.self,
+                           to_span<PBRVertex>({{-1, -1, 0.5F, 0, 0},
+                                               {1, -1, 0.5F, 0, 1},
+                                               {-1, 1, 0.5F, 0, 1},
+                                               {1, 1, 0.5F, 1, 1},
+                                               {-1, -1, -0.5F, 0, 0},
+                                               {1, -1, -0.5F, 0, 1},
+                                               {-1, 1, -0.5F, 0, 1},
+                                               {1, 1, -0.5F, 1, 1}})
+                               .as_u8(),
+                           0, pbr_vtx_buff);
 
-    enc->update_buffer(enc.self,
-                       to_span<u32>({// Top
-                                     2, 6, 7, 2, 3, 7,
-                                     // Bottom
-                                     0, 4, 5, 0, 1, 5,
-                                     // Left
-                                     0, 2, 6, 0, 4, 6,
-                                     // Right
-                                     1, 3, 7, 1, 5, 7,
-                                     // Front
-                                     0, 2, 3, 0, 1, 3,
-                                     // Back
-                                     4, 6, 7, 4, 5, 7})
-                           .as_u8(),
-                       0, pbr_idx_buff);
+        enc->update_buffer(enc.self,
+                           to_span<u32>({// Top
+                                         2, 6, 7, 2, 3, 7,
+                                         // Bottom
+                                         0, 4, 5, 0, 1, 5,
+                                         // Left
+                                         0, 2, 6, 0, 4, 6,
+                                         // Right
+                                         1, 3, 7, 1, 5, 7,
+                                         // Front
+                                         0, 2, 3, 0, 1, 3,
+                                         // Back
+                                         4, 6, 7, 4, 5, 7})
+                               .as_u8(),
+                           0, pbr_idx_buff);
 
-    enc->update_buffer(
-        enc.self,
-        to_span<PBRParam>(
-            {{.model = affine_scale3d({.5F, .5F, .5F}) *
-                       affine_rotate3d_z(rot_z) * affine_rotate3d_x(rot_x),
-              .view       = affine_scale3d({1080.0f / 1920, 1, 1}),
-              .projection = (Mat4) orthographic(1, 1, 0.1F, 100),
-              .albedo     = {1, 0, 1, 1}}})
-            .as_u8(),
-        0, pbr_prm_buff);
+        enc->update_buffer(
+            enc.self,
+            to_span<PBRParam>(
+                {{.model = affine_scale3d({.5F, .5F, .5F}) *
+                           affine_rotate3d_z(rot_z) * affine_rotate3d_x(rot_x),
+                  .view       = affine_scale3d({1080.0f / 1920, 1, 1}),
+                  .projection = (Mat4) orthographic(1, 1, 0.1F, 100),
+                  .albedo     = {1, 0, 1, 1}}})
+                .as_u8(),
+            0, pbr_prm_buff);
 
-    enc->update_buffer(enc.self,
-                       to_span<PunctualLight>({{.direction   = {0, 0, 0},
-                                                .position    = {-1, -1, 0},
-                                                .color       = {1, 1, 0, 1},
-                                                .inner_angle = 0,
-                                                .outer_angle = PI,
-                                                .intensity   = 1,
-                                                .radius      = 0.5}})
-                           .as_u8(),
-                       0, pbr_lights_buff);
+        enc->update_buffer(enc.self,
+                           to_span<PunctualLight>({{.direction   = {0, 0, 0},
+                                                    .position    = {-1, -1, 0},
+                                                    .color       = {1, 1, 0, 1},
+                                                    .inner_angle = 0,
+                                                    .outer_angle = PI,
+                                                    .intensity   = 1,
+                                                    .radius      = 0.5}})
+                               .as_u8(),
+                           0, pbr_lights_buff);
 
-    enc->update_buffer(enc.self,
-                       to_span({gfx::DrawCommand{.vertex_count   = 36,
-                                                 .instance_count = 1,
-                                                 .first_vertex   = 0,
-                                                 .first_instance = 0}})
-                           .as_u8(),
-                       0, pbr_indirect_buff);
+        enc->update_buffer(enc.self,
+                           to_span({gfx::DrawCommand{.vertex_count   = 36,
+                                                     .instance_count = 1,
+                                                     .first_vertex   = 0,
+                                                     .first_instance = 0}})
+                               .as_u8(),
+                           0, pbr_indirect_buff);
 
-    // pbr.add_pass(
-    //     ctx,
-    //     PBRPassParams{
-    //         .rendering_info =
-    //             gfx::RenderingInfo{
-    //                 .extent            = ctx.framebuffer.extent,
-    //                 .num_layers        = 1,
-    //                 .color_attachments = to_span({gfx::RenderingAttachment{
-    //                     .view = ctx.framebuffer.color_image_view,
-    //                 }}),
-    //                 .depth_attachment  = to_span({gfx::RenderingAttachment{
-    //                      .view =
-    //                      ctx.framebuffer.depth_stencil_image_view}})},
-    //         .wireframe         = false,
-    //         .vertex_ssbo       = pbr_vtx_ssbo,
-    //         .index_ssbo        = pbr_idx_ssbo,
-    //         .param_ssbo        = pbr_prm_ssbo,
-    //         .param_ssbo_offset = 0,
-    //         .light_ssbo        = pbr_lights_ssbo,
-    //         .textures          = textures,
-    //         .indirect          = {.buffer     = pbr_indirect_buff,
-    //                               .offset     = 0,
-    //                               .draw_count = 1,
-    //                               .stride     = sizeof(gfx::DrawCommand)}});
+        // pbr.add_pass(
+        //     ctx,
+        //     PBRPassParams{
+        //         .rendering_info =
+        //             gfx::RenderingInfo{
+        //                 .extent            = ctx.framebuffer.extent,
+        //                 .num_layers        = 1,
+        //                 .color_attachments =
+       to_span({gfx::RenderingAttachment{
+        //                     .view = ctx.framebuffer.color_image_view,
+        //                 }}),
+        //                 .depth_attachment  =
+       to_span({gfx::RenderingAttachment{
+        //                      .view =
+        //                      ctx.framebuffer.depth_stencil_image_view}})},
+        //         .wireframe         = false,
+        //         .vertex_ssbo       = pbr_vtx_ssbo,
+        //         .index_ssbo        = pbr_idx_ssbo,
+        //         .param_ssbo        = pbr_prm_ssbo,
+        //         .param_ssbo_offset = 0,
+        //         .light_ssbo        = pbr_lights_ssbo,
+        //         .textures          = textures,
+        //         .indirect          = {.buffer     = pbr_indirect_buff,
+        //                               .offset     = 0,
+        //                               .draw_count = 1,
+        //                               .stride     =
+       sizeof(gfx::DrawCommand)}});
 
-    // TODO(lamarrr): needs to be two-pass??
+        // TODO(lamarrr): needs to be two-pass??
 
-    /// downsample 4 times then upsample 4 times, ping-ponging between two
-    /// textures. use the mips? or specific areas of the images
+        /// downsample 4 times then upsample 4 times, ping-ponging between two
+        /// textures. use the mips? or specific areas of the images
 
-    /* blur.add_pass(
-         ctx, BlurPassParams{
-                  .rendering_info =
-                      gfx::RenderingInfo{
-                          .extent            = ctx.scratch_framebuffer.extent,
-                          .num_layers        = 1,
-                          .color_attachments =
-     to_span({gfx::RenderingAttachment{ .view =
-     ctx.scratch_framebuffer.color_image_view,
-                          }})},
-                  .param    = BlurParam{.offset  = {0, 0},
-                                        .extent  = {1, 1},
-                                        .radius  = {6 / 1920.0, 6 / 1080.0},
-                                        .texture = 0},
-                  .textures = textures_2});
-
-     enc->copy_image(enc.self, ctx.scratch_framebuffer.color_image,
-                     ctx.framebuffer.color_image,
-                     to_span<gfx::ImageCopy>(
-                         {{.src_layers = {.aspects = gfx::ImageAspects::Color,
-                                          .num_array_layers = 1},
-                           .src_offset = {},
-                           .dst_layers = {.aspects = gfx::ImageAspects::Color,
-                                          .num_array_layers = 1},
-                           .dst_offset = {},
-                           .extent =
-     ctx.framebuffer.color_image_desc.extent}}));
-                           */
+        blur.add_pass(
+             ctx, BlurPassParams{
+                      .rendering_info =
+                          gfx::RenderingInfo{
+                              .extent            =
+       ctx.scratch_framebuffer.extent, .num_layers        = 1,
+                              .color_attachments =
+         to_span({gfx::RenderingAttachment{ .view =
+         ctx.scratch_framebuffer.color_image_view,
+                              }})},
+                      .param    = BlurParam{.offset  = {0, 0},
+                                            .extent  = {1, 1},
+                                            .radius  = {6 / 1920.0, 6 / 1080.0},
+                                            .texture = 0},
+                      .textures = textures_2});
+    */
+    enc->copy_image(enc.self, ctx.scratch_framebuffer.color_image,
+                    ctx.framebuffer.color_image,
+                    to_span<gfx::ImageCopy>(
+                        {{.src_layers = {.aspects = gfx::ImageAspects::Color,
+                                         .num_array_layers = 1},
+                          .src_offset = {},
+                          .dst_layers = {.aspects = gfx::ImageAspects::Color,
+                                         .num_array_layers = 1},
+                          .dst_offset = {},
+                          .extent = ctx.framebuffer.color_image_desc.extent}}));
   }
 };
 
