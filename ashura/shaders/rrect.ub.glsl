29d2bede1142bdb3f6b0ad2d2816e06956063eed
Basit Ayantunde <rlamarrr@gmail.com>
2024-06-19 08:02:29 +0100

    fixed shaders

--- ashura/shaders/rrect.ub.glsl
+++ ashura/shaders/rrect.ub.glsl
@@ -8,7 +8,6 @@
 #define BOTTOM_LEFT 2
 #define BOTTOM_RIGHT 3
 
-/// @param radius relative to height
 struct Params
 {
   vec4  transform[4];
@@ -39,15 +38,14 @@ const vec2 VERTEX_BUFFER[] = {vec2(-1, -1), vec2(1, -1), vec2(1, 1),
 #ifdef VERTEX_SHADER
 
 layout(location = 0) flat out uint o_idx;
-layout(location = 1) out vec2 o_uv;
+layout(location = 1) out vec2 o_pos;
 
 void main()
 {
   Params p    = params[gl_InstanceIndex];
   vec2   pos  = VERTEX_BUFFER[gl_VertexIndex];
-  vec2   uv   = (pos + 1.0) * 0.5;
   o_idx       = gl_InstanceIndex;
-  o_uv        = uv;
+  o_pos       = pos;
   gl_Position = to_mat4(p.transform) * vec4(pos, 0.0, 1.0);
 }
 #endif
@@ -55,29 +53,30 @@ void main()
 #ifdef FRAGMENT_SHADER
 
 layout(location = 0) flat in uint i_idx;
-layout(location = 1) in vec2 i_uv;
+layout(location = 1) in vec2 i_pos;
 
 layout(location = 0) out vec4 o_color;
 
 void main()
 {
   Params p    = params[i_idx];
-  bool   left = i_uv.x < 0.5;
-  bool   top  = i_uv.y < 0.5;
+  bool   left = i_pos.x < 0;
+  bool   top  = i_pos.y < 0;
   uint   corner =
       left ? (top ? TOP_LEFT : BOTTOM_LEFT) : (top ? TOP_RIGHT : BOTTOM_RIGHT);
   float radius      = p.radii[corner];
-  vec2  pos         = i_uv * vec2(p.aspect_ratio, 1);
-  vec2  half_extent = vec2(p.aspect_ratio, 1) * 0.5;
+  vec2  pos         = i_pos * vec2(p.aspect_ratio, 1);
+  vec2  half_extent = vec2(p.aspect_ratio, 1);
   float dist        = rrect_sdf(pos, half_extent, radius);
-  vec2  tex_uv      = mix(p.uv.xy, p.uv.zw, i_uv);
+  vec2  uv          = (i_pos + 1) * 0.5;
+  vec2  tex_uv      = mix(p.uv.xy, p.uv.zw, uv);
   vec4  color       = texture(sampler2D(textures[nonuniformEXT(p.albedo)],
                                         samplers[nonuniformEXT(p.isampler)]),
                               tex_uv * p.tiling) *
-               bilerp(p.tint, i_uv, 0.5);
+               bilerp(p.tint, uv, 0.5);
   float fill_alpha   = 1 - smoothstep(0, p.edge_smoothness, dist);
   float stroke_alpha = 1 - smoothstep(0, p.thickness, abs(dist));
-  float alpha        = mix(stroke_alpha, fill_alpha, p.stroke);
+  float alpha        = mix(fill_alpha, stroke_alpha, p.stroke);
   o_color            = vec4(color.rgb, color.a * alpha);
 }
 #endif
