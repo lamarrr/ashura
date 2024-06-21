262d016ac0239dfa46f19f4c02fd36e8608c6d4d
Basit Ayantunde <rlamarrr@gmail.com>
2024-06-21 11:57:09 +0100

    added simd requirements

--- ashura/std/simd.h
+++ ashura/std/simd.h
@@ -1,5 +1,8 @@
+#pragma once
+#include "ashura/std/types.h"
 
-
+namespace ash
+{
 // any
 // all
 // none
@@ -18,3 +21,17 @@
 // deinterleaving
 // reverse
 // broadcast
+// mask-operations, must use unsigned types
+
+/// REQUIREMENTS
+/// - automatic dispatch or algorithm specialization based on data alignment and
+/// the number of valid elements in the specified data.
+/// - allow interoperation with plain float pointers
+/// - create simd adapter allocator that always allocates simd-aligned memory of
+/// a specified minimum alignment.
+/// - default over-alignment is always 1, unspecified will invoke dynamic
+/// dispatch
+/// - choose between runtime and comptime (most pref) dispatch. avoid using a
+/// separate type for simd elements
+
+}        // namespace ash
