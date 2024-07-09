13adb1d8b363ee09271efccb03379ca0a044f0b6
Basit Ayantunde <rlamarrr@gmail.com>
2024-07-09 08:50:57 +0100

    🚧updated text compositor

--- ashura/engine/text_compositor.cc
+++ ashura/engine/text_compositor.cc
@@ -1,1 +1,532 @@
 #include "ashura/engine/text_compositor.h"
+#include "ashura/std/error.h"
+#include "ashura/std/mem.h"
+#include "ashura/std/range.h"
+
+namespace ash
+{
+
+TextLocation TextCompositor::hit(TextLayout const &layout, Vec2 pos) const
+{
+  f32       current_top = 0;
+  u32       l           = 0;
+  u32 const num_lines   = layout.lines.size32();
+
+  if (num_lines == 0)
+  {
+    return TextLocation{.first = 0, .num = 0, .line = 0, .column = 0};
+  }
+
+  // separated vertical and horizontal clamped hit test
+  for (; l < num_lines; l++)
+  {
+    if (current_top <= pos.y &&
+        (current_top + layout.lines[l].metrics.height) >= pos.y)
+    {
+      break;
+    }
+  }
+
+  l = min(l, num_lines - 1);
+
+  Line const &line = layout.lines[l];
+
+  f32 cursor_x = 0;
+  u32 r        = 0;
+  for (; r < line.num_runs; r++)
+  {
+    TextRun const &run        = layout.runs[line.first_run + r];
+    bool           intersects = (cursor_x <= pos.x &&
+                       (cursor_x + pt_to_px(run.metrics.advance,
+                                                      run.font_height)) >= pos.x) ||
+                      (r == line.num_runs - 1);
+    if (!intersects)
+    {
+      continue;
+    }
+    for (u32 g = 0; g < run.num_glyphs; g++)
+    {
+      // TODO(lamarrr): not correct, needs to perform actual intersection
+      // test also, need to take care of directionality
+      GlyphShape const &glyph = layout.glyphs[run.first_glyph + g];
+      // based on direction, find first grapheme that is lesser, doesn't have
+      // to strictly intersect
+      bool intersects =
+          (pt_to_px(glyph.advance.x, run.font_height) + cursor_x < pos.x) ||
+          (g == run.num_glyphs - 1 && r == line.num_runs - 1);
+      if (intersects)
+      {
+        return TextLocation{
+            .first = glyph.cluster, .num = 1, .line = l, .column = 0};
+      }
+    }
+    cursor_x += pt_to_px(run.metrics.advance, run.font_height);
+  }
+
+  return TextLocation{.first = 0, .num = 0, .line = l, .column = 0};
+}
+
+void TextCompositor::pop_records(u32 num)
+{
+  CHECK(num <= records.size32());
+  u32 reclaimed = 0;
+  for (u32 i = 0; i < num; i++)
+  {
+    reclaimed += records[i].num;
+  }
+  mem::move(to_span(buffer).slice(reclaimed).as_const(), to_span(buffer));
+  mem::move(to_span(records).slice(num).as_const(), to_span(records));
+  buffer_size -= reclaimed;
+  latest_record -= num;
+  current_record -= num;
+}
+
+void TextCompositor::append_record(bool is_insert, u32 text_pos,
+                                   Span<u32 const> text)
+{
+  if (text.size32() > buffer.size32())
+  {
+    // clear all records as we can't insert a new record without invalidating
+    // the history
+    pop_records(records.size32());
+    return;
+  }
+
+  while (buffer_size + text.size32() > buffer.size32())
+  {
+    // pop half, to amortize shifting cost.
+    // always pop by atleast 1. since the buffer can fit it and atleast 1
+    // record would be using the available memory.
+    pop_records(max(records.size32() >> 1, 1U));
+  }
+
+  if (current_record + 1 >= records.size32())
+  {
+    pop_records(max(records.size32() >> 1, 1U));
+  }
+
+  mem::copy(text, to_span(buffer).slice(buffer_size));
+
+  current_record++;
+  latest_record           = current_record;
+  records[current_record] = TextEditRecord{
+      .text_pos = text_pos, .num = text.size32(), .is_insert = is_insert};
+}
+
+void TextCompositor::undo(Insert insert, Erase erase)
+{
+  if (current_record == 0)
+  {
+    return;
+  }
+  // undo changes of current record
+  TextEditRecord &record = records[current_record];
+  buffer_pos -= record.num;
+  if (record.is_insert)
+  {
+    erase(Slice32{record.text_pos, record.num});
+  }
+  else
+  {
+    insert(record.text_pos, to_span(buffer).slice(buffer_pos, record.num));
+  }
+  current_record--;
+}
+
+void TextCompositor::redo(Insert insert, Erase erase)
+{
+  if (current_record + 1 > latest_record)
+  {
+    return;
+  }
+  current_record++;
+  // apply changes of next record
+  TextEditRecord &record = records[current_record];
+  if (record.is_insert)
+  {
+    insert(record.text_pos, to_span(buffer).slice(buffer_pos, record.num));
+  }
+  else
+  {
+    erase(Slice32{record.text_pos, record.num});
+  }
+
+  buffer_pos += record.num;
+}
+
+void TextCompositor::delete_selection(Span<u32 const> text, Insert insert,
+                                      Erase erase)
+{
+  (void) insert;
+  if (selection.is_empty() || selection.offset > text.size32())
+  {
+    selection = {};
+    return;
+  }
+
+  append_record(false, selection.offset, text.slice(selection));
+  erase(selection);
+  selection.span = 0;
+}
+
+void TextCompositor::input_text(Span<u32 const> text, Span<u32 const> input,
+                                Insert insert, Erase erase)
+{
+  if (!selection.is_empty())
+  {
+    delete_selection(text.slice(selection), insert, erase);
+    selection.span = 0;
+  }
+
+  append_record(true, selection.offset, text.slice(selection));
+  insert(selection.offset, input);
+}
+
+void TextCompositor::drag(TextLayout const &layout, Vec2 pos)
+{
+  u32 grapheme = hit(layout, pos).first;
+  if (grapheme < selection.offset)
+  {
+    u32 extra        = selection.offset - grapheme;
+    selection.offset = grapheme;
+    selection.span += extra;
+  }
+  else
+  {
+    u32 extra = grapheme - selection.offset;
+    selection.span += extra;
+  }
+}
+
+static constexpr bool is_symbol(Span<u32 const> symbols, u32 c)
+{
+  return !find(symbols, c).is_empty();
+}
+
+/// @brief given a sequence of text and symbols find the symbol boundary from
+/// a given codepoint index.
+/// @param text must be non-empty
+/// @param pos must be valid index into the text
+/// @param symbols additional unicode symbols that specify word boundaries,
+/// i.e. a code editor may use '(',')' as word boundaries.
+/// @param[out] first index of first char in symbolic boundary
+/// @param[out] last index of last char in symbolic boundary
+static constexpr void find_boundary(Span<u32 const> text, u32 const pos,
+                                    Span<u32 const> symbols, Slice32 &slice)
+{
+  u32 fwd = pos;
+  u32 bwd = pos;
+
+  if (is_symbol(symbols, text[pos]))
+  {
+    while (fwd < text.size32() && is_symbol(symbols, text[fwd]))
+    {
+      fwd++;
+    }
+
+    while (bwd != 0 && is_symbol(symbols, text[bwd]))
+    {
+      bwd--;
+    }
+  }
+  else
+  {
+    while (fwd < text.size32() && !is_symbol(symbols, text[fwd]))
+    {
+      fwd++;
+    }
+
+    while (bwd != 0 && !is_symbol(symbols, text[bwd]))
+    {
+      bwd--;
+    }
+  }
+
+  bwd = (bwd == 0) ? bwd : (bwd + 1);
+  fwd = fwd - 1;
+
+  slice.offset = bwd;
+  slice.span   = (fwd - bwd) + 1;
+}
+
+void TextCompositor::select_codepoint(Span<u32 const> text)
+{
+  selection.span = 1;
+  selection      = selection.resolve(text.size32());
+}
+
+void TextCompositor::select_word(Span<u32 const> text)
+{
+  selection = selection.resolve(text.size32());
+  if (text.is_empty())
+  {
+    selection = Slice32{};
+    return;
+  }
+
+  find_boundary(text, selection.offset, word_symbols, selection);
+}
+
+void TextCompositor::select_line(Span<u32 const> text)
+{
+  selection = selection.resolve(text.size32());
+
+  if (selection.is_empty())
+  {
+    selection = Slice32{};
+    return;
+  }
+
+  find_boundary(text, selection.offset, line_symbols, selection);
+}
+
+void TextCompositor::select_all(Span<u32 const> text)
+{
+  selection = Slice32{0, text.size32()};
+}
+
+void TextCompositor::single_click(TextLayout const &layout, Vec2 pos)
+{
+  u32 grapheme = hit(layout, pos).first;
+  selection    = Slice32{grapheme, 0};
+}
+
+void TextCompositor::double_click(TextLayout const &layout, Vec2 pos,
+                                  Span<u32 const> text)
+{
+  single_click(layout, pos);
+  select_word(text);
+}
+
+void TextCompositor::triple_click(TextLayout const &layout, Vec2 pos,
+                                  Span<u32 const> text)
+{
+  single_click(layout, pos);
+  select_line(text);
+}
+
+void TextCompositor::quad_click(TextLayout const &layout, Vec2 pos,
+                                Span<u32 const> text)
+{
+  single_click(layout, pos);
+  select_all(text);
+}
+
+void TextCompositor::click(TextLayout const &layout, Span<u32 const> text,
+                           u32 num_clicks, Vec2 pos)
+{
+  switch (num_clicks)
+  {
+    case 0:
+      break;
+
+    case 1:
+      single_click(layout, pos);
+      break;
+
+    case 2:
+      double_click(layout, pos, text);
+      break;
+
+    case 3:
+      triple_click(layout, pos, text);
+      break;
+
+    case 4:
+    default:
+      quad_click(layout, pos, text);
+      break;
+  }
+}
+
+void TextCompositor::up()
+{
+  // move the cursor up to the previous line
+  // remove cursor selection
+}
+
+void TextCompositor::down()
+{
+  // move the cursor down to the next line
+  // remove cursor selection
+}
+
+void TextCompositor::line_start()
+{
+  // move the cursor to the beginning of the line
+  // remove cursor selection
+}
+
+void TextCompositor::line_end()
+{
+  // move the cursor to the end of the line
+  // remove cursor selection
+}
+
+void TextCompositor::left()
+{
+  // if has selection, move to left of selection
+  if (selection.span != 0)
+  {
+    selection.span = 0;
+    return;
+  }
+
+  // move the cursor to the previous codepoint
+  if (selection.offset != 0)
+  {
+    selection.offset--;
+  }
+}
+
+void TextCompositor::right()
+{
+  // move to the next codepoint
+  if (selection.span == 0)
+  {
+    if (++selection.offset == U32_MAX)
+    {
+      selection.span = 0;
+    }
+    return;
+  }
+
+  // move to the last codepoint
+  selection.offset = selection.end() - 1;
+  selection.span   = 0;
+}
+
+void TextCompositor::page_up()
+{
+}
+void TextCompositor::page_down()
+{
+}
+
+void TextCompositor::escape()
+{
+  selection.span = 0;
+}
+
+void TextCompositor::command(TextCommand cmd, Span<u32 const> text,
+                             Insert insert, Erase erase,
+                             Fn<Span<u32 const>()>     get_content,
+                             Fn<void(Span<u32 const>)> set_content)
+{
+  switch (cmd)
+  {
+    case TextCommand::Escape:
+    {
+      escape();
+    }
+    break;
+    case TextCommand::BackSpace:
+    {
+    }
+    break;
+    case TextCommand::Delete:
+    {
+    }
+    break;
+    case TextCommand::Left:
+    {
+      left();
+    }
+    break;
+    case TextCommand::Right:
+    {
+      right();
+    }
+    break;
+    case TextCommand::Up:
+    {
+      up();
+    }
+    break;
+    case TextCommand::Down:
+    {
+      down();
+    }
+    break;
+    case TextCommand::WordStart:
+    {
+      //
+    }
+    break;
+    case TextCommand::WordEnd:
+    {
+      //
+    }
+    break;
+    case TextCommand::LineStart:
+    {
+      line_start();
+    }
+    break;
+    case TextCommand::LineEnd:
+    {
+      line_end();
+    }
+    break;
+    case TextCommand::PageUp:
+    {
+      page_up();
+    }
+    break;
+    case TextCommand::PageDown:
+    {
+      page_down();
+    }
+    break;
+    case TextCommand::Cut:
+    {
+      set_content(text.slice(selection));
+      delete_selection(text, insert, erase);
+    }
+    break;
+    case TextCommand::Copy:
+    {
+      set_content(text.slice(selection));
+    }
+    break;
+    case TextCommand::Paste:
+    {
+      input_text(text, get_content(), insert, erase);
+    }
+    break;
+    case TextCommand::Undo:
+    {
+      undo(insert, erase);
+    }
+    break;
+    case TextCommand::Redo:
+    {
+      redo(insert, erase);
+    }
+    break;
+    case TextCommand::SelectCodepoint:
+    {
+      select_codepoint(text);
+    }
+    break;
+    case TextCommand::SelectWord:
+    {
+      select_word(text);
+    }
+    break;
+    case TextCommand::SelectLine:
+    {
+      select_line(text);
+    }
+    break;
+    case TextCommand::SelectAll:
+    {
+      select_all(text);
+    }
+    break;
+    default:
+      break;
+  }
+}
+
+}        // namespace ash
--- ashura/engine/text_compositor.h
+++ ashura/engine/text_compositor.h
@@ -1,14 +1,10 @@
 #pragma once
-#include "ashura/engine/key.h"
 #include "ashura/engine/text.h"
-#include "ashura/std/error.h"
-#include "ashura/std/mem.h"
 #include "ashura/std/types.h"
 
 namespace ash
 {
 
-// TODO(lamarrr): use this
 struct TextLocation
 {
   u32 first  = 0;
@@ -30,6 +26,32 @@ struct TextEditRecord
   bool is_insert = false;
 };
 
+enum class TextCommand : u8
+{
+  Escape          = 0,
+  BackSpace       = 1,
+  Delete          = 2,
+  Left            = 3,
+  Right           = 4,
+  Up              = 5,
+  Down            = 6,
+  WordStart       = 7,
+  WordEnd         = 8,
+  LineStart       = 9,
+  LineEnd         = 10,
+  PageUp          = 11,
+  PageDown        = 12,
+  Cut             = 13,
+  Copy            = 14,
+  Paste           = 15,
+  Undo            = 16,
+  Redo            = 17,
+  SelectCodepoint = 18,
+  SelectWord      = 19,
+  SelectLine      = 20,
+  SelectAll       = 21
+};
+
 /// @brief A simple stack-based text compositor
 /// @param selection_first first codepoint in the selection range. not always a
 /// valid index into the text.
@@ -48,21 +70,20 @@ struct TextCompositor
   /// @param index index to be erased from, needs to be clamped to the size of
   /// the destination container.
   /// @param num number of items to be erased
-  typedef Fn<void(u32, u32)> Erase;
+  typedef Fn<void(Slice32)> Erase;
 
   static constexpr u32 DEFAULT_WORD_SYMBOLS[] = {' ', '\t'};
   static constexpr u32 DEFAULT_LINE_SYMBOLS[] = {'\n'};
 
-  u32                 selection_first = 0;
-  u32                 selection_last  = 0;
-  Vec<u32>            buffer          = {};
-  Vec<TextEditRecord> records         = {};
-  u32                 buffer_size     = 0;
-  u32                 buffer_pos      = 0;
-  u32                 latest_record   = 0;
-  u32                 current_record  = 0;
-  Span<u32 const>     word_symbols    = to_span(DEFAULT_WORD_SYMBOLS);
-  Span<u32 const>     line_symbols    = to_span(DEFAULT_LINE_SYMBOLS);
+  Slice32             selection      = {0, 0};
+  Vec<u32>            buffer         = {};
+  Vec<TextEditRecord> records        = {};
+  u32                 buffer_size    = 0;
+  u32                 buffer_pos     = 0;
+  u32                 latest_record  = 0;
+  u32                 current_record = 0;
+  Span<u32 const>     word_symbols   = to_span(DEFAULT_WORD_SYMBOLS);
+  Span<u32 const>     line_symbols   = to_span(DEFAULT_LINE_SYMBOLS);
 
   void init(u32 num_buffer_codepoints, u32 num_records)
   {
@@ -76,387 +97,87 @@ struct TextCompositor
 
   void reset()
   {
-    selection_first = 0;
-    selection_last  = 0;
+    selection = {0, 0};
     buffer.reset();
     records.reset();
-    // current_record = U32_MAX;
+    buffer_size    = 0;
+    buffer_pos     = 0;
+    latest_record  = 0;
+    current_record = 0;
+    word_symbols   = to_span(DEFAULT_WORD_SYMBOLS);
+    line_symbols   = to_span(DEFAULT_LINE_SYMBOLS);
   }
-
-  void clear();
 
   void uninit()
   {
     reset();
   }
 
-  constexpr bool has_selection() const
-  {
-    return selection_first != selection_last;
-  }
-
-  /// @brief given a position in the laid-out text return the grapheme the
-  /// cursor points to.
+  /// @brief given a position in the laid-out text return the location of the
+  /// grapheme the cursor points to.
   /// @param pos position in laid-out text to return from
-  /// @return U32_MAX if no grapheme overlaps the selection, otherwise the
-  /// grapheme index in the original text, see GlyphShape::grapheme
-  TextLocation hit(TextLayout const &layout, Vec2 pos) const
-  {
-    // TODO(lamarrr): hit tests always have to return a result
-    f32       current_top = 0;
-    u32       line        = 0;
-    u32 const num_lines   = layout.lines.size32();
-    for (; line < num_lines; line++)
-    {
-      if (current_top <= pos.y &&
-          (current_top + layout.lines[line].metrics.height) >= pos.y)
-      {
-        break;
-      }
-    }
+  TextLocation hit(TextLayout const &layout, Vec2 pos) const;
 
-    if (line != num_lines)
-    {
-      f32 line_x = 0;
-      for (u32 r = 0; r < layout.lines[line].num_runs; r++)
-      {
-        TextRun const &run = layout.runs[layout.lines[line].first_run + r];
-        if (!(line_x <= pos.x && (line_x + pt_to_px(run.metrics.advance,
-                                                    run.font_height)) >= pos.x))
-        {
-          continue;
-        }
-        for (u32 g = 0; g < run.num_glyphs; g++)
-        {
-          // TODO(lamarrr): not correct, needs to perform actual intersection
-          // test also, need to take care of directionality
-          GlyphShape const &glyph = layout.glyphs[run.first_glyph + g];
-          if (pt_to_px(glyph.advance.x, run.font_height) + line_x < pos.x)
-          {
-            return glyph.cluster;
-          }
-        }
-        line_x += pt_to_px(run.metrics.advance, run.font_height);
-      }
+  void pop_records(u32 num);
 
-      // return last char on line
-    }
+  void append_record(bool is_insert, u32 text_pos, Span<u32 const> text);
 
-    // if non-empty line, return char on last line
+  void undo(Insert insert, Erase erase);
 
-    return TextLocation{};
-  }
+  void redo(Insert insert, Erase erase);
 
-  void pop_records(u32 num)
-  {
-    CHECK(num <= records.size32());
-    u32 reclaimed = 0;
-    for (u32 i = 0; i < num; i++)
-    {
-      reclaimed += records[i].num;
-    }
-    mem::move(to_span(buffer).slice(reclaimed).as_const(), to_span(buffer));
-    mem::move(to_span(records).slice(num).as_const(), to_span(records));
-    buffer_size -= reclaimed;
-    latest_record -= num;
-    current_record -= num;
-  }
-
-  void append_record(bool is_insert, u32 text_pos, Span<u32 const> text)
-  {
-    if (text.size32() > buffer.size32())
-    {
-      // clear all records as we can't insert a new record without invalidating
-      // the history
-      pop_records(records.size32());
-      return;
-    }
-
-    while (buffer_size + text.size32() > buffer.size32())
-    {
-      // pop half, to amortize shifting cost.
-      // always pop by atleast 1. since the buffer can fit it and atleast 1
-      // record would be using the available memory.
-      pop_records(max(records.size32() >> 1, 1U));
-    }
-
-    if (current_record + 1 >= records.size32())
-    {
-      pop_records(max(records.size32() >> 1, 1U));
-    }
-
-    mem::copy(text, to_span(buffer).slice(buffer_size));
-
-    current_record++;
-    latest_record           = current_record;
-    records[current_record] = TextEditRecord{
-        .text_pos = text_pos, .num = text.size32(), .is_insert = is_insert};
-  }
-
-  void undo(Insert insert, Erase erase)
-  {
-    if (current_record == 0)
-    {
-      return;
-    }
-    // undo changes of current record
-    TextEditRecord &record = records[current_record];
-    buffer_pos -= record.num;
-    if (record.is_insert)
-    {
-      erase(record.text_pos, record.num);
-    }
-    else
-    {
-      insert(record.text_pos, to_span(buffer).slice(buffer_pos, record.num));
-    }
-    current_record--;
-  }
-
-  void redo(Insert insert, Erase erase)
-  {
-    if (current_record + 1 > latest_record)
-    {
-      return;
-    }
-    current_record++;
-    // apply changes of next record
-    TextEditRecord &record = records[current_record];
-    if (record.is_insert)
-    {
-      insert(record.text_pos, to_span(buffer).slice(buffer_pos, record.num));
-    }
-    else
-    {
-      erase(record.text_pos, record.num);
-    }
-
-    buffer_pos += record.num;
-  }
-
-  void delete_selection(Span<u32 const> text, Insert insert, Erase erase)
-  {
-    if (selection_first == selection_last)
-    {
-      return;
-    }
-
-    if (selection_first > selection_last)
-    {
-      swap(selection_first, selection_last);
-    }
-
-    u32 first      = selection_first;
-    u32 num        = (selection_last - selection_first) + 1;
-    selection_last = selection_first;
-
-    append_record(false, first, text.slice(first, num));
-    erase(first, num);
-  }
-
-  /// @brief Parses keyboard commands using keyboard states, i.e: keyboard
-  /// cursor navigation (left, right, home, end, pg up, pg down), keyboard cut,
-  /// copy, & paste, etc.
-  /// @param key_states bit mask of all key states
-  void commands(u64 const (&key_states)[NUM_KEYS / 64]);
+  void delete_selection(Span<u32 const> text, Insert insert, Erase erase);
 
   /// @brief IME-text input
-  /// @param text text from IME to insert
-  void input_text(Span<u32 const> text, Insert insert, Erase erase);
+  /// @param text original text
+  /// @param input text from IME to insert
+  void input_text(Span<u32 const> text, Span<u32 const> input, Insert insert,
+                  Erase erase);
 
-  // move to selection last
-  // move to selection first
-  // move to word previous
-  // move to word next
-  // select word
+  void drag(TextLayout const &layout, Vec2 pos);
 
-  // TODO(lamarrr): multi-select via middle mouse drag.
+  void select_codepoint(Span<u32 const> text);
 
-  void click(TextLayout const &layout, Span<u32 const> text, u32 num, Vec2 pos)
-  {
-    switch (num)
-    {
-      case 0:
-        break;
+  void select_word(Span<u32 const> text);
 
-      case 1:
-        single_click(layout, pos);
-        break;
+  void select_line(Span<u32 const> text);
 
-      case 2:
-        double_click(layout, pos, text);
-        break;
-
-      case 3:
-        triple_click(layout, pos, text);
-        break;
-
-      case 4:
-      default:
-        quad_click(layout, pos, text);
-        break;
-    }
-  }
+  void select_all(Span<u32 const> text);
 
   /// @brief: on mouse press, move the cursor to the clicked location, and reset
   /// the selection
-  void single_click(TextLayout const &layout, Vec2 pos)
-  {
-    // TODO(lamarrr): handle U32_MAX
-    selection_last  = hit(layout, pos);
-    selection_first = selection_last;
-  }
+  void single_click(TextLayout const &layout, Vec2 pos);
 
-  void drag(TextLayout const &layout, Vec2 pos)
-  {
-    // TODO(lamarrr): handle U32_MAX
-    selection_last = hit(layout, pos);
-  }
+  void double_click(TextLayout const &layout, Vec2 pos, Span<u32 const> text);
 
-  static constexpr Slice32 selection_slice(u32 size, u32 first, u32 last)
-  {
-    if (first > last)
-    {
-      swap(first, last);
-    }
-    return Slice32{first, (last + 1) - first}.resolve(size);
-  }
+  void triple_click(TextLayout const &layout, Vec2 pos, Span<u32 const> text);
 
-  static constexpr bool is_symbol(u32 c, Span<u32 const> symbols)
-  {
-    return c == ' ' || c == '\t' || !find(symbols, c).is_empty();
-  }
+  void quad_click(TextLayout const &layout, Vec2 pos, Span<u32 const> text);
 
-  /// @brief given a sequence of text and symbols find the symbol boundary from
-  /// a given codepoint index.
-  /// @param text must be non-empty
-  /// @param pos must be valid index into the text
-  /// @param symbols additional unicode symbols that specify word boundaries,
-  /// i.e. a code editor may use '(',')' as word boundaries.
-  /// @param[out] first index of first char in symbolic boundary
-  /// @param[out] last index of last char in symbolic boundary
-  static constexpr void find_boundary(Span<u32 const> text, u32 const pos,
-                                      Span<u32 const> symbols, u32 &first,
-                                      u32 &last)
-  {
-    u32 f = pos;
-    u32 b = pos;
+  void click(TextLayout const &layout, Span<u32 const> text, u32 num_clicks,
+             Vec2 pos);
 
-    if (is_symbol(text[pos], symbols))
-    {
-      while (f < text.size32() && is_symbol(text[f], symbols))
-      {
-        f++;
-      }
+  void up();
 
-      while (b != 0 && is_symbol(text[b], symbols))
-      {
-        b--;
-      }
-    }
-    else
-    {
-      while (f < text.size32() && !is_symbol(text[f], symbols))
-      {
-        f++;
-      }
+  void down();
 
-      while (b != 0 && !is_symbol(text[f], symbols))
-      {
-        b--;
-      }
-    }
+  void line_start();
 
-    // TODO(lamarrr): is this correct?
-    f     = f - 1;
-    b     = (b == 0) ? b : (b + 1);
-    first = b;
-    last  = f;
-  }
+  void line_end();
 
-  void double_click(TextLayout const &layout, Vec2 pos, Span<u32 const> text)
-  {
-    // select word
-    u32 sym = hit(layout, pos);
-    // find word boundary forward and backward from current point
-    //
-    if (sym == U32_MAX)
-    {
-    }
+  void left();
 
-    find_boundary(text, 0x0000f, word_symbols, );
-  }
+  void right();
 
-  void triple_click(TextLayout const &layout, Vec2 pos, Span<u32 const> text)
-  {
-    // select line
-    u32 sym = hit(layout, pos);
-    //
-    // TODO(lamarrr): function to return indices as
+  void page_up();
 
-    find_boundary(text, pos, line_symbols, , );
-  }
+  void page_down();
 
-  void quad_click(TextLayout const &layout, Vec2 pos, Span<u32 const> text)
-  {
-    // select whole text
-  }
+  void escape();
 
-  void key_up()
-  {
-    // move the cursor up to the previous line
-    // remove cursor selection
-  }
-
-  void key_down()
-  {
-    // move the cursor down to the next line
-    // remove cursor selection
-  }
-
-  void key_home()
-  {
-    // move the cursor to the beginning of the line
-    // remove cursor selection
-  }
-
-  void key_end()
-  {
-    // move the cursor to the end of the line
-    // remove cursor selection
-  }
-
-  void key_left()
-  {
-    // if has selection, move to left of selection
-    if (selection_first != selection_last)
-    {
-      selection_last  = min(selection_first, selection_last);
-      selection_first = selection_last;
-      return;
-    }
-
-    // move the cursor to the previous codepoint
-    selection_first = max(selection_last, 1U) - 1;
-    selection_last  = selection_first;
-  }
-
-  void key_right()
-  {
-    if (selection_first != selection_last)
-    {
-      selection_last  = max(selection_first, selection_last);
-      selection_first = selection_last;
-      return;
-    }
-
-    // move the cursor to the next codepoint
-    selection_first = selection_last + 1;
-    selection_last  = selection_first;
-  }
-
-  void key_page_up();
-  void key_page_down();
+  void command(TextCommand cmd, Span<u32 const> text, Insert insert,
+               Erase erase, Fn<Span<u32 const>()> get_content,
+               Fn<void(Span<u32 const>)> set_content);
 };
 
 }        // namespace ash
