3614f9be393c7663bcbd6ecbb949c9217f72306b
Basit Ayantunde <rlamarrr@gmail.com>
2024-07-18 14:45:06 +0100

    🎨 made fmt::Context const ref'd

--- ashura/std/format.cc
+++ ashura/std/format.cc
@@ -7,7 +7,7 @@ namespace ash
 {
 
 template <typename IntT>
-bool push_int(fmt::Context &ctx, fmt::Spec const &spec, IntT value)
+bool push_int(fmt::Context const& ctx, fmt::Spec const &spec, IntT value)
 {
   int base = 0;
 
@@ -52,7 +52,7 @@ bool push_int(fmt::Context &ctx, fmt::Spec const &spec, IntT value)
 }
 
 template <typename FloatT>
-bool push_float(fmt::Context &ctx, fmt::Spec const &spec, FloatT value)
+bool push_float(fmt::Context const& ctx, fmt::Spec const &spec, FloatT value)
 {
   std::chars_format format = std::chars_format::general;
   switch (spec.style)
@@ -88,69 +88,69 @@ bool push_float(fmt::Context &ctx, fmt::Spec const &spec, FloatT value)
   return false;
 }
 
-bool fmt::push(fmt::Context &ctx, fmt::Spec const &, bool value)
+bool fmt::push(fmt::Context const& ctx, fmt::Spec const &, bool value)
 {
   return ctx.push(value ? "true"_span : "false"_span);
 }
 
-bool fmt::push(fmt::Context &ctx, fmt::Spec const &spec, u8 value)
+bool fmt::push(fmt::Context const& ctx, fmt::Spec const &spec, u8 value)
 {
   return push_int(ctx, spec, value);
 }
 
-bool fmt::push(fmt::Context &ctx, fmt::Spec const &spec, u16 value)
+bool fmt::push(fmt::Context const& ctx, fmt::Spec const &spec, u16 value)
 {
   return push_int(ctx, spec, value);
 }
 
-bool fmt::push(fmt::Context &ctx, fmt::Spec const &spec, u32 value)
+bool fmt::push(fmt::Context const& ctx, fmt::Spec const &spec, u32 value)
 {
   return push_int(ctx, spec, value);
 }
 
-bool fmt::push(fmt::Context &ctx, fmt::Spec const &spec, u64 value)
+bool fmt::push(fmt::Context const& ctx, fmt::Spec const &spec, u64 value)
 {
   return push_int(ctx, spec, value);
 }
 
-bool fmt::push(fmt::Context &ctx, fmt::Spec const &spec, i8 value)
+bool fmt::push(fmt::Context const& ctx, fmt::Spec const &spec, i8 value)
 {
   return push_int(ctx, spec, value);
 }
 
-bool fmt::push(fmt::Context &ctx, fmt::Spec const &spec, i16 value)
+bool fmt::push(fmt::Context const& ctx, fmt::Spec const &spec, i16 value)
 {
   return push_int(ctx, spec, value);
 }
 
-bool fmt::push(fmt::Context &ctx, fmt::Spec const &spec, i32 value)
+bool fmt::push(fmt::Context const& ctx, fmt::Spec const &spec, i32 value)
 {
   return push_int(ctx, spec, value);
 }
 
-bool fmt::push(fmt::Context &ctx, fmt::Spec const &spec, i64 value)
+bool fmt::push(fmt::Context const& ctx, fmt::Spec const &spec, i64 value)
 {
   return push_int(ctx, spec, value);
 }
 
-bool fmt::push(fmt::Context &ctx, fmt::Spec const &spec, f32 value)
+bool fmt::push(fmt::Context const& ctx, fmt::Spec const &spec, f32 value)
 {
   return push_float(ctx, spec, value);
 }
 
-bool fmt::push(fmt::Context &ctx, fmt::Spec const &spec, f64 value)
+bool fmt::push(fmt::Context const& ctx, fmt::Spec const &spec, f64 value)
 {
   return push_float(ctx, spec, value);
 }
 
-bool fmt::push(fmt::Context &ctx, fmt::Spec const &spec, Vec2 const &value)
+bool fmt::push(fmt::Context const& ctx, fmt::Spec const &spec, Vec2 const &value)
 {
   return push(ctx, spec, "Vec2{") && push(ctx, spec, value.x) &&
          push(ctx, spec, ", ") && push(ctx, spec, value.y) &&
          push(ctx, spec, "}");
 }
 
-bool fmt::push(fmt::Context &ctx, fmt::Spec const &spec, Vec3 const &value)
+bool fmt::push(fmt::Context const& ctx, fmt::Spec const &spec, Vec3 const &value)
 {
   return push(ctx, spec, "Vec3{") && push(ctx, spec, value.x) &&
          push(ctx, spec, ", ") && push(ctx, spec, value.y) &&
@@ -158,7 +158,7 @@ bool fmt::push(fmt::Context &ctx, fmt::Spec const &spec, Vec3 const &value)
          push(ctx, spec, "}");
 }
 
-bool fmt::push(fmt::Context &ctx, fmt::Spec const &spec, Vec4 const &value)
+bool fmt::push(fmt::Context const& ctx, fmt::Spec const &spec, Vec4 const &value)
 {
   return push(ctx, spec, "Vec4{") && push(ctx, spec, value.x) &&
          push(ctx, spec, ", ") && push(ctx, spec, value.y) &&
@@ -167,14 +167,14 @@ bool fmt::push(fmt::Context &ctx, fmt::Spec const &spec, Vec4 const &value)
          push(ctx, spec, "}");
 }
 
-bool fmt::push(fmt::Context &ctx, fmt::Spec const &spec, Vec2I const &value)
+bool fmt::push(fmt::Context const& ctx, fmt::Spec const &spec, Vec2I const &value)
 {
   return push(ctx, spec, "Vec2I{") && push(ctx, spec, value.x) &&
          push(ctx, spec, ", ") && push(ctx, spec, value.y) &&
          push(ctx, spec, "}");
 }
 
-bool fmt::push(fmt::Context &ctx, fmt::Spec const &spec, Vec3I const &value)
+bool fmt::push(fmt::Context const& ctx, fmt::Spec const &spec, Vec3I const &value)
 {
   return push(ctx, spec, "Vec3I{") && push(ctx, spec, value.x) &&
          push(ctx, spec, ", ") && push(ctx, spec, value.y) &&
@@ -182,7 +182,7 @@ bool fmt::push(fmt::Context &ctx, fmt::Spec const &spec, Vec3I const &value)
          push(ctx, spec, "}");
 }
 
-bool fmt::push(fmt::Context &ctx, fmt::Spec const &spec, Vec4I const &value)
+bool fmt::push(fmt::Context const& ctx, fmt::Spec const &spec, Vec4I const &value)
 {
   return push(ctx, spec, "Vec4I{") && push(ctx, spec, value.x) &&
          push(ctx, spec, ", ") && push(ctx, spec, value.y) &&
@@ -191,14 +191,14 @@ bool fmt::push(fmt::Context &ctx, fmt::Spec const &spec, Vec4I const &value)
          push(ctx, spec, "}");
 }
 
-bool fmt::push(fmt::Context &ctx, fmt::Spec const &spec, Vec2U const &value)
+bool fmt::push(fmt::Context const& ctx, fmt::Spec const &spec, Vec2U const &value)
 {
   return push(ctx, spec, "Vec2I{") && push(ctx, spec, value.x) &&
          push(ctx, spec, ", ") && push(ctx, spec, value.y) &&
          push(ctx, spec, "}");
 }
 
-bool fmt::push(fmt::Context &ctx, fmt::Spec const &spec, Vec3U const &value)
+bool fmt::push(fmt::Context const& ctx, fmt::Spec const &spec, Vec3U const &value)
 {
   return push(ctx, spec, "Vec3U{") && push(ctx, spec, value.x) &&
          push(ctx, spec, ", ") && push(ctx, spec, value.y) &&
@@ -206,7 +206,7 @@ bool fmt::push(fmt::Context &ctx, fmt::Spec const &spec, Vec3U const &value)
          push(ctx, spec, "}");
 }
 
-bool fmt::push(fmt::Context &ctx, fmt::Spec const &spec, Vec4U const &value)
+bool fmt::push(fmt::Context const& ctx, fmt::Spec const &spec, Vec4U const &value)
 {
   return push(ctx, spec, "Vec4U{") && push(ctx, spec, value.x) &&
          push(ctx, spec, ", ") && push(ctx, spec, value.y) &&
@@ -215,23 +215,23 @@ bool fmt::push(fmt::Context &ctx, fmt::Spec const &spec, Vec4U const &value)
          push(ctx, spec, "}");
 }
 
-bool fmt::push(fmt::Context &, fmt::Spec &spec, fmt::Spec const &value)
+bool fmt::push(fmt::Context const& , fmt::Spec &spec, fmt::Spec const &value)
 {
   spec = value;
   return true;
 }
 
-bool fmt::push(fmt::Context &ctx, fmt::Spec const &, Span<char const> str)
+bool fmt::push(fmt::Context const& ctx, fmt::Spec const &, Span<char const> str)
 {
   return ctx.push(str);
 }
 
-bool fmt::push(fmt::Context &ctx, fmt::Spec const &spec, char const *str)
+bool fmt::push(fmt::Context const& ctx, fmt::Spec const &spec, char const *str)
 {
   return push(ctx, spec, Span<char const>{str, strlen(str)});
 }
 
-bool fmt::push(fmt::Context &ctx, fmt::Spec const &, void const *ptr)
+bool fmt::push(fmt::Context const& ctx, fmt::Spec const &, void const *ptr)
 {
   Spec const ptr_spec{.style = Style::Hex};
   return ctx.push("0x"_span) && push_int(ctx, ptr_spec, (uintptr_t) ptr);
--- ashura/std/format.h
+++ ashura/std/format.h
@@ -25,7 +25,9 @@ struct Spec
   i32   precision = 0;
 };
 
-/// @sratch_buffer: minimum recommended size of 256 bytes
+/// @param push function to be called to insert text into the format context.
+/// should return true when it is successful.
+/// @param sratch_buffer minimum recommended size of 256 bytes
 struct Context
 {
   Fn<bool(Span<char const>)> push = fn([](Span<char const>) { return true; });
@@ -33,55 +35,55 @@ struct Context
 };
 
 template <typename T>
-bool push(Context &ctx, Spec const &, T const &)
+bool push(Context const &ctx, Spec const &, T const &)
 {
   return ctx.push("{Unformatted Object}"_span);
 }
 
-bool push(Context &ctx, Spec const &, bool value);
-bool push(Context &ctx, Spec const &spec, u8 value);
-bool push(Context &ctx, Spec const &spec, u16 value);
-bool push(Context &ctx, Spec const &spec, u32 value);
-bool push(Context &ctx, Spec const &spec, u64 value);
-bool push(Context &ctx, Spec const &spec, i8 value);
-bool push(Context &ctx, Spec const &spec, i16 value);
-bool push(Context &ctx, Spec const &spec, i32 value);
-bool push(Context &ctx, Spec const &spec, i64 value);
-bool push(Context &ctx, Spec const &spec, f32 value);
-bool push(Context &ctx, Spec const &spec, f64 value);
-bool push(Context &ctx, Spec const &spec, Vec2 const &value);
-bool push(Context &ctx, Spec const &spec, Vec3 const &value);
-bool push(Context &ctx, Spec const &spec, Vec4 const &value);
-bool push(Context &ctx, Spec const &spec, Vec2I const &value);
-bool push(Context &ctx, Spec const &spec, Vec3I const &value);
-bool push(Context &ctx, Spec const &spec, Vec4I const &value);
-bool push(Context &ctx, Spec const &spec, Vec2U const &value);
-bool push(Context &ctx, Spec const &spec, Vec3U const &value);
-bool push(Context &ctx, Spec const &spec, Vec4U const &value);
-bool push(Context &, Spec &spec, Spec const &value);
-bool push(Context &ctx, Spec const &, Span<char const> str);
+bool push(Context const &ctx, Spec const &, bool value);
+bool push(Context const &ctx, Spec const &spec, u8 value);
+bool push(Context const &ctx, Spec const &spec, u16 value);
+bool push(Context const &ctx, Spec const &spec, u32 value);
+bool push(Context const &ctx, Spec const &spec, u64 value);
+bool push(Context const &ctx, Spec const &spec, i8 value);
+bool push(Context const &ctx, Spec const &spec, i16 value);
+bool push(Context const &ctx, Spec const &spec, i32 value);
+bool push(Context const &ctx, Spec const &spec, i64 value);
+bool push(Context const &ctx, Spec const &spec, f32 value);
+bool push(Context const &ctx, Spec const &spec, f64 value);
+bool push(Context const &ctx, Spec const &spec, Vec2 const &value);
+bool push(Context const &ctx, Spec const &spec, Vec3 const &value);
+bool push(Context const &ctx, Spec const &spec, Vec4 const &value);
+bool push(Context const &ctx, Spec const &spec, Vec2I const &value);
+bool push(Context const &ctx, Spec const &spec, Vec3I const &value);
+bool push(Context const &ctx, Spec const &spec, Vec4I const &value);
+bool push(Context const &ctx, Spec const &spec, Vec2U const &value);
+bool push(Context const &ctx, Spec const &spec, Vec3U const &value);
+bool push(Context const &ctx, Spec const &spec, Vec4U const &value);
+bool push(Context const &, Spec &spec, Spec const &value);
+bool push(Context const &ctx, Spec const &, Span<char const> str);
 
 template <usize N>
-bool push(Context &ctx, Spec const &spec, char const (&str)[N])
+bool push(Context const &ctx, Spec const &spec, char const (&str)[N])
 {
   return push(ctx, spec, Span{str, N});
 }
 
-bool push(Context &ctx, Spec const &spec, char const *str);
-bool push(Context &ctx, Spec const &spec, void const *ptr);
+bool push(Context const &ctx, Spec const &spec, char const *str);
+bool push(Context const &ctx, Spec const &spec, void const *ptr);
 
-inline bool push(Context &ctx, Spec const &spec, std::string const &str)
+inline bool push(Context const &ctx, Spec const &spec, std::string const &str)
 {
-  return push(ctx, spec, to_span(str));
+  return push(ctx, spec, span(str));
 }
 
-inline bool push(Context &ctx, Spec const &spec, std::string_view str)
+inline bool push(Context const &ctx, Spec const &spec, std::string_view str)
 {
-  return push(ctx, spec, to_span(str));
+  return push(ctx, spec, span(str));
 }
 
 template <typename... Args>
-bool format(Context &ctx, Args const &...args)
+bool format(Context const &ctx, Args const &...args)
 {
   Spec spec;
   return true && (push(ctx, spec, args) && ...);
