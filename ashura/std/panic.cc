5444f5b39afa8b2780d90183ea1a5afcc978d198
Basit Ayantunde <rlamarrr@gmail.com>
2024-11-07 00:59:25 +0000

    🎨 added noop panic handler

--- ashura/std/panic.cc
+++ ashura/std/panic.cc
@@ -6,13 +6,17 @@ namespace ash
 
 static u64 panic_count_impl = 0;
 
-static void custom_panic_handler()
+static void noop_panic_handler()
 {
-  // no-op
 }
+
+static void exception_panic_handler()
+{
+  throw Panic{};
+};
 
 ASH_C_LINKAGE ASH_DLL_EXPORT u64 *panic_count = &panic_count_impl;
 
-ASH_C_LINKAGE ASH_DLL_EXPORT PanicHandler panic_handler = custom_panic_handler;
+ASH_C_LINKAGE ASH_DLL_EXPORT PanicHandler panic_handler = noop_panic_handler;
 
 }        // namespace ash
