e22498f33d983f832771d936cb60554bae2b6a7c
Basit Ayantunde <rlamarrr@gmail.com>
2024-11-10 00:40:23 +0000

    🎨 made tuple type auto-generated

---
+++ ashura/std/v.gen.h
@@ -0,0 +1,37 @@
+
+/// SPDX-License-Identifier: MIT
+#pragma once
+#include <cstddef>
+
+namespace ash{
+
+template <std::size_t I>
+struct V
+{
+  static constexpr std::size_t INDEX = I;
+
+  static constexpr std::size_t index()
+  {
+    return INDEX;
+  }
+};
+    
+constexpr V<0> v0;
+constexpr V<1> v1;
+constexpr V<2> v2;
+constexpr V<3> v3;
+constexpr V<4> v4;
+constexpr V<5> v5;
+constexpr V<6> v6;
+constexpr V<7> v7;
+constexpr V<8> v8;
+constexpr V<9> v9;
+constexpr V<10> v10;
+constexpr V<11> v11;
+constexpr V<12> v12;
+constexpr V<13> v13;
+constexpr V<14> v14;
+constexpr V<15> v15;
+
+} // namespace ash
+
