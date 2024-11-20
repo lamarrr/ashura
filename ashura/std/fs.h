53ba7fce83386e74136408ccd3e0735ce1910da0
Basit Ayantunde <rlamarrr@gmail.com>
2024-11-20 12:05:44 +0000

    ✨ more core system improvements

--- ashura/std/fs.h
+++ ashura/std/fs.h
@@ -66,113 +66,110 @@ enum class [[nodiscard]] IoErr : i32
 
 constexpr Span<char const> to_string(IoErr err)
 {
-  switch (err)
-  {
-    case IoErr::None:
-      return "None"_span;
-    case IoErr::PermissionDenied:
-      return "PermissionDenied"_span;
-    case IoErr::AddressInUse:
-      return "AddressInUse"_span;
-    case IoErr::Again:
-      return "Again"_span;
-    case IoErr::Already:
-      return "Already"_span;
-    case IoErr::BadFileDescriptor:
-      return "BadFileDescriptor"_span;
-    case IoErr::Busy:
-      return "Busy"_span;
-    case IoErr::Canceled:
-      return "Canceled"_span;
-    case IoErr::DeadlockAvoided:
-      return "DeadlockAvoided"_span;
-    case IoErr::Exists:
-      return "Exists"_span;
-    case IoErr::BadAddress:
-      return "BadAddress"_span;
-    case IoErr::FileTooBig:
-      return "FileTooBig"_span;
-    case IoErr::IllegalCharSequence:
-      return "IllegalCharSequence"_span;
-    case IoErr::OpInProgress:
-      return "OpInProgress"_span;
-    case IoErr::SysCallInterrupted:
-      return "SysCallInterrupted"_span;
-    case IoErr::InvalidArg:
-      return "InvalidArg"_span;
-    case IoErr::IOErr:
-      return "IOErr"_span;
-    case IoErr::IsDirectory:
-      return "IsDirectory"_span;
-    case IoErr::TooManySymLinks:
-      return "TooManySymLinks"_span;
-    case IoErr::TooManyOpenFiles:
-      return "TooManyOpenFiles"_span;
-    case IoErr::TooManyLinks:
-      return "TooManyLinks"_span;
-    case IoErr::MsgTooLong:
-      return "MsgTooLong"_span;
-    case IoErr::FileNameTooLong:
-      return "FileNameTooLong"_span;
-    case IoErr::TooManyOpenSysFiles:
-      return "TooManyOpenSysFiles"_span;
-    case IoErr::NoBufferSpace:
-      return "NoBufferSpace"_span;
-    case IoErr::NoData:
-      return "NoData"_span;
-    case IoErr::InvalidDev:
-      return "InvalidDev"_span;
-    case IoErr::InvalidFileOrDir:
-      return "InvalidFileOrDir"_span;
-    case IoErr::ExecFormat:
-      return "ExecFormat"_span;
-    case IoErr::NoLocksAvailable:
-      return "NoLocksAvailable"_span;
-    case IoErr::NoLink:
-      return "NoLink"_span;
-    case IoErr::OutOfMemory:
-      return "OutOfMemory"_span;
-    case IoErr::OutOfSpace:
-      return "OutOfSpace"_span;
-    case IoErr::OutOfStreamRes:
-      return "OutOfStreamRes"_span;
-    case IoErr::NotStream:
-      return "NotStream"_span;
-    case IoErr::UnImplemented:
-      return "UnImplemented"_span;
-    case IoErr::NotDir:
-      return "NotDir"_span;
-    case IoErr::DirectoryNotEmpty:
-      return "DirectoryNotEmpty"_span;
-    case IoErr::Unsupported:
-      return "Unsupported"_span;
-    case IoErr::InvalidDeviceOrAddr:
-      return "InvalidDeviceOrAddr"_span;
-    case IoErr::OpUnsupported:
-      return "OpUnsupported"_span;
-    case IoErr::Overflow:
-      return "Overflow"_span;
-    case IoErr::OwnerDead:
-      return "OwnerDead"_span;
-    case IoErr::UnpermittedOp:
-      return "UnpermittedOp"_span;
-    case IoErr::BrokenPipe:
-      return "BrokenPipe"_span;
-    case IoErr::OutOfRange:
-      return "OutOfRange"_span;
-    case IoErr::ReadOnlyFileSys:
-      return "ReadOnlyFileSys"_span;
-    case IoErr::IllegalSeek:
-      return "IllegalSeek"_span;
-    case IoErr::NoSuchProcess:
-      return "NoSuchProcess"_span;
-    case IoErr::TextFileBusy:
-      return "TextFileBusy"_span;
-    case IoErr::TemporarilyUnavailable:
-      return "TemporarilyUnavailable"_span;
-    default:
-      return "Unidentified Filesystem Error"_span;
-  }
+  if (err == IoErr::None)
+    return "None"_span;
+  else if (err == IoErr::PermissionDenied)
+    return "PermissionDenied"_span;
+  else if (err == IoErr::AddressInUse)
+    return "AddressInUse"_span;
+  else if (err == IoErr::Again)
+    return "Again"_span;
+  else if (err == IoErr::Already)
+    return "Already"_span;
+  else if (err == IoErr::BadFileDescriptor)
+    return "BadFileDescriptor"_span;
+  else if (err == IoErr::Busy)
+    return "Busy"_span;
+  else if (err == IoErr::Canceled)
+    return "Canceled"_span;
+  else if (err == IoErr::DeadlockAvoided)
+    return "DeadlockAvoided"_span;
+  else if (err == IoErr::Exists)
+    return "Exists"_span;
+  else if (err == IoErr::BadAddress)
+    return "BadAddress"_span;
+  else if (err == IoErr::FileTooBig)
+    return "FileTooBig"_span;
+  else if (err == IoErr::IllegalCharSequence)
+    return "IllegalCharSequence"_span;
+  else if (err == IoErr::OpInProgress)
+    return "OpInProgress"_span;
+  else if (err == IoErr::SysCallInterrupted)
+    return "SysCallInterrupted"_span;
+  else if (err == IoErr::InvalidArg)
+    return "InvalidArg"_span;
+  else if (err == IoErr::IOErr)
+    return "IOErr"_span;
+  else if (err == IoErr::IsDirectory)
+    return "IsDirectory"_span;
+  else if (err == IoErr::TooManySymLinks)
+    return "TooManySymLinks"_span;
+  else if (err == IoErr::TooManyOpenFiles)
+    return "TooManyOpenFiles"_span;
+  else if (err == IoErr::TooManyLinks)
+    return "TooManyLinks"_span;
+  else if (err == IoErr::MsgTooLong)
+    return "MsgTooLong"_span;
+  else if (err == IoErr::FileNameTooLong)
+    return "FileNameTooLong"_span;
+  else if (err == IoErr::TooManyOpenSysFiles)
+    return "TooManyOpenSysFiles"_span;
+  else if (err == IoErr::NoBufferSpace)
+    return "NoBufferSpace"_span;
+  else if (err == IoErr::NoData)
+    return "NoData"_span;
+  else if (err == IoErr::InvalidDev)
+    return "InvalidDev"_span;
+  else if (err == IoErr::InvalidFileOrDir)
+    return "InvalidFileOrDir"_span;
+  else if (err == IoErr::ExecFormat)
+    return "ExecFormat"_span;
+  else if (err == IoErr::NoLocksAvailable)
+    return "NoLocksAvailable"_span;
+  else if (err == IoErr::NoLink)
+    return "NoLink"_span;
+  else if (err == IoErr::OutOfMemory)
+    return "OutOfMemory"_span;
+  else if (err == IoErr::OutOfSpace)
+    return "OutOfSpace"_span;
+  else if (err == IoErr::OutOfStreamRes)
+    return "OutOfStreamRes"_span;
+  else if (err == IoErr::NotStream)
+    return "NotStream"_span;
+  else if (err == IoErr::UnImplemented)
+    return "UnImplemented"_span;
+  else if (err == IoErr::NotDir)
+    return "NotDir"_span;
+  else if (err == IoErr::DirectoryNotEmpty)
+    return "DirectoryNotEmpty"_span;
+  else if (err == IoErr::Unsupported)
+    return "Unsupported"_span;
+  else if (err == IoErr::InvalidDeviceOrAddr)
+    return "InvalidDeviceOrAddr"_span;
+  else if (err == IoErr::OpUnsupported)
+    return "OpUnsupported"_span;
+  else if (err == IoErr::Overflow)
+    return "Overflow"_span;
+  else if (err == IoErr::OwnerDead)
+    return "OwnerDead"_span;
+  else if (err == IoErr::UnpermittedOp)
+    return "UnpermittedOp"_span;
+  else if (err == IoErr::BrokenPipe)
+    return "BrokenPipe"_span;
+  else if (err == IoErr::OutOfRange)
+    return "OutOfRange"_span;
+  else if (err == IoErr::ReadOnlyFileSys)
+    return "ReadOnlyFileSys"_span;
+  else if (err == IoErr::IllegalSeek)
+    return "IllegalSeek"_span;
+  else if (err == IoErr::NoSuchProcess)
+    return "NoSuchProcess"_span;
+  else if (err == IoErr::TextFileBusy)
+    return "TextFileBusy"_span;
+  else if (err == IoErr::TemporarilyUnavailable)
+    return "TemporarilyUnavailable"_span;
+  else
+    return "Unidentified Filesystem Error"_span;
 }
 
 namespace fmt
