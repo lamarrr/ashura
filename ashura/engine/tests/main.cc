85dfc444ba16c6c16f14286e540e5390922afb13
Basit Ayantunde <rlamarrr@gmail.com>
2024-11-28 09:11:40 +0000

    🚑 removed window system from tests

--- ashura/engine/tests/main.cc
+++ ashura/engine/tests/main.cc
@@ -13,7 +13,6 @@ int main(int argc, char **argv)
   using namespace ash;
   Logger::init();
   CHECK(logger->add_sink(&stdio_sink));
-  WindowSystem::init();
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
