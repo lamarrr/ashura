fdfca87d1f04bd75de6c3f92c9b83ae58ad6a62f
Basit Ayantunde <rlamarrr@gmail.com>
2024-11-30 03:07:06 +0000

    ✨ added hash seeding and changed hashvalue identifier to hash64

--- ashura/std/hash.cc
+++ ashura/std/hash.cc
@@ -1,13 +1,14 @@
 /// SPDX-License-Identifier: MIT
 #include "ashura/std/hash.h"
+#include "ashura/std/types.h"
 #include "xxh3.h"
 
 namespace ash
 {
 
-Hash hash_bytes(Span<u8 const> bytes)
+hash64 hash_bytes(Span<u8 const> bytes, hash64 seed)
 {
-  return XXH3_64bits(bytes.data(), bytes.size());
+  return XXH3_64bits_withSeed(bytes.data(), bytes.size_bytes(), seed);
 }
 
 }        // namespace ash
--- ashura/std/hash.h
+++ ashura/std/hash.h
@@ -5,24 +5,39 @@
 namespace ash
 {
 
-constexpr Hash hash_combine(Hash hash_a, Hash hash_b)
+constexpr hash64 hash_combine(hash64 hash_a, hash64 hash_b)
 {
   hash_a ^= hash_b + 0x9e37'79b9 + (hash_a << 6) + (hash_a >> 2);
   return hash_a;
 }
 
 template <typename... H>
-constexpr Hash hash_combine_n(Hash hash_a, H... hash_b)
+constexpr hash64 hash_combine_n(hash64 hash_a, H... hash_b)
 {
   ((hash_a = hash_combine(hash_a, hash_b)), ...);
   return hash_a;
 }
 
-Hash hash_bytes(Span<u8 const> bytes);
+hash64 hash_bytes(Span<u8 const> bytes, hash64 seed = 0);
 
 struct StrHasher
 {
-  Hash operator()(Span<char const> str) const
+  hash64 operator()(Span<char const> str) const
+  {
+    return hash_bytes(str.as_u8());
+  }
+
+  hash64 operator()(Span<c8 const> str) const
+  {
+    return hash_bytes(str.as_u8());
+  }
+
+  hash64 operator()(Span<c16 const> str) const
+  {
+    return hash_bytes(str.as_u8());
+  }
+
+  hash64 operator()(Span<c32 const> str) const
   {
     return hash_bytes(str.as_u8());
   }
@@ -31,7 +46,7 @@ struct StrHasher
 struct BitHasher
 {
   template <typename T>
-  Hash operator()(T const & a) const
+  hash64 operator()(T const & a) const
   {
     return hash_bytes(Span<T const>{&a, 1}.as_u8());
   }
