5f84c04e9180f633a6642081dcccbbba09615bfa
Basit Ayantunde <rlamarrr@gmail.com>
2024-11-30 00:04:27 +0000

    ♻️consistency improvements

--- ashura/std/time.h
+++ ashura/std/time.h
@@ -4,8 +4,8 @@
 
 namespace ash
 {
+
 using std::chrono::days;
-using std::chrono::high_resolution_clock;
 using std::chrono::hours;
 using std::chrono::microseconds;
 using std::chrono::milliseconds;
@@ -13,10 +13,16 @@ using std::chrono::minutes;
 using std::chrono::months;
 using std::chrono::nanoseconds;
 using std::chrono::seconds;
+using std::chrono::weeks;
+using std::chrono::years;
+
+using std::chrono::high_resolution_clock;
 using std::chrono::steady_clock;
-using clock      = steady_clock;
+
+using clock = steady_clock;
+
 using time_point = std::chrono::steady_clock::time_point;
-using time_point = steady_clock::time_point;
+
 using namespace std::chrono_literals;
 
 using std::chrono::duration_cast;
