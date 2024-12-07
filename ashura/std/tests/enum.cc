ca4917406aef4cec7a633a2b53afc6dfac703b3c
Basit Ayantunde <rlamarrr@gmail.com>
2024-12-07 04:38:48 +0000

    ✨ added tagged-enum matching and element type getter

--- ashura/std/tests/enum.cc
+++ ashura/std/tests/enum.cc
@@ -9,8 +9,12 @@ TEST(EnumTest, Basic)
   Enum<StrMap<int>, int, float, int *> f{nullptr};
   EXPECT_EQ(f.index(), 3);
 
-  // [ ] map(   )
-  // [ ]
+  static constexpr Enum<int, char, float> e{0.2F};
 
-  static constexpr Enum<int, char, float> e{0.0F};
+  constexpr auto a = e.match([](f32) { return 1.0F; },        //
+                             [](f32) { return 0.5F; },        //
+                             [](f32) { return 3.0F; });
+
+  EXPECT_EQ(a, 3.0F);
+  EXPECT_EQ(e.v2_, 0.2F);
 }
