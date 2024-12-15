b7c4e0a3b5c205d98d70718256ded6e005821aa6
Basit Ayantunde <rlamarrr@gmail.com>
2024-12-15 06:34:21 +0000

    ✅ added binary_find tests

--- ashura/std/tests/range.cc
+++ ashura/std/tests/range.cc
@@ -23,3 +23,38 @@ TEST(Range, RunEndEncoding)
   EXPECT_EQ((*iter).v0.size(), 8);
   EXPECT_EQ((*iter).v0[0], 8);
 }
+
+TEST(BinaryFind, Find)
+{
+  static constexpr u32 data[] = {1, 1, 2, 3, 4, 5, 6, 7, 8};
+
+  {
+    constexpr auto s = binary_find(span(data), gt, 4U);
+    EXPECT_EQ(s[0], 5);
+  }
+
+  {
+    constexpr auto s = binary_find(span(data), geq, 4U);
+    EXPECT_EQ(s[0], 4);
+  }
+
+  {
+    constexpr auto s = binary_find(span(data), gt, 7U);
+    EXPECT_EQ(s[0], 8);
+  }
+
+  {
+    constexpr auto s = binary_find(span(data), geq, 7U);
+    EXPECT_EQ(s[0], 7);
+  }
+
+  {
+    constexpr auto s = binary_find(span(data), lt, 7U);
+    EXPECT_EQ(s[0], 1);
+  }
+
+  {
+    constexpr auto s = binary_find(span(data), leq, 7U);
+    EXPECT_EQ(s[0], 1);
+  }
+}
