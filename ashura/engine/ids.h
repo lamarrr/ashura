aad1195c38a0399adf1ca208ba2900078572c328
Basit Ayantunde <rlamarrr@gmail.com>
2024-12-24 20:28:31 +0000

    🎨 added asset id types

---
+++ ashura/engine/ids.h
@@ -0,0 +1,32 @@
+/// SPDX-License-Identifier: MIT
+#pragma once
+
+#include "ashura/std/types.h"
+
+namespace ash
+{
+
+enum ImageId : usize
+{
+};
+
+enum AudioId : usize
+{
+};
+
+enum VideoId : usize
+{
+};
+
+enum ShaderId : usize
+{
+};
+
+enum FontId : usize
+{
+  Default   = 0,
+  Primary   = 0,
+  Secondary = 1
+};
+
+}    // namespace ash
