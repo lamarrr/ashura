5b9b39419f3ceb4bff74c04fcb33c961ed9e9eb0
Basit Ayantunde <rlamarrr@gmail.com>
2025-10-14 21:17:59 +0100

    update

--- ashura/std/pieced.h
+++ ashura/std/pieced.h
@@ -2,14 +2,8 @@
 #pragma once
 
 #include "ashura/std/allocator.h"
-#include "ashura/std/dyn.h"
-#include "ashura/std/growth.h"
-#include "ashura/std/mem.h"
-#include "ashura/std/obj.h"
-#include "ashura/std/option.h"
+#include "ashura/std/range.h"
 #include "ashura/std/rc.h"
-#include "ashura/std/result.h"
-#include "ashura/std/traits.h"
 #include "ashura/std/types.h"
 #include "ashura/std/vec.h"
 
@@ -30,28 +24,96 @@ namespace ash
 template <typename Char>
 struct Pieced
 {
-  Vec<Char const *> segments_;
-  Vec<usize>        run_offsets_;
+  struct Iter
+  {
+  };
 
-  explicit constexpr Pieced(Allocator segments_allocator,
-                            Allocator run_allocator) :
+  Vec<Rc<Char const *>> segments_;
+  Vec<usize>            run_offsets_;
+
+  constexpr Pieced(Vec<Rc<Char const *>> segments, Vec<usize> run_offsets) :
+    segments_{std::move(segments)},
+    run_offsets_{std::move(run_offsets)}
+  {
+  }
+
+  constexpr Pieced(Allocator segments_allocator, Allocator run_allocator) :
     segments_{segments_allocator},
     run_offsets_{run_allocator}
   {
   }
 
-  void erase();
-  void insert();
-  void extend();
-  void clear();
-  void replace();
-  void get(Slice range, Vec<Char> & out);
+  constexpr Pieced() : Pieced{default_allocator, default_allocator}
+  {
+  }
+
+  void erase(Slice range)
+  {
+    if (run_offsets_.is_empty() || range.is_empty()) [[unlikely]]
+    {
+      return;
+    }
+
+    auto first = binary_find(run_offsets_.view(), gt, range.begin());
+
+    if (first.is_empty())
+    {
+      return;
+    }
+
+    auto last = binary_find(first, geq, range.end());
+
+    auto first_run = (usize) (first.pbegin() - run_offsets_.view().pbegin()) - 1;
+    auto last_run  = (usize) (last.pbegin() - run_offsets_.view().pbegin()) - 1;
+
+    if(run_offsets_[first_run] == range.begin())
+    {
+    }
+    else if(run_offsets_[last_run + 1] == range.end())
+    {
+    }
+    else
+    {
+    }
+    
+  }
+
+  void insert(usize pos, Span<Char const> text)
+  {
+  }
+
+  void extend(Span<Char const> text)
+  {
+  }
+
+  void clear()
+  {
+  }
+
+  void reset()
+  {
+  }
+
+  void replace(Slice range, Span<Char const> text)
+  {
+  }
+
+  void get(Slice range, Vec<Char> & out)
+  {
+  }
 
   template <typename F>
-  void iter(Slice range, F && func);
+  void iter(Slice range, F && func)
+  {
+  }
 
-  void begin();
-  void end();
+  Iter begin()
+  {
+  }
+
+  IterEnd end()
+  {
+  }
 };
 
 }    // namespace ash
