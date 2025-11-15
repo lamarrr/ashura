d9d6f42b43d9e47c7ff6735fe034a6ae3ac1f513
Basit Ayantunde <rlamarrr@gmail.com>
2025-11-15 04:38:04 +0000

    added strvec aliases

--- ashura/std/vec.h
+++ ashura/std/vec.h
@@ -2615,4 +2615,8 @@ void format(fmt::Sink sink, fmt::Spec spec, InplaceVec<char, C> const & str)
   format(sink, spec, str.view());
 }
 
+using StrVec8  = Vec<c8>;
+using StrVec16 = Vec<c16>;
+using StrVec32 = Vec<c32>;
+
 }    // namespace ash
