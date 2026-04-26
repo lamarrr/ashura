13c0c23169e24a0b10eb9fa9f9240e2897634df6
Basit Ayantunde <rlamarrr@gmail.com>
2026-04-26 21:18:34 +0100

    Refactor stacktrace implementation: remove Linux-specific stacktrace files
    - Removed `stacktrace_linux.hpp` and `stacktrace_linux_monolithic_example.hpp` files as they are no longer needed.
    - Updated `stacktrace.hpp` to include necessary headers and removed the unused `walk_stack_functions` declaration.

--- ashura/std/stacktrace.cpp
+++ ashura/std/stacktrace.cpp
@@ -3,9 +3,72 @@
 
 #include "ashura/std/cfg.hpp"
 
-#if ASH_CFG(OS, LINUX) && ASH_CFG(BINARY, ELF) && \
-  (ASH_CFG(ARCH, X86_64) || ASH_CFG(ARCH, ARM64))
-
-#else
-
+#if ASH_CFG(OS, LINUX)
+#    include <dlfcn.h>
 #endif
+
+namespace ash
+{
+
+#if ASH_CFG(OS, LINUX)
+
+void walk_stack_linux()
+{
+    void ** rbp = (void **) __builtin_frame_address(0);
+    usize   i   = 0;
+
+    while (rbp)
+    {
+        void *  ret_addr = *(rbp + 1);
+        Dl_info info;
+        if (dladdr(ret_addr, &info) != 0)
+        {
+            (void)
+              info.dli_fname;    // file name of shared object that contains the address
+            (void) info.dli_sname;    // name of nearest symbol
+            (void) info.dli_saddr;    // start address of the symbol
+            (void) i;
+        }
+        else
+        {
+        }
+        rbp = (void **) *rbp;
+        i++;
+    }
+}
+#endif
+
+#if ASH_CFG(OS, WINDOWS)
+void walk_stack_windows()
+{
+    void ** rbp = (void **) __builtin_frame_address(0);
+
+    while (rbp)
+    {
+        void * ret_addr = *(rbp + 1);
+
+        HANDLE       process = GetCurrentProcess();
+        char         buffer[sizeof(SYMBOL_INFO) + 256];
+        PSYMBOL_INFO symbol  = (PSYMBOL_INFO) buffer;
+        symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
+        symbol->MaxNameLen   = 255;
+
+        DWORD64 displacement = 0;
+
+        if (SymFromAddr(process, (DWORD64) ret_addr, &displacement, symbol))
+        {
+            (void) symbol->Name;       // name of the symbol
+            (void) symbol->Address;    // address of the symbol
+            (void) i;
+        }
+        else
+        {
+        }
+
+        rbp = (void **) *rbp;
+        i++;
+    }
+}
+#endif
+
+}    // namespace ash
--- ashura/std/stacktrace.hpp
+++ ashura/std/stacktrace.hpp
@@ -1,13 +1,12 @@
 #pragma once
 
 #include "ashura/std/fn.hpp"
-#include "ashura/std/span.hpp"
 #include "ashura/std/result.hpp"
+#include "ashura/std/span.hpp"
+
 
 namespace ash
 {
-
-Result<> walk_stack_functions(Fn<void(Str, usize)> callback);
 
 
 }    // namespace ash
